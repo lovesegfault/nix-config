e20764a77fce8076679462e177ab67dd169e80bb
Bernardo Meurer <meurerbernardo@gmail.com>
2019-03-20 16:39:56 -0700

    Finished update scripts

--- update-home.bash
+++ update-home.bash
@@ -4,9 +4,56 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# cp -df
+# Check if root
+if [ "$EUID" -eq 0 ]
+  then echo "Please run this as a normal user."
+  exit 1
+fi
+
+# Get locations
 home_manager_dir="${HOME}/.config/nixpkgs"
 git_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+home_dir="${git_dir}/home"
 
-# Copy files, overwriting & preserving symlinks
-cp -df "${git_dir}/home/"* "${home_manager_dir}/"
+# Check that we have configuration files
+if [ ! -d "${home_dir}" ] || [ ! -f "${home_dir}/home.nix" ]; then
+    echo "No home.nix found. Please place it under ${home_dir}."
+    exit 1
+fi
+
+# Prompt user about viewing file diffs
+read -p "Do you want to see a diff of the files being changed?  [Y/N] " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # Iterate over files and show diffs
+    for config in "${home_dir}/"*; do
+        f_name="${config##*/}"
+        if [ -f "${home_manager_dir}/${f_name}" ]; then
+            diff -y --color=always -- "${home_manager_dir}/${f_name}" "${config}" | less -cR
+        else
+            echo "New file ${f_name}"
+        fi
+    done
+fi
+
+# Prompt user for confirmation
+read -p "Are you sure you want to overwrite your current home configurations? [Y/N] " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    # Give up
+    echo "No files were altered."
+    exit 0
+fi
+
+# Backup existing files to /tmp & delete originals
+for config in "${home_manager_dir}/"*; do
+    [ -e ${config} ] || continue
+    f_name="${config##*/}"
+    mv -f "${config}" "/tmp/${f_name}.bak"
+done
+
+# Copy files
+cp -d "${home_dir}/"* "${home_manager_dir}/"
+
+# Success
+echo "Updated home configuration files. Please verify them before usage."
--- update-system.bash
+++ update-system.bash
@@ -10,28 +10,51 @@ if [ "$EUID" -ne 0 ]
   exit 1
 fi
 
-read -p "Are you sure? " -n 1 -r
+# Get locations
+hostname="$(hostname)"
+nixos_config_dir="/etc/nixos"
+git_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+system_dir="${git_dir}/system/${hostname}"
+
+# Check that we have configurations for this system in the tree
+if [ ! -d "${system_dir}" ] || [ ! -f "${system_dir}/configuration.nix" ]; then
+    echo "No configuration.nix found. Please place them under ${system_dir}."
+    exit 1
+fi
+
+# Prompt user about viewing file diffs
+read -p "Do you want to see a diff of the files being changed?  [Y/N] " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    # Get locations
-    hostname="$(hostname)"
-    nixos_config_dir="/etc/nixos"
-    git_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-    system_dir="${git_dir}/system/${hostname}"
+    # Iterate over files and show diffs
+    for config in "${system_dir}/"*; do
+        f_name="${config##*/}"
+        if [ -f "${nixos_config_dir}/${f_name}" ]; then
+            diff -y --color=always -- "${nixos_config_dir}/${f_name}" "${config}" | less -cR
+        else
+            echo "New file ${f_name}"
+        fi
+    done
+fi
 
-    # Check that we have configurations for this system in the tree
-    if [ ! -d "$system_dir" ]; then
-        echo "No configurations found. Please place them under ${system_dir}."
-        exit 1
-    fi
-
-    # Copy files, overwriting & preserving symlinks
-    cp -df "${system_dir}"* "${nixos_config_dir}/"
-else
+# Prompt user for confirmation
+read -p "Are you sure you want to overwrite your current system configurations? [Y/N] " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
     # Give up
     echo "No files were altered."
     exit 0
 fi
+
+# Backup existing files to /tmp & delete originals
+for config in "${nixos_config_dir}/"*; do
+    [ -e ${config} ] || continue
+    f_name="${config##*/}"
+    mv -f "${config}" "/tmp/${f_name}.bak"
+done
+
+# Copy files
+cp -d "${system_dir}/"* "${nixos_config_dir}/"
 
 # Success
 echo "Updated system configuration files. Please verify them before usage."
