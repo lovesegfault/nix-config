11d418757a167beb9931395b1134f5f363e8301f
Bernardo Meurer <meurerbernardo@gmail.com>
2019-10-25 15:17:32 -0700

    home/modules/zsh: finish declaring plugins

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -17,8 +17,8 @@
       bindkey "$${terminfo[khome]}" beginning-of-line
       bindkey "$${terminfo[kend]}" end-of-line
       bindkey "$${terminfo[kdch1]}" delete-char
-      bindkey "^[[A" history-substring-search-up
-      bindkey "^[[B" history-substring-search-down
+      bindkey "$$terminfo[kcuu1]" history-substring-search-up
+      bindkey "$$terminfo[kcud1]" history-substring-search-down
       bindkey "^[[1;5C" forward-word
       bindkey "^[[1;3C" forward-word
       bindkey "^[[1;5D" backward-word
@@ -75,11 +75,12 @@
     plugins = [
       {
         name = "zsh-async";
+        file = "async.plugin.zsh";
         src = pkgs.fetchFromGitHub {
           owner = "mafredri";
           repo = "zsh-async";
           rev = "v1.7.2";
-          sha256 = "0vl5dymd07mi42wgkh0c3q8vf76hls1759dirlh3ryrq6w9nrdbf";
+          sha256 = "0kp94pqpy7qbmy51s6iwpnc96a1aq0nmmdp5fgssksan070hnw96";
         };
       }
       {
@@ -89,25 +90,57 @@
           owner = "sindresorhus";
           repo = "pure";
           rev = "v1.11.0";
-          sha256 = "0vl5dymd07mi42wgkh0c3q8vf76hls1759dirlh3ryrq6w9nrdbf";
+          sha256 = "0nzvb5iqyn3fv9z5xba850mxphxmnsiq3wxm1rclzffislm8ml1j";
         };
       }
       {
         name = "fast-syntax-highlighting";
+        file = "fast-syntax-highlighting.plugin.zsh";
         src = pkgs.fetchFromGitHub {
           owner = "zdharma";
           repo = "fast-syntax-highlighting";
           rev = "v1.54";
-          sha256 = "0vl5dymd07mi42wgkh0c3q8vf76hls1759dirlh3ryrq6w9nrdbf";
+          sha256 = "019hda2pj8lf7px4h1z07b9l6icxx4b2a072jw36lz9bh6jahp32";
         };
       }
       {
         name = "history-search-multi-word";
+        file = "history-search-multi-word.plugin.zsh";
         src = pkgs.fetchFromGitHub {
           owner = "zdharma";
           repo = "history-search-multi-word";
-          rev = "ab2cb3444b543bd1f0f983956f9dbad4620368b8";
-          sha256 = "0vl5dymd07mi42wgkh0c3q8vf76hls1759dirlh3ryrq6w9nrdbf";
+          rev = "v1.2.4";
+          sha256 = "1r1kyy1w2g2jhal4zjj2zp5i2giya6j4v7a7vk32g4vjg12q8hv0";
+        };
+      }
+      {
+        name = "zsh-autopair";
+        file = "zsh-autopair.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "hlissner";
+          repo = "zsh-autopair";
+          rev = "v1.0";
+          sha256 = "1h0vm2dgrmb8i2pvsgis3lshc5b0ad846836m62y8h3rdb3zmpy1";
+        };
+      }
+      {
+        name = "zsh-sudo";
+        file = "sudo.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "hcgraf";
+          repo = "zsh-sudo";
+          rev = "d8084def6bb1bde2482e7aa636743f40c69d9b32";
+          sha256 = "1dpm51w3wjxil8sxqw4qxim5kmf6afmkwz1yfhldpdlqm7rfwpi3";
+        };
+      }
+      {
+        name = "zsh-history-substring-search";
+        file = "zsh-history-substring-search.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-history-substring-search";
+          rev = "0f80b8eb3368b46e5e573c1d91ae69eb095db3fb";
+          sha256 = "0y8va5kc2ram38hbk2cibkk64ffrabfv1sh4xm7pjspsba9n5p1y";
         };
       }
     ];
