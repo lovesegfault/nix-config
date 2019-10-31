3d33fb99f4f7dd4233af7d7b21586864b981d0a4
Bernardo Meurer <meurerbernardo@gmail.com>
2019-10-31 15:41:19 -0700

    neovim: coc-neovim setup

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -3,42 +3,164 @@
     enable = true;
     viAlias = true;
     vimAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      coc-nvim
+      vim-plug
+      ale
+      ayu-vim
+      fzf-vim
+      gentoo-syntax
+      gist-vim
+      tagbar
+      goyo
+      lalrpop-vim
+      lightline-ale
+      lightline-vim
+      meson
+      rainbow
+      rust-vim
+      vim-flatbuffers
+      vim-indent-guides
+      vim-multiple-cursors
+      vim-nftables
+      vim-nix
+      vim-protobuf
+      vim-surround
+      vim-toml
+      vim-trailing-whitespace
+      vimtex
+      webapi-vim
+    ];
     extraConfig = ''
-      " Plugins
-      call plug#begin('~/.local/share/nvim/plugged')
-      Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
-      Plug 'Shougo/neco-syntax'
-      Plug 'ayu-theme/ayu-vim'
-      Plug 'bronson/vim-trailing-whitespace'
-      Plug 'cespare/vim-toml'
-      Plug 'dag/vim-fish'
-      Plug 'dcharbon/vim-flatbuffers'
-      Plug 'dense-analysis/ale'
-      Plug 'gentoo/gentoo-syntax'
-      Plug 'igankevich/mesonic'
-      Plug 'itchyny/lightline.vim'
-      Plug 'junegunn/goyo.vim'
-      Plug 'lervag/vimtex'
-      Plug 'liuchengxu/graphviz.vim'
-      Plug 'lnl7/vim-nix'
-      Plug 'lotabout/skim', {'do':'./install'}
-      Plug 'lotabout/skim.vim'
-      Plug 'luochen1990/rainbow'
-      Plug 'majutsushi/tagbar'
-      Plug 'mattn/gist-vim'
-      Plug 'mattn/webapi-vim'
-      Plug 'maximbaz/lightline-ale'
-      Plug 'nathanaelkane/vim-indent-guides'
-      Plug 'nfnty/vim-nftables'
-      Plug 'potatoesmaster/i3-vim-syntax'
-      Plug 'powerman/vim-plugin-AnsiEsc'
-      Plug 'qnighy/lalrpop.vim'
-      Plug 'rust-lang/rust.vim'
-      Plug 'tbodt/deoplete-tabnine', { 'do': './install.sh' }
-      Plug 'terryma/vim-multiple-cursors'
-      Plug 'tpope/vim-surround'
-      Plug 'uarun/vim-protobuf'
-      call plug#end()
+      " ---- CoC
+      " Some servers have issues with backup files, see #649
+      set nobackup
+      set nowritebackup
+
+      " Better display for messages
+      set cmdheight=2
+
+      " You will have bad experience for diagnostic messages when it's default 4000.
+      set updatetime=300
+
+      " don't give |ins-completion-menu| messages.
+      set shortmess+=c
+
+      " always show signcolumns
+      set signcolumn=yes
+
+      " Use tab for trigger completion with characters ahead and navigate.
+      " Use command ':verbose imap <tab>' to make sure tab is not mapped by other plugin.
+      inoremap <silent><expr> <TAB>
+            \ pumvisible() ? "\<C-n>" :
+            \ <SID>check_back_space() ? "\<TAB>" :
+            \ coc#refresh()
+      inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+
+      function! s:check_back_space() abort
+        let col = col('.') - 1
+        return !col || getline('.')[col - 1]  =~# '\s'
+      endfunction
+
+      " Use <c-space> to trigger completion.
+      inoremap <silent><expr> <c-space> coc#refresh()
+
+      " Use <cr> to confirm completion, `<C-g>u` means break undo chain at current position.
+      " Coc only does snippet and additional edit on confirm.
+      inoremap <expr> <cr> pumvisible() ? "\<C-y>" : "\<C-g>u\<CR>"
+      " Or use `complete_info` if your vim support it, like:
+      " inoremap <expr> <cr> complete_info()["selected"] != "-1" ? "\<C-y>" : "\<C-g>u\<CR>"
+
+      " Use `[g` and `]g` to navigate diagnostics
+      nmap <silent> [g <Plug>(coc-diagnostic-prev)
+      nmap <silent> ]g <Plug>(coc-diagnostic-next)
+
+      " Remap keys for gotos
+      nmap <silent> gd <Plug>(coc-definition)
+      nmap <silent> gy <Plug>(coc-type-definition)
+      nmap <silent> gi <Plug>(coc-implementation)
+      nmap <silent> gr <Plug>(coc-references)
+
+      " Use K to show documentation in preview window
+      nnoremap <silent> K :call <SID>show_documentation()<CR>
+
+      function! s:show_documentation()
+        if (index(['vim','help'], &filetype) >= 0)
+          execute 'h '.expand('<cword>')
+        else
+          call CocAction('doHover')
+        endif
+      endfunction
+
+      " Highlight symbol under cursor on CursorHold
+      autocmd CursorHold * silent call CocActionAsync('highlight')
+
+      " Remap for rename current word
+      nmap <leader>rn <Plug>(coc-rename)
+
+      " Remap for format selected region
+      xmap <leader>f  <Plug>(coc-format-selected)
+      nmap <leader>f  <Plug>(coc-format-selected)
+
+      augroup mygroup
+        autocmd!
+        " Setup formatexpr specified filetype(s).
+        autocmd FileType typescript,json setl formatexpr=CocAction('formatSelected')
+        " Update signature help on jump placeholder
+        autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
+      augroup end
+
+      " Remap for do codeAction of selected region, ex: `<leader>aap` for current paragraph
+      xmap <leader>a  <Plug>(coc-codeaction-selected)
+      nmap <leader>a  <Plug>(coc-codeaction-selected)
+
+      " Remap for do codeAction of current line
+      nmap <leader>ac  <Plug>(coc-codeaction)
+      " Fix autofix problem of current line
+      nmap <leader>qf  <Plug>(coc-fix-current)
+
+      " Create mappings for function text object, requires document symbols feature of languageserver.
+      xmap if <Plug>(coc-funcobj-i)
+      xmap af <Plug>(coc-funcobj-a)
+      omap if <Plug>(coc-funcobj-i)
+      omap af <Plug>(coc-funcobj-a)
+
+      " Use <C-d> for select selections ranges, needs server support, like: coc-tsserver, coc-python
+      nmap <silent> <C-d> <Plug>(coc-range-select)
+      xmap <silent> <C-d> <Plug>(coc-range-select)
+
+      " Use `:Format` to format current buffer
+      command! -nargs=0 Format :call CocAction('format')
+
+      " Use `:Fold` to fold current buffer
+      command! -nargs=? Fold :call     CocAction('fold', <f-args>)
+
+      " use `:OR` for organize import of current buffer
+      command! -nargs=0 OR   :call     CocAction('runCommand', 'editor.action.organizeImport')
+
+      " Add status line support, for integration with other plugin, checkout `:h coc-status`
+      set statusline^=%{coc#status()}%{get(b:,'coc_current_function',''')}
+
+      " Using CocList
+      " Show all diagnostics
+      nnoremap <silent> <space>a  :<C-u>CocList diagnostics<cr>
+      " Manage extensions
+      nnoremap <silent> <space>e  :<C-u>CocList extensions<cr>
+      " Show commands
+      nnoremap <silent> <space>c  :<C-u>CocList commands<cr>
+      " Find symbol of current document
+      nnoremap <silent> <space>o  :<C-u>CocList outline<cr>
+      " Search workspace symbols
+      nnoremap <silent> <space>s  :<C-u>CocList -I symbols<cr>
+      " Do default action for next item.
+      nnoremap <silent> <space>j  :<C-u>CocNext<CR>
+      " Do default action for previous item.
+      nnoremap <silent> <space>k  :<C-u>CocPrev<CR>
+      " Resume latest coc list
+      nnoremap <silent> <space>p  :<C-u>CocListResume<CR>
+
+      " ----------------------------------------------------------------------
+
       " ---- Line numbers
       set number
       set cursorline
@@ -167,13 +289,6 @@
           let &t_EI = "\<Esc>]50;CursorShape=0\x7"
       endif
       " Plugin settings
-      " ---- Shougo/deoplete.nvim
-      let g:deoplete#enable_at_startup = 1
-      let g:deoplete#sources#clang#libclang_path = "/usr/lib/llvm/9/lib64/libclang.so"
-      let g:deoplete#sources#clang#clang_header = "/usr/lib/llvm/9/include/clang"
-
-      call deoplete#custom#source('tabnine', 'rank', 100)
-      inoremap <expr><TAB>  pumvisible() ? "\<C-n>" : "\<TAB>"
       " ---- luochen1990/rainbow
       let g:rainbow_active = 1
       " ---- lotabout/skim.vim
@@ -190,7 +305,6 @@
       set grepprg=rg\ --vimgrep\ --no-heading\ --smart-case
       command -nargs=+ -complete=file -bar Rg silent! grep! <args>|cwindow|redraw!
       " ---- lervag/vimtex
-      call deoplete#custom#var('omni', 'input_patterns', {'tex': g:vimtex#re#deoplete})
       let g:vimtex_compiler_latexmk = {
           \ 'options' : [
           \   '-pdf',
