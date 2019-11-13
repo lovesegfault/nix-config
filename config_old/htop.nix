f6adff4242c5b153e344af9ee3339c9e283043ad
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-13 01:07:51 -0800

    nuke old

--- config_old/htop.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  programs.htop = {
-    enable = true;
-  };
-}
--- config_old/xresources.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  xresources = if config.isDesktop then {
-    properties = {
-      "Xft.dpi" = 220;
-    };
-  } else {};
-}
