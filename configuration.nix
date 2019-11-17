8ab77dc4fbcbf87b03745358bf0b42df55e4047f
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-16 17:22:39 -0800

    system: fix example configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,8 +1,6 @@
-{ config, pkgs, ... }: {
+{
   imports = [
     ./hardware-configuration.nix
-    "./machines/${networking.hostName}.nix"
+    ./machines/HOSTNAME.nix
   ];
-
-  networking.hostName = "wittgenstein";
 }
