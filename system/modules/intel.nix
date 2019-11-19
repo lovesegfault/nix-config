43ae0411490e7d8f8d12aac7f474a923440df979
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-18 22:23:53 -0800

    system/intel: fix spacing

--- system/modules/intel.nix
+++ system/modules/intel.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }: {
   boot.kernelParams = [ "intel_iommu=on" ];
+
   hardware = {
     cpu.intel.updateMicrocode = true;
     opengl = {
       enable = true;
-      extraPackages = with pkgs; [ vaapiIntel vaapiVdpau libvdpau-va-gl ];
+      extraPackages = with pkgs; [ vaapiIntel vaapiVdpau libvdpau-va-gl libva intel-compute-runtime ];
     };
   };
-  services.xserver.useGlamor = true;
 
+  services.xserver.useGlamor = true;
 }
