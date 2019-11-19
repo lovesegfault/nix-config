7a5600e45b726bd76403c2630c46e7ddbc607c6c
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-18 23:14:54 -0800

    system/i3: add

---
+++ system/modules/i3.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }: {
+  services.xserver = {
+    enable = true;
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+      extraPackages = with pkgs; [
+        alacritty
+        scrot
+        libinput-gestures
+        light
+        dunst
+        feh
+        i3lock
+        i3status-rust
+        xclip
+        xsel
+      ];
+    };
+
+    xautolock.locker = "${pkgs.i3lock}/bin/i3lock -i ~/pictures/walls/clouds.png";
+  };
+}
