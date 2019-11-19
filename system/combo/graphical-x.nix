3f3aafa6b5970085f05805280d7ada6cbd20213b
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-18 23:16:38 -0800

    system/graphical-x: add

---
+++ system/combo/graphical-x.nix
@@ -0,0 +1,14 @@
+{
+  imports = [
+    ../modules/boot_silent.nix
+    ../modules/fonts.nix
+    ../modules/gdm.nix
+    ../modules/gnome.nix
+    ../modules/gnome_keyring.nix
+    ../modules/i3.nix
+    ../modules/printing.nix
+    ../modules/sound.nix
+    ../modules/xautolock.nix
+    ../modules/xserver.nix
+  ];
+}
