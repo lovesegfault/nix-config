1d158bab4ef28e74affe3efad992ea5a8960b7b0
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-20 00:30:46 -0800

    system/x: force wayland off

--- system/combo/x.nix
+++ system/combo/x.nix
@@ -1,7 +1,9 @@
-{
+{ lib, ... }: {
   imports = [
     ../modules/i3.nix
     ../modules/xautolock.nix
     ../modules/xserver.nix
   ];
+
+  services.xserver.displayManager.gdm.wayland = lib.mkForce false;
 }
