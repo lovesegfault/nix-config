2ae3139dd292a0f6f62802d954dc65f1b57c3227
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-20 00:30:22 -0800

    system/xautolock: interpolate the integer

--- system/modules/xautolock.nix
+++ system/modules/xautolock.nix
@@ -5,7 +5,7 @@
       enable = true;
       enableNotifier = true;
       extraOptions = [ "-lockaftersleep" ];
-      notifier = ''${pkgs.libnotify}/bin/notify-send "Locking in ${notify} seconds"'';
+      notifier = ''${pkgs.libnotify}/bin/notify-send "Locking in ${toString notify} seconds"'';
       notify = 10;
       nowlocker = config.services.xserver.xautolock.locker;
       time = 5;
