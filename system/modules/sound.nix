413be51d7c8654c8763d4236dbe69c0fe6fd78b8
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-21 18:53:58 -0800

    system/sound: high quality audio

--- system/modules/sound.nix
+++ system/modules/sound.nix
@@ -2,7 +2,17 @@
   hardware.pulseaudio = {
     enable = true;
     package = pkgs.pulseaudioFull;
-    daemon.config = { realtime-scheduling = "yes"; };
+    daemon.config = {
+      realtime-scheduling = "yes";
+      default-sample-format = "s32le";
+      default-sample-rate = 96000;
+      alternate-sample-rate = 88200;
+      default-sample-channels = 2;
+      default-fragments = 2;
+      enable-lfe-remixing = "no";
+      default-fragment-size-msec = 125;
+      resample-method = "src-sinc-best-quality";
+    };
   };
 
   security.rtkit.enable = true;
