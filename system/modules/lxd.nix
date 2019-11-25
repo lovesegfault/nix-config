91ca6b5a70141badf9ad8d679a1e0f8a9125f45e
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-24 20:29:05 -1000

    system/lxd: remove whitespace

--- system/modules/lxd.nix
+++ system/modules/lxd.nix
@@ -3,7 +3,6 @@
     "fs.inotify.max_queued_events" = 1048576;
     "fs.inotify.max_user_instances" = 1048576;
     "fs.inotify.max_user_watches" = 1048576;
-
   };
 
   users.users.bemeurer.extraGroups = [ "lxd" ];
