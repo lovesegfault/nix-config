db4cdef61f954eb354cae87fe883f5d8f9cf42c8
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-28 09:14:45 -1000

    system/aspell add pt_BR

--- system/modules/aspell.nix
+++ system/modules/aspell.nix
@@ -3,12 +3,14 @@
     aspellDicts.en
     aspellDicts.en-computers
     aspellDicts.en-science
+    aspellDicts.pt_BR
   ];
 
   # Configure aspell system wide
   environment.etc."aspell.conf".text = ''
     master en_US
-    extra-dicts en-computers.rws
+    add-extra-dicts en-computers.rws
     add-extra-dicts en_US-science.rws
+    add-extra-dicts pt_BR
   '';
 }
