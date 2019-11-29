d92462ec53370e2946242b227dfba1eb59b8b8b0
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-28 19:37:13 -1000

    system/silent-boot: rework params

--- system/modules/boot-silent.nix
+++ system/modules/boot-silent.nix
@@ -3,10 +3,12 @@
     consoleLogLevel = 1;
     kernelParams = [
       "quiet"
+      "splash"
+      "vga=current"
+      "i915.fastboot=1"
+      "loglevel=3"
       "systemd.show_status=auto"
       "udev.log_priority=3"
-      "i915.fastboot=1"
-      "vga=current"
     ];
   };
 }
