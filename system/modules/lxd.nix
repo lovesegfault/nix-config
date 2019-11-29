9afa2e3243442182cab9c01a0f022b410a1d99e4
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-28 19:38:51 -1000

    system/lxd: nixfmt

--- system/modules/lxd.nix
+++ system/modules/lxd.nix
@@ -6,8 +6,18 @@
   };
 
   security.pam.loginLimits = [
-    { domain = "lxd"; type = "soft"; item ="nofile"; value = "1000000"; }
-    { domain = "lxd"; type = "hard"; item ="nofile"; value = "1000000"; }
+    {
+      domain = "lxd";
+      type = "soft";
+      item = "nofile";
+      value = "1000000";
+    }
+    {
+      domain = "lxd";
+      type = "hard";
+      item = "nofile";
+      value = "1000000";
+    }
   ];
 
   systemd.services.lxd.serviceConfig.LimitNOFILE = 49152;
