bf7d2fd0d5dc534f1e06926dc4e78b308c73e9fa
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-30 10:41:31 -1000

    system/bluetooth: Hi-Fi audio support

--- system/modules/bluetooth.nix
+++ system/modules/bluetooth.nix
@@ -5,4 +5,12 @@
     powerOnBoot = false;
     config = { General = { Enable = "Source,Sink,Media,Socket"; }; };
   };
+
+  hardware.pulseaudio = {
+    extraConfig = ''
+      load-module module-switch-on-connect
+      load-module module-bluetooth-discover a2dp_config="ldac_eqmid=hq ldac_fmt=f32"
+    '';
+    extraModules = with pkgs; [ pulseaudio-modules-bt ];
+  };
 }
