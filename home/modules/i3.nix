ba07fb27f2db0da0b2ab487a08ac31e626bbd9ef
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-02 22:41:00 -0800

    home/i3: revamp

--- home/modules/i3.nix
+++ home/modules/i3.nix
@@ -1,11 +1,18 @@
 { config, lib, pkgs, ... }: {
-  imports = [ ../pkgs/passmenu.nix ../pkgs/prtsc.nix ../pkgs/swaymenu.nix ];
+  imports = [ ../pkgs/gebaar.nix ../pkgs/passmenu.nix ../pkgs/prtsc.nix ../pkgs/swaymenu.nix ];
 
-  xdg.configFile.libinput-gestures = {
-    target = "libinput-gestures.conf";
+  xdg.configFile.gebaar = {
+    target = "gebaar/gebaard.toml";
     text = ''
-      gesture swipe left 3 i3-msg "workspace next"
-      gesture swipe right 3 i3-msg "workspace prev"
+      [commands.swipe.three]
+      left_up = ""
+      right_up = ""
+      up = ""
+      left_down = ""
+      right_down = ""
+      down = ""
+      left = "swaymsg \"workspace next\""
+      right = "swaymsg \"workspace prev\""
     '';
   };
 
@@ -13,17 +20,6 @@
     windowManager.i3 = {
       enable = true;
       config = rec {
-        bars = let
-          bar = "i3status-rs";
-          cfg = "${config.xdg.configFile.i3status-rust.target}";
-        in [{
-          statusCommand = "${bar} ${cfg}";
-          fonts = [ "FontAwesome 10" "Hack 10" ];
-          position = "top";
-          workspaceNumbers = false;
-          colors.background = "#424242";
-        }];
-
         floating = {
           border = 0;
           criteria = [ { title = "swaymenu"; } { title = "passmenu"; } ];
@@ -36,7 +32,7 @@
           newWindow = "smart";
         };
 
-        fonts = [ "FontAwesome 8" "Hack 8" ];
+        fonts = [ "Hack 8" ];
 
         gaps = {
           inner = 5;
@@ -46,27 +42,34 @@
         };
 
         keybindings = let
+          dunst = "${pkgs.dunst}/bin/dunst";
+          feh = "${pkgs.feh}/bin/feh";
+          gebaard = "${pkgs.gebaar}/bin/gebaard";
+          light = "${pkgs.light}/bin/light";
           lock = "i3lock -i ~/pictures/walls/clouds.png -e -f";
           menu = "${term} -d 80 20 -t swaymenu -e ${swaymenu}";
           passmenu = "${pkgs.passmenu}/bin/passmenu";
+          playerctl = "${pkgs.playerctl}/bin/playerctl";
           prtsc = "${pkgs.prtsc}/bin/prtsc";
+          redshift = "${pkgs.redshift}/bin/redshift";
           swaymenu = "${pkgs.swaymenu}/bin/swaymenu";
-          term = "alacritty";
+          term = "${pkgs.alacritty}/bin/alacritty";
         in lib.mkOptionDefault {
           "${modifier}+Return" = "exec ${term}";
           "${modifier}+d" = "exec ${menu}";
           "${modifier}+p" = "exec ${passmenu}";
           "Print" = "exec ${prtsc}";
-          "XF86MonBrightnessUp" = "exec light -A 1";
-          "XF86MonBrightnessDown" = "exec light -U 1";
-          "XF86AudioRaiseVolume" =
-            "exec pactl set-sink-volume @DEFAULT_SINK@ +1%";
-          "XF86AudioLowerVolume" =
-            "exec pactl set-sink-volume @DEFAULT_SINK@ -1%";
+          "XF86MonBrightnessUp" = "exec ${light} -A 1";
+          "XF86MonBrightnessDown" = "exec ${light} -U 1";
+          "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +1%";
+          "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -1%";
           "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
-          "XF86AudioMicMute" =
-            "exec pactl set-source-mute @DEFAULT_SOURCE@ toggle";
+          "XF86AudioMicMute" = "exec pactl set-source-mute @DEFAULT_SOURCE@ toggle";
           "XF86Display" = "exec ${lock}";
+          "XF86AudioPlay" = "exec ${playerctl} play";
+          "XF86AudioPause" = "exec ${playerctl} pause";
+          "XF86AudioNext" = "exec ${playerctl} next";
+          "XF86AudioPrev" = "exec ${playerctl} previous";
           "${modifier}+Shift+c" = "reload";
           "${modifier}+Shift+r" = "restart";
           "${modifier}+1" = "workspace 1:α";
@@ -98,12 +101,13 @@
         modifier = "Mod4";
 
         startup = [
-          { command = "dbus-update-activation-environment --all"; }
-          { command = "dunst"; }
-          { command = "feh --bg-fill ~/pictures/walls/clouds.png"; }
-          { command = "libinput-gestures"; }
+          { command = "dbus-update-activation-environment --systemd DISPLAY"; }
+          { command = "${dunst}"; }
+          { command = "${feh} --bg-fill ~/pictures/walls/clouds.png"; }
+          { command = "${gebaard}"; }
           { command = "pactl set-sink-mute @DEFAULT_SINK@ true"; }
           { command = "pactl set-source-mute @DEFAULT_SOURCE@ true"; }
+          { command = "${redshift}"; }
           { command = "systemctl --user start gnome-keyring"; }
         ];
 
