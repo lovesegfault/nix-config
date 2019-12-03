13d7ee8006c069f05070b78ff2d4cb3ee961a7b1
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-03 01:22:18 -0800

    system/thinkpad_p1: fix var exports for i3

--- system/combo/thinkpad-p1.nix
+++ system/combo/thinkpad-p1.nix
@@ -42,9 +42,9 @@
         accelSpeed = "0.7";
       };
       windowManager.i3.extraSessionCommands = ''
-        export GDK_SCALE="2"
-        export GDK_DPI_SCALE="0.5"
-        export QT_AUTO_SCREEN_SCALE_FACTOR="1"
+        export GDK_SCALE=2
+        export GDK_DPI_SCALE=0.5
+        export QT_AUTO_SCREEN_SCALE_FACTOR=1
       '';
     };
   };
