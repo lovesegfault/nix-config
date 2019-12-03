9f5f93df4a742908b29a148a319750ce5cfb98f8
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-02 16:58:23 -0800

    system/thinkpad_p1: set scaling

--- system/combo/thinkpad-p1.nix
+++ system/combo/thinkpad-p1.nix
@@ -36,8 +36,13 @@
       coreOffset = "-70";
       temp = "95";
     };
-
-    xserver.libinput.accelSpeed = "0.7";
+    xserver = {
+      windowManager.i3.extraSessionCommands = ''
+        export GDK_SCALE = "2";
+        export GDK_DPI_SCALE = "0.5";
+        export QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      '';
+    };
   };
 
   users.users.bemeurer.extraGroups = [ "camera" ];
