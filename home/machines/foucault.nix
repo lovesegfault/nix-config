35bba4111f0b6f003d88296bd2d9cf9a2a181203
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-04 12:24:22 -0800

    home/foucault: disable i3

--- home/machines/foucault.nix
+++ home/machines/foucault.nix
@@ -5,7 +5,7 @@
     ../combo/graphical.nix
     ../combo/music.nix
     ../combo/sway.nix
-    ../combo/i3.nix
+    # ../combo/i3.nix
     ../combo/thinkpad-p1.nix
 
     ../../share/secrets/secrets.nix
