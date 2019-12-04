5cdbad8f23de47b5438b4c1e42f795c1fac76182
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-03 21:59:01 -0800

    home/xsession: nuke xrandr cmd

--- home/modules/xsession.nix
+++ home/modules/xsession.nix
@@ -11,9 +11,6 @@
       export QT_AUTO_SCREEN_SCALE_FACTOR=1
       export GDK_SCALE=2
       export GDK_DPI_SCALE=0.5
-      xrandr \
-      --output DP-1.1 --scale 2x2 --mode 2560x1440 --pos 3840x0 --rotate left \
-      --output eDP-1-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal
     '';
   };
 }
