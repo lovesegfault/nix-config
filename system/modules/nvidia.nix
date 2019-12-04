88f1672b5e235fdc7c8eb84ab93b14aceb4ec3d9
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-03 18:47:07 -0800

    system/nvidia: set gdm.wayland = false; fix drivers

--- system/modules/nvidia.nix
+++ system/modules/nvidia.nix
@@ -5,15 +5,13 @@
   };
 
   hardware = {
-    nvidia = {
-      optimus_prime = {
-        enable = true;
-        nvidiaBusId = "PCI:1:0:0";
-        intelBusId = "PCI:0:2:0";
-      };
-      modesetting.enable = true;
+    nvidia.optimus_prime = {
+      enable = true;
+      nvidiaBusId = "PCI:1:0:0";
+      intelBusId = "PCI:0:2:0";
     };
   };
 
-  services.xserver.videoDrivers = [ "intel" "nvidia" ];
+  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.displayManager.gdm.wayland = false;
 }
