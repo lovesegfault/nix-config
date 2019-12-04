61b2a254793b909d9d81507bed8883c59258d818
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-03 18:45:56 -0800

    system/x: simplify

--- system/combo/x.nix
+++ system/combo/x.nix
@@ -1,6 +1,3 @@
-{ lib, ... }: {
-  imports =
-    [ ../modules/i3.nix ../modules/xautolock.nix ../modules/xserver.nix ];
-
-  # services.xserver.displayManager.gdm.wayland = lib.mkForce false;
+{
+  imports = [ ../modules/i3.nix ../modules/xautolock.nix ../modules/xserver.nix ];
 }
