c6eb9b556c2a866b547ad52759c561bea8d34640
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-05 14:08:07 -0800

    home/waybar: make bar opaque

--- home/modules/sway.nix
+++ home/modules/sway.nix
@@ -76,7 +76,7 @@
       input "1739:31251:SYNA2393:00_06CB:7A13_Touchpad" {
           accel_profile adaptive
           click_method button_areas
-          dwt disabled
+          dwt enabled
           natural_scroll enabled
           scroll_method two_finger
           tap enabled
@@ -85,7 +85,7 @@
       input "2:8:AlpsPS/2_ALPS_DualPoint_TouchPad" {
           accel_profile adaptive
           click_method button_areas
-          dwt disabled
+          dwt enabled
           natural_scroll enabled
           scroll_method two_finger
           tap enabled
@@ -206,7 +206,7 @@
       # Layout stuff:
       #
           font pango:Hack 8
-          gaps inner 5
+          gaps inner 10
           gaps outer 10
           smart_borders on
           default_border none
