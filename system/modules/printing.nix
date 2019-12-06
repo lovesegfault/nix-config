f9b1f79f9c5eaeadb3716d3fcfcbc464b4592544
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-05 20:29:01 -0800

    system/printing: enable nddmdns

--- system/modules/printing.nix
+++ system/modules/printing.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }: {
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
   services.printing = {
     enable = true;
-    drivers = with pkgs; [ gutenprint cups-googlecloudprint ];
+    drivers = with pkgs; [ gutenprint cups-googlecloudprint canon-cups-ufr2 ];
   };
 }
