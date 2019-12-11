893e0fdec7c4b5b3fe45e91601e9aefc4e0e2129
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-11 13:39:59 -0800

    system/aarch64-build-box: change key

--- system/modules/aarch64-build-box.nix
+++ system/modules/aarch64-build-box.nix
@@ -5,7 +5,7 @@
       {
         hostName = "aarch64.nixos.community";
         maxJobs = 64;
-        sshKey = "/root/bemeurer_rsa";
+        sshKey = "/root/aarch64.community.nixos";
         sshUser = "lovesegfault";
         system = "aarch64-linux";
         supportedFeatures = [ "big-parallel" ];
