af9331e6f4c7b8cfcad8396a3c67b57062ef9394
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-11 14:29:49 -0800

    system/fonts: color emoji

--- system/modules/fonts.nix
+++ system/modules/fonts.nix
@@ -2,8 +2,112 @@
   fonts = {
     fontconfig = {
       defaultFonts.monospace = [ "Hack" ];
-      penultimate.enable = true;
+      localConf = ''
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+        <fontconfig>
+          <!-- Add generic family. -->
+          <match target="pattern">
+              <test qual="any" name="family"><string>emoji</string></test>
+              <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <!-- This adds Noto Color Emoji as a final fallback font for the default font families. -->
+          <match target="pattern">
+              <test name="family"><string>sans</string></test>
+              <edit name="family" mode="append"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test name="family"><string>serif</string></test>
+              <edit name="family" mode="append"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test name="family"><string>sans-serif</string></test>
+              <edit name="family" mode="append"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test name="family"><string>monospace</string></test>
+              <edit name="family" mode="append"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <!-- Block Symbola from the list of fallback fonts. -->
+          <selectfont>
+              <rejectfont>
+                  <pattern>
+                      <patelt name="family">
+                          <string>Symbola</string>
+                      </patelt>
+                  </pattern>
+              </rejectfont>
+          </selectfont>
+
+          <!-- Use Noto Color Emoji when other popular fonts are being specifically requested. -->
+          <match target="pattern">
+              <test qual="any" name="family"><string>Apple Color Emoji</string></test>
+              <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test qual="any" name="family"><string>Segoe UI Emoji</string></test>
+              <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test qual="any" name="family"><string>Segoe UI Symbol</string></test>
+              <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test qual="any" name="family"><string>Android Emoji</string></test>
+              <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test qual="any" name="family"><string>Twitter Color Emoji</string></test>
+              <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test qual="any" name="family"><string>Twemoji</string></test>
+              <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test qual="any" name="family"><string>Twemoji Mozilla</string></test>
+              <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test qual="any" name="family"><string>TwemojiMozilla</string></test>
+              <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test qual="any" name="family"><string>EmojiTwo</string></test>
+              <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test qual="any" name="family"><string>Emoji Two</string></test>
+              <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test qual="any" name="family"><string>EmojiSymbols</string></test>
+              <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+          </match>
+
+          <match target="pattern">
+              <test qual="any" name="family"><string>Symbola</string></test>
+              <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+          </match>
+        </fontconfig>
+      '';
+      penultimate.enable = false;
     };
-    fonts = with pkgs; [ hack-font font-awesome noto-fonts-cjk ];
+    fonts = with pkgs; [ hack-font font-awesome noto-fonts-cjk noto-fonts-emoji ];
   };
 }
