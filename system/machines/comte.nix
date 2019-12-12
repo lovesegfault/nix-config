248da73108eba4cd513d2a687c3f287a856c9729
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-11 17:44:38 -0800

    system/comte: add dumb user password

--- system/machines/comte.nix
+++ system/machines/comte.nix
@@ -9,5 +9,11 @@
 
   networking.hostName = "comte";
 
+  users.users.exclusivor = {
+    createHome = true;
+    isNormalUser = true;
+    hashedPassword = "$6$KpUi.HX9QiHUWD$ITJ42rfXV2PTR5fFeGcbBrevFNrzL1aSUHVqxBFd8350WqhjyzK4gWnUCaq3pDYZGNZiNLisZgxts1QvNQthN1";
+  };
+
   time.timeZone = "America/Los_Angeles";
 }
