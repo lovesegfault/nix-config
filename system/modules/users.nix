291842f784f40fb667dbcdb66c56d4905213e0a8
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-12 13:11:28 -0800

    system/users: add key to bemeurer

--- system/modules/users.nix
+++ system/modules/users.nix
@@ -4,5 +4,8 @@
     description = "Bernardo Meurer";
     extraGroups = [ "wheel" ];
     isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIQgTWfmR/Z4Szahx/uahdPqvEP/e/KQ1dKUYLenLuY2 bemeurer.personal"
+    ];
   };
 }
