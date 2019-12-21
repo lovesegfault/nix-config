65ab980133e35f442953f22eef716786902d93bf
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-20 23:51:31 -0800

    system/aarch64-build-box: add to knownHosts

--- system/modules/aarch64-build-box.nix
+++ system/modules/aarch64-build-box.nix
@@ -10,4 +10,8 @@
       supportedFeatures = [ "big-parallel" ];
     }];
   };
+
+  programs.ssh.knownHosts = [
+    { hostNames = [ "aarch64.nixos.community" "147.75.77.190" ]; publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUTz5i9u5H2FHNAmZJyoJfIGyUm/HfGhfwnc142L3ds"; }
+  ];
 }
