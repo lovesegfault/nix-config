b775b7753d4b25432e026ece229c7f71b9d8d3e5
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-22 02:27:39 -0800

    system/wayland: add colemickens cachix

--- system/combo/wayland.nix
+++ system/combo/wayland.nix
@@ -8,6 +8,11 @@ in {
 
   environment.systemPackages = [ pkgs.qt5.qtwayland ];
 
+  nix = {
+    binaryCaches = [ "https://nixpkgs-wayland.cachix.org/" ];
+    binaryCachePublicKeys = [ "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=" ];
+  };
+
   nixpkgs.overlays = [ waylandOverlay ];
 
   services.xserver = {
