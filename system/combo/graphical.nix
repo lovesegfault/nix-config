78e6f6a23e1b4e3feb0314af91bdec7ec439211c
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-27 19:45:04 -0800

    system/graphical: add qgnomeplatform

--- system/combo/graphical.nix
+++ system/combo/graphical.nix
@@ -1,11 +1,12 @@
-{
+{ pkgs, ... }:{
   imports = [
     ../modules/boot-silent.nix
     ../modules/fonts.nix
     ../modules/gdm.nix
-    ../modules/gnome.nix
     ../modules/gnome-keyring.nix
     ../modules/printing.nix
     ../modules/sound.nix
   ];
+
+  environment.systemPackages = with pkgs; [ qgnomeplatform ];
 }
--- system/modules/gnome.nix
+++
@@ -1,1 +0,0 @@
-{ pkgs, ... }: { environment.systemPackages = with pkgs; [ qgnomeplatform ]; }
