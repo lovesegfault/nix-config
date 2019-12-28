0307b75862c8386f60e87617894781060d51d282
Bernardo Meurer <meurerbernardo@gmail.com>
2019-12-27 19:42:27 -0800

    system/gdm: bare minimum

--- system/modules/gdm.nix
+++ system/modules/gdm.nix
@@ -1,24 +1,13 @@
 { pkgs, ... }: {
-  programs.dconf.enable = true;
+  environment.systemPackages = with pkgs.gnome3; [ adwaita-icon-theme ];
 
-  services = {
-    dbus.packages = [ pkgs.gnome3.dconf ];
-    geoclue2.enable = true;
-    gvfs.enable = true;
-    gnome3 = {
-      gnome-settings-daemon.enable = true;
-      gnome-online-accounts.enable = true;
-      gnome-online-miners.enable = true;
-      tracker.enable = true;
-      tracker-miners.enable = true;
-      core-shell.enable = true;
-    };
-    xserver = {
+  services.xserver = {
       enable = true;
       displayManager.gdm = {
         enable = true;
         autoSuspend = false;
       };
     };
-  };
+
+  systemd.packages = with pkgs.gnome3; [ gnome-session gnome-shell ];
 }
