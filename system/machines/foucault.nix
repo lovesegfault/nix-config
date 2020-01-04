d8d8659566b5b63b0c4226d92194b1aed4aa3c43
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-04 14:49:24 -0800

    system/foucault: no dhcpd, keybase

--- system/machines/foucault.nix
+++ system/machines/foucault.nix
@@ -6,8 +6,6 @@
     ../combo/wayland.nix
     ../combo/thinkpad-p1.nix
 
-    ../modules/keybase.nix
-
     ../modules/aarch64-build-box.nix
 
     ../modules/stcg-cachix.nix
@@ -22,14 +20,14 @@
 
   time.timeZone = "America/Los_Angeles";
 
-  services.dhcpd4 = {
-    enable = true;
-    extraConfig = ''
-      option subnet-mask 255.255.255.0;
-      subnet 10.1.2.0 netmask 255.255.255.0 {
-        range 10.1.2.0 10.1.2.10;
-      }
-    '';
-    interfaces = [ "enp0s31f6" ];
-  };
+  # services.dhcpd4 = {
+  #   enable = true;
+  #   extraConfig = ''
+  #     option subnet-mask 255.255.255.0;
+  #     subnet 10.1.2.0 netmask 255.255.255.0 {
+  #       range 10.1.2.0 10.1.2.10;
+  #     }
+  #   '';
+  #   interfaces = [ "enp0s31f6" ];
+  # };
 }
