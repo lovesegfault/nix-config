b59b8a45c323d360adf32d6ea01beb9a16a875eb
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-06 22:30:58 -0800

    home/neovim: deoplete tab & multiple-cursors

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -557,6 +557,20 @@
         " => deoplete
         """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
         let g:deoplete#enable_at_startup = 1
+        inoremap <silent><expr> <TAB>
+            \ pumvisible() ? "\<C-n>" :
+            \ <SID>check_back_space() ? "\<TAB>" :
+            \ deoplete#manual_complete()
+        function! s:check_back_space() abort "{{{
+        let col = col('.') - 1
+        return !col || getline('.')[col - 1]  =~ '\s'
+        endfunction"}}}
+       function g:Multiple_cursors_before()
+         call deoplete#custom#buffer_option('auto_complete', v:false)
+       endfunction
+       function g:Multiple_cursors_after()
+         call deoplete#custom#buffer_option('auto_complete', v:true)
+       endfunction
       '';
       languageClientConfig = ''
         """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
