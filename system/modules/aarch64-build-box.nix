26575caca132c7cfa4b290b9e76db2a84cf0bf0e
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-08 19:00:34 -0800

    system/aarch64-build-box: convert knownHosts from list to attrset

--- system/modules/aarch64-build-box.nix
+++ system/modules/aarch64-build-box.nix
@@ -11,9 +11,9 @@
     }];
   };
 
-  programs.ssh.knownHosts = [{
+  programs.ssh.knownHosts.aarch64-build-box = {
     hostNames = [ "aarch64.nixos.community" "147.75.77.190" ];
     publicKey =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUTz5i9u5H2FHNAmZJyoJfIGyUm/HfGhfwnc142L3ds";
-  }];
+  };
 }
