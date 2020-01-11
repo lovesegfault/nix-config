e6ff0b9b997764044ea75969f13509af748ac911
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-10 22:03:09 -0800

    home/ranger: only add ffmpegthumbnailer on Linux

--- home/modules/ranger.nix
+++ home/modules/ranger.nix
@@ -1,15 +1,15 @@
 { config, pkgs, ... }: {
-  home.packages = with pkgs; [
-    ranger
+  home.packages = with pkgs;
+    [
+      ranger
 
-    atool
-    lynx
-    exiftool
-    ffmpegthumbnailer
-    file
-    imagemagick
-    jq
-    mediainfo
-    pythonPackages.pygments
-  ];
+      atool
+      lynx
+      exiftool
+      file
+      imagemagick
+      jq
+      mediainfo
+      pythonPackages.pygments
+    ] ++ (if pkgs.stdenv.isLinux then [ pkgs.ffmpegthumbnailer ] else [ ]);
 }
