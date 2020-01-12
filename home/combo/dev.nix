6d0ea38f925f9ddf098247252ff0b206c73d4183
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-12 13:57:24 -0800

    home/dev: move lorri to system

--- home/combo/dev.nix
+++ home/combo/dev.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: {
   imports = [ ../modules/arcanist.nix ];
-  home.extraOutputsToInstall = [ "doc" "info" "devdoc" ];
+  home = {
+    extraOutputsToInstall = [ "doc" "info" "devdoc" ];
+    packages = with pkgs; [ lorri ];
+  };
   programs.direnv.enable = true;
-  services.lorri.enable = true;
 }
--- system/combo/dev.nix
+++ system/combo/dev.nix
@@ -3,4 +3,5 @@
     [ ../modules/documentation.nix ../modules/lxd.nix ../modules/qemu.nix ];
 
   environment.systemPackages = with pkgs; [ neovim ];
+  services.lorri.enable = true;
 }
