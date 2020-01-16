2663329b613ab8458bc8bfdb917f40267e2b0877
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-16 15:52:22 -0800

    home: add all machines; arch support

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,13 @@
 let
-  homePkg = machine:
-    (import <home-manager/home-manager/home-manager.nix> {
+  homePkg = machine: arch:
+  (import <home-manager/home-manager/home-manager.nix> {
+      pkgs = import <nixpkgs> { system = arch; };
       confPath = machine;
       confAttr = "";
     }).activationPackage;
-in { foucault = homePkg ./machines/foucault.nix; }
+in {
+  foucault = homePkg ./machines/foucault.nix "x86_64-linux";
+  bohr = homePkg ./machines/bohr.nix "aarch64-linux";
+  camus = homePkg ./machines/camus.nix "aarch64-linux";
+  spinoza = homePkg ./machines/spinoza.nix "x86_64-darwin";
+}
