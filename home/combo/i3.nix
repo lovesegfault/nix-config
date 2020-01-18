b03fd3d7cc017b86ade841ac5bacc24064a2a104
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-17 17:22:26 -0800

    home/i3status-rust: nuke

--- home/combo/i3.nix
+++ home/combo/i3.nix
@@ -3,7 +3,6 @@
     ../modules/dunst.nix
     ../modules/feh.nix
     ../modules/i3.nix
-    ../modules/i3status-rust.nix
     ../modules/polybar.nix
     ../modules/xresources.nix
     ../modules/xsession.nix
