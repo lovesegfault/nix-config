f9c894669ae11039c1f9bc8a8181ccf465b63e88
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-22 15:32:41 -0800

    system/foucault: sort

--- system/machines/foucault.nix
+++ system/machines/foucault.nix
@@ -22,13 +22,6 @@
       "/dev/disk/by-uuid/2d6ff3d0-cdfd-4b6e-a689-c43d21627279";
   };
 
-  networking = {
-    hostName = "foucault";
-    interfaces = { enp0s31f6.mtu = 9000; };
-  };
-
-  time.timeZone = "America/Los_Angeles";
-
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-uuid/4e217a4b-40ae-4bde-b771-04eabfe2369d";
@@ -40,6 +33,13 @@
     };
   };
 
+  networking = {
+    hostName = "foucault";
+    interfaces = { enp0s31f6.mtu = 9000; };
+  };
+
   swapDevices =
     [{ device = "/dev/disk/by-uuid/ec8c101f-65fd-47c4-8e17-f1b5395b68c7"; }];
+
+  time.timeZone = "America/Los_Angeles";
 }
