f7fbf207f18106e24292849c73833971f584df55
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-21 16:01:19 -0800

    system/stcg-cameras: open firewall further

--- system/modules/stcg-cameras.nix
+++ system/modules/stcg-cameras.nix
@@ -5,5 +5,9 @@
     "net.core.rmem_max" = 2147483647;
     "net.core.wmem_max" = 2147483647;
   };
-  networking.firewall.allowedUDPPorts = [ 3956 10010 ];
+  networking.firewall = {
+    allowedUDPPorts = [ 3956 10010 10020 ];
+    allowedTCPPorts = [ 3956 ];
+    allowedUDPPortRanges = [ { from = 32768; to=61000; } ];
+  };
 }
