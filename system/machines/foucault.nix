67f82b2fb2fa6caa14b0f9c7c30cf45aa1a22dcc
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-23 15:41:12 -0800

    system/foucault: enable gnome3

--- system/machines/foucault.nix
+++ system/machines/foucault.nix
@@ -38,6 +38,8 @@
     interfaces = { enp0s31f6.mtu = 9000; };
   };
 
+  services.xserver.desktopManager.gnome3.enable = true;
+
   swapDevices =
     [{ device = "/dev/disk/by-uuid/ec8c101f-65fd-47c4-8e17-f1b5395b68c7"; }];
 
