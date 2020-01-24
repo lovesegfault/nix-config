6b0db7f41f4cf03c841ce69ea0d57093f592a999
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-24 13:55:17 -0800

    home/{abel,peano}: register

--- home/default.nix
+++ home/default.nix
@@ -7,6 +7,8 @@ let
     }).activationPackage;
 in {
   foucault = homePkg ./machines/foucault.nix "x86_64-linux";
+  abel = homePkg ./machines/abel.nix "x86_64-linux";
+  peano = homePkg ./machines/peano.nix "x86_64-linux";
   bohr = homePkg ./machines/bohr.nix "aarch64-linux";
   camus = homePkg ./machines/camus.nix "aarch64-linux";
   spinoza = homePkg ./machines/spinoza.nix "x86_64-darwin";
