b57f52783ac7923682592d5511cbc76de6bcad57
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-24 12:07:43 -0800

    system/nvidia: barebones

--- system/modules/nvidia.nix
+++ system/modules/nvidia.nix
@@ -4,14 +4,5 @@
     extraModulePackages = with config.boot.kernelPackages; [ nvidia_x11 ];
   };
 
-  hardware = {
-    nvidia.optimus_prime = {
-      enable = true;
-      nvidiaBusId = "PCI:1:0:0";
-      intelBusId = "PCI:0:2:0";
-    };
-  };
-
   services.xserver.videoDrivers = [ "nvidia" ];
-  services.xserver.displayManager.gdm.wayland = false;
 }
