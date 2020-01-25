82d74b481c924d320cca67876b9920fac46a1b49
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-24 16:12:04 -0800

    system/abel: make tushar a normal user

--- system/machines/abel.nix
+++ system/machines/abel.nix
@@ -78,6 +78,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINH2jTl/COeeNZ6SXGsT0k/3fa1kgaSxgNGeg20s+OHV tushar@standard.ai"
       ];
+      isNormalUser = true;
     };
   };
 }
