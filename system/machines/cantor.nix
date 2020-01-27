8ccdba187477a4bf01cd181682d252959546b171
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-27 12:43:43 -0800

    system/cantor: add users to docker group

--- system/machines/cantor.nix
+++ system/machines/cantor.nix
@@ -32,10 +32,10 @@
   time.timeZone = "America/Los_Angeles";
 
   users.users = {
-    bemeurer.extraGroups = [ "video" ];
+    bemeurer.extraGroups = [ "docker" ];
     ogle = {
       createHome = true;
-      extraGroups = [ "video" "wheel" ];
+      extraGroups = [ "docker" "wheel" ];
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMbxQQ6asa917aD8HTavEinmIEsm6G3pZEOv7Rf33JCvvrdCW5ktOsjBm0oeRLt3aeC0QZa3nrMXixP7GCmJQWFPnAsQLlrpZnNRte5GB9X0wcUTUcvLo1kXzTBB5CRhSwdVQ9+/Ztc+LSiObPqFfsYY2pa85wYU6Q+Hu+aYSDrTvCzcL1ojEvUKnOmSWFYQ+fmYV7skKJL3Xr66zpWeCKyVtY8h7Ju3H3IWZTTl8Fyqtej63uHxqjQlMNzEjUL9Nzmev+O8+lCKvHXG+8dQBAYe3+tsIi1NKLSODSKxLpka52XIiNrgGnnr74YTZ8sp8Sd9STr3HUPr7uNK5I8DSL brandon@standard.ai"
@@ -44,7 +44,7 @@
 
     cloud = {
       createHome = true;
-      extraGroups = [ "video" "wheel" ];
+      extraGroups = [ "docker" "wheel" ];
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfPRiesz2VviTkRJAd3mzGRm2P+K67SutblfJ9I1+rU cloud@standard.ai"
