a3328472bf5b46e7f1b703cdc7b431bddb99c914
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-27 12:27:31 -0800

    system/dev: replace lxd with docker

--- system/combo/dev.nix
+++ system/combo/dev.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }: {
   imports =
-    [ ../modules/documentation.nix ../modules/lxd.nix ../modules/qemu.nix ];
+    [ ../modules/documentation.nix ../modules/qemu.nix ];
 
   environment.systemPackages = with pkgs; [ neovim ];
   services.lorri.enable = true;
+  virtualisation.docker.enable = true;
 }
