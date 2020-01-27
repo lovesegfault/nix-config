f8623d3e96880986bebe490257861d656ab4bbd6
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-27 11:10:24 -0800

    system/{abel,peano}: add ekleog

--- system/machines/abel.nix
+++ system/machines/abel.nix
@@ -93,5 +93,13 @@
       ];
       isNormalUser = true;
     };
+    ekleog = {
+      createHome = true;
+      extraGroups = [ "lxd" "wheel" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpQUN+FGyX80jjYX8HZv600c8jCSmhwnC/2mCsn+j/vnm5a3Tw8La7A2LocfKpocVU7cUkZNrPsXrLFOBu3jqM3VYqRlQw6ixgwonb3klpfeUu0kPg72Q6FQHMZO+yLIKwGWYDADFwxXk/KeuZK1VgAq1GGY4AdIB+PgtJ9/63rqtd0ooBX7rfKj+mMVuahS0/wxUhDufHBjv54Kk66JsUHd3gbW6EKa6bLfUQxRbJkxDycs7OvjUmUO0ndxdnqEYMaExbM+j0AFPVEc/7TW8SDL3dXtOEmIttB8nVcja5lpGyxpRJWiAxOBaosY76P5AEvfdt/JgRFdEhcr2F7YyR ekleog@llwynog"
+      ];
+      isNormalUser = true;
+    };
   };
 }
