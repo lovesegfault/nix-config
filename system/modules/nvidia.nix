4c50dfb061a7172a35e36c199ed6a1550a9faab9
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-27 12:26:56 -0800

    system/nvidia: add nvidia_x11 to systemPackages

--- system/modules/nvidia.nix
+++ system/modules/nvidia.nix
@@ -4,5 +4,7 @@
     extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
   };
 
+  environment.systemPackages = [ config.boot.kernelPackages.nvidia_x11 ];
+
   services.xserver.videoDrivers = [ "nvidia" ];
 }
