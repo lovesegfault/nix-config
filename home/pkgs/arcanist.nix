047e6774d107e28debf98fd8326683f07e5966e5
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-28 23:44:00 -0800

    all: fmt

--- home/pkgs/arcanist.nix
+++ home/pkgs/arcanist.nix
@@ -4,7 +4,8 @@
       arcanist = let
         permPatch = super.fetchpatch {
           name = "no-perm-check.patch";
-          url = "https://gist.githubusercontent.com/lovesegfault/32ddd009f41cbd785d21db1b3d95c810/raw/a5fe1f60f1eee5b5dd35865bce8b8e8cc5a59a2c/no-perm-check.patch";
+          url =
+            "https://gist.githubusercontent.com/lovesegfault/32ddd009f41cbd785d21db1b3d95c810/raw/a5fe1f60f1eee5b5dd35865bce8b8e8cc5a59a2c/no-perm-check.patch";
           sha256 = "151asgn7wnncr2rmaspmhghj0f19qhcwjsfm019jnfn1hl739h9s";
         };
         libphutil = super.fetchFromGitHub {
