2b2237ccdced920d7e05076babb790643b32989c
Bernardo Meurer <meurerbernardo@gmail.com>
2020-01-30 21:56:34 -0800

    home/arcanist: fix patch hash

--- home/pkgs/arcanist.nix
+++ home/pkgs/arcanist.nix
@@ -6,7 +6,7 @@
           name = "no-perm-check.patch";
           url =
             "https://gist.githubusercontent.com/lovesegfault/32ddd009f41cbd785d21db1b3d95c810/raw/a5fe1f60f1eee5b5dd35865bce8b8e8cc5a59a2c/no-perm-check.patch";
-          sha256 = "151asgn7wnncr2rmaspmhghj0f19qhcwjsfm019jnfn1hl739h9s";
+          sha256 = "0s6sc9pn6plfm5y5007986iwh9phrscvz049v5r3vb7z2s0f104i";
         };
         libphutil = super.fetchFromGitHub {
           owner = "phacility";
