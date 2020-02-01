e917be86e662f53e6d12425fa278ffd894cc3137
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-01 15:45:13 -0800

    home/arcanist: remove sources from arcanist pkg

--- home/pkgs/arcanist.nix
+++ home/pkgs/arcanist.nix
@@ -8,23 +8,7 @@
             "https://gist.githubusercontent.com/lovesegfault/32ddd009f41cbd785d21db1b3d95c810/raw/a5fe1f60f1eee5b5dd35865bce8b8e8cc5a59a2c/no-perm-check.patch";
           sha256 = "0s6sc9pn6plfm5y5007986iwh9phrscvz049v5r3vb7z2s0f104i";
         };
-        libphutil = super.fetchFromGitHub {
-          owner = "phacility";
-          repo = "libphutil";
-          rev = "cc2a3dbf590389400da55563cb6993f321ec6d73";
-          sha256 = "1k7sr3racwz845i7r5kdwvgqrz8gldz07pxj3yw77s58rqbix3ad";
-        };
-        arcanist = super.fetchFromGitHub {
-          owner = "phacility";
-          repo = "arcanist";
-          rev = "21a1828ea06cf031e93082db8664d73efc88290a";
-          sha256 = "05rq9l9z7446ks270viay57r5ibx702b5bnlf4ck529zc4abympx";
-        };
-      in (super.arcanist.overrideAttrs (old: {
-        src = [ arcanist libphutil ];
-        patches = [ permPatch ];
-        version = "20200127";
-      }));
+      in (super.arcanist.overrideAttrs (old: { patches = [ permPatch ]; }));
     })
   ];
 }
