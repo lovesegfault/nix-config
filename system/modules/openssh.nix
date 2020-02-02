ad1b4ec6b2c5ad5c2fa305270f7bea8034feaa23
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-01 22:52:42 -0800

    system/openssh: enable StreamLocalBindUnlink

--- system/modules/openssh.nix
+++ system/modules/openssh.nix
@@ -6,6 +6,10 @@ rec {
     challengeResponseAuthentication = false;
     permitRootLogin = "no";
     ports = [ 22 55888 ];
+    extraConfig = ''
+      # Specifies whether to remove an existing Unix-domain socket file for local or remote port forwarding before creating a new one
+      StreamLocalBindUnlink yes
+    '';
   };
 
   networking.firewall.allowedTCPPorts = services.openssh.ports;
