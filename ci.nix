430a6cbb195a1a812d0e464e714c86303127ac01
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-01 23:11:02 -0800

    ci: add darwin

--- ci.nix
+++ ci.nix
@@ -3,4 +3,5 @@
   machines = lib.zipAttrs [ system home ];
   x86_64 = with machines; [ abel cantor foucault peano ];
   aarch64 = with machines; [ bohr camus ];
+  darwin = with machines; [ spinoza ];
 }
