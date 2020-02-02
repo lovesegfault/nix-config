b83c0bd1528dfb177a8962901f705f31e5a56ea5
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-01 22:32:46 -0800

    all: fmt

--- ci.nix
+++ ci.nix
@@ -1,8 +1,6 @@
-{ lib ? import <nixpkgs/lib>,
-  pkgs ? import <nixpkgs>,
-  system ? import ./system,
-  home ? import ./home }: rec {
-    machines = lib.zipAttrs [ system home ];
-    x86_64 = with machines; [ abel cantor foucault peano ];
-    aarch64 = with machines; [ bohr camus ];
-  }
+{ lib ? import <nixpkgs/lib>, pkgs ? import <nixpkgs>, system ? import ./system
+, home ? import ./home }: rec {
+  machines = lib.zipAttrs [ system home ];
+  x86_64 = with machines; [ abel cantor foucault peano ];
+  aarch64 = with machines; [ bohr camus ];
+}
