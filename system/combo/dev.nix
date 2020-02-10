3453ee9e631f288dba81ba9220b3c9761385fbc4
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-09 17:02:25 -0800

    system/dev: add git

--- system/combo/dev.nix
+++ system/combo/dev.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   imports = [ ../modules/documentation.nix ../modules/qemu.nix ];
 
-  environment.systemPackages = with pkgs; [ neovim ];
+  environment.systemPackages = with pkgs; [ git neovim ];
   services.lorri.enable = true;
   virtualisation.docker.enable = true;
 }
