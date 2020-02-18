312c7a3df1d30ef6647dac093db361917c66ece6
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-18 14:34:00 -0800

    system/intel: enable GUC

--- system/modules/intel.nix
+++ system/modules/intel.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }: {
-  boot.kernelParams = [ "intel_iommu=on" ];
+  boot.kernelParams = [ "intel_iommu=on" "i915.enable_guc=2" ];
 
   hardware = {
     cpu.intel.updateMicrocode = true;
     opengl = {
       extraPackages = with pkgs; [
         vaapiIntel
-        libva
+        libva-full
+        libvdpau-va-gl
         intel-media-driver
         intel-compute-runtime
       ];
