bf994f29f3e5c3fadbaedfa3e086d53f988b95d4
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-27 14:08:18 -0800

    system/linux-5.5-fixes: update patches for 5.5.6

--- system/pkgs/linux-5.5-fixes.nix
+++ system/pkgs/linux-5.5-fixes.nix
@@ -5,15 +5,17 @@
         nouveau-gr-fix = {
           name = "nouveau-gr-fix";
           patch = (builtins.fetchurl {
-            url = "https://github.com/karolherbst/linux/commit/db333d73683578b5176a6f544750c62cbb5a57e0.patch";
-            sha256 = "1rrsvagvk5pc96ig6pg1b30w2fswj8vm2fhm53wqb52ksdrh8a2s";
+            url =
+              "https://github.com/karolherbst/linux/commit/0a4d0a9f2ab29b4765ee819753fbbcbc2aa7da97.patch";
+            sha256 = "0ir453x9k736hqxk0wklsxyxcsh72qvlndf74wgbry3fhb8ii1xi";
           });
         };
         nouveau-pci-fix = {
           name = "nouveau-pci-fix";
           patch = (builtins.fetchurl {
-            url = "https://github.com/karolherbst/linux/commit/b4c8166b1a5da40041ce899e143f30c54a79e9fe.patch";
-            sha256 = "16pxih1wqvps6f6ilrrd3k8iw3gcyjpskzwwk73y3ayamjnrwhv6";
+            url =
+              "https://github.com/karolherbst/linux/commit/cb048d65b71ac05158918fba2c68ca6896e51492.patch";
+            sha256 = "1785cq3lfx20zm5ngscan31aizlncyipvnxc0vlsxv9573cdajaj";
           });
         };
       }
