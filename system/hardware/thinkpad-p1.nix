0f68a29608ccaa6a5d314e2eb6966c239954773b
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-28 14:19:32 -0800

    treewide: fmt

--- system/hardware/thinkpad-p1.nix
+++ system/hardware/thinkpad-p1.nix
@@ -16,7 +16,7 @@
     kernelModules = [ "acpi_call" "kvm-intel" ];
     kernelPackages = pkgs.linuxPackages_latest;
     kernelPatches = with pkgs; [ nouveau-gr-fix nouveau-pci-fix ];
-    kernelParams = [ "log_buf_len=5M" "psmouse.synaptics_intertouch=1"];
+    kernelParams = [ "log_buf_len=5M" "psmouse.synaptics_intertouch=1" ];
   };
 
   environment.systemPackages = with pkgs; [ powertop ];
