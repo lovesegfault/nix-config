d0d4fb0fb0ada42ef86434020fc5b6b34748ca60
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-28 14:13:36 -0800

    home/sway: add input for synaptics intertouch

--- home/modules/sway.nix
+++ home/modules/sway.nix
@@ -83,6 +83,15 @@
             tap enabled
         }
 
+        input "1739:0:Synaptics_TM3418-002" {
+            accel_profile adaptive
+            click_method button_areas
+            dwt disabled
+            natural_scroll enabled
+            scroll_method two_finger
+            tap enabled
+        }
+
         input "1739:31251:SYNA2393:00_06CB:7A13_Touchpad" {
             accel_profile adaptive
             click_method button_areas
