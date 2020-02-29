35ac2846b7c42b18a974f2e389c997c08f69904e
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-28 21:22:07 -0800

    system/tlp: re-enable bt, wifi on AC

--- system/modules/tlp.nix
+++ system/modules/tlp.nix
@@ -8,6 +8,7 @@
       CPU_SCALING_GOVERNOR_ON_AC=performance
 
       DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE="bluetooth wifi"
+      DEVICES_TO_ENABLE_ON_AC="bluetooth wifi"
 
       DISK_DEVICES="nvme0n1 sda"
       DISK_IOSCHED="mq-deadline bfq"
