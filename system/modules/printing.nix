ceb7d4c1804197d80654b14bceeb354e5c6aa8a2
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-28 19:34:43 -0800

    system/printing: remove nssmdns

--- system/modules/printing.nix
+++ system/modules/printing.nix
@@ -1,8 +1,4 @@
 { pkgs, ... }: {
-  services.avahi = {
-    enable = true;
-    nssmdns = true;
-  };
   services.printing = {
     enable = true;
     drivers = with pkgs; [ gutenprint cups-googlecloudprint canon-cups-ufr2 ];
