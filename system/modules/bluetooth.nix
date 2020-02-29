16d5f32c5a6ae9a7ab0669c47ddecb026c9b191f
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-28 22:17:14 -0800

    system/bluetooh: clean up

--- system/modules/bluetooth.nix
+++ system/modules/bluetooth.nix
@@ -1,17 +1,25 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: rec {
   hardware.bluetooth = {
     enable = true;
     package = pkgs.bluezFull;
     powerOnBoot = true;
     config = {
       General = {
-        Enable = "Source,Sink,Media,Socket";
-        Disable = "Headset";
-        Autoconnect = true;
+        FastConnectable = "true";
+        JustWorksRepairing = "always";
         MultiProfile = "multiple";
       };
     };
   };
+
+  # XXX: Due to how .serviceConfig is an option and .ExecStart is technically
+  # a list, you will add multiple ExecStart='s to the .service file, and this
+  # not a nixos option, you cant use things like mkForce on it. If you set it to
+  # one. The empty one is a signal to systemd, to ignore the original copy
+  # will cause it to have 3 entries, the original, an empty, and the new
+  systemd.services.bluetooth.serviceConfig.ExecStart = let
+    bluez = "${hardware.bluetooth.package}/libexec/bluetooth/bluetoothd";
+  in [ "" "${bluez} --noplugin=sap" ];
 
   hardware.pulseaudio = {
     package = pkgs.pulseaudio.override { bluetoothSupport = true; };
