26f991be0d225130704f4a5d307b84a77e12f903
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-28 21:10:43 -0800

    system/tlp: disable bluetooth/wifi when not in use on battery

--- system/modules/tlp.nix
+++ system/modules/tlp.nix
@@ -7,6 +7,8 @@
       CPU_MAX_PERF_ON_BAT=50
       CPU_SCALING_GOVERNOR_ON_AC=performance
 
+      DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE="bluetooth wifi"
+
       DISK_DEVICES="nvme0n1 sda"
       DISK_IOSCHED="mq-deadline bfq"
 
