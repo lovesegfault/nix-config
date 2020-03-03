b199a819c1e25deeef37f4fb48f2cb589a50b2ca
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-03 13:50:09 -0800

    system/foucault: jettison gnome3

--- system/machines/foucault.nix
+++ system/machines/foucault.nix
@@ -35,7 +35,6 @@
   };
 
   services.avahi.enable = false;
-  services.xserver.desktopManager.gnome3.enable = true;
 
   swapDevices =
     [ { device = "/dev/disk/by-uuid/ec8c101f-65fd-47c4-8e17-f1b5395b68c7"; } ];
