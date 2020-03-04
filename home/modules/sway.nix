32c587a76137bf2aaddb54e733acdb50845d0de1
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-04 11:14:48 -0800

    all/sway: set WLR_DRM_DEVICES

--- home/modules/sway.nix
+++ home/modules/sway.nix
@@ -218,15 +218,16 @@
     };
 
     extraSessionCommands = ''
-      export QT_QPA_PLATFORM=wayland
-      export QT_WAYLAND_FORCE_DPI=physical
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-      export _JAVA_AWT_WM_NONREPARENTING=1
-      export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true"
       export ECORE_EVAS_ENGINE=wayland_egl
       export ELM_ENGINE=wayland_egl
-      export SDL_VIDEODRIVER=wayland
       export MOZ_ENABLE_WAYLAND=1
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+      export QT_WAYLAND_FORCE_DPI=physical
+      export SDL_VIDEODRIVER=wayland
+      export WLR_DRM_DEVICES=/dev/dri/card0:/dev/dri/card1 sway
+      export _JAVA_AWT_WM_NONREPARENTING=1
+      export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true"
     '';
 
     systemdIntegration = true;
