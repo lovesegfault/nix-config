c5c36273427cca5d82a07daf23af950f37926210
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-04 14:58:23 -0800

    system/nvidia: force-disable modesetting

--- system/modules/nvidia.nix
+++ system/modules/nvidia.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ config, lib, pkgs, ... }: {
   boot = {
     blacklistedKernelModules = [ "nouveau" ];
     extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
@@ -6,6 +6,7 @@
 
   environment.systemPackages = [ config.boot.kernelPackages.nvidia_x11 ];
 
+  hardware.nvidia.modesetting = lib.mkForce false;
   hardware.opengl.driSupport32Bit = true;
 
   services.xserver.videoDrivers = [ "nvidia" ];
