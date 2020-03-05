0124c52a4453202784076be065a2e5e54ac7a3cb
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-04 19:43:51 -0800

    treewide: fmt

--- system/pkgs/linux-5.5-fixes.nix
+++ system/pkgs/linux-5.5-fixes.nix
@@ -24,10 +24,12 @@
         };
         xfs-2038-fix = {
           name = "xfs-2038-fix";
-          patch = (builtins.fetchurl {
-            url = "https://lkml.org/lkml/diff/2019/12/26/349/1";
-            sha256 = "1jzxncv97w3ns60nk91b9b0a11bp1axng370qhv4fs7ik01yfsa4";
-          });
+          patch = (
+            builtins.fetchurl {
+              url = "https://lkml.org/lkml/diff/2019/12/26/349/1";
+              sha256 = "1jzxncv97w3ns60nk91b9b0a11bp1axng370qhv4fs7ik01yfsa4";
+            }
+          );
         };
       }
     )
