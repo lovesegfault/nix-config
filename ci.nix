bfa41fd6740348ecf9095ad46dc2ecec54fa6780
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-05 15:59:32 -0800

    ci: nuke special nix file

--- ci.nix
+++
@@ -1,8 +0,0 @@
-rec {
-  machines = import ./. {};
-  x86_64 = with machines; [ abel cantor foucault peano ];
-  aarch64 = with machines; [ bohr camus ];
-  darwin = with machines; [ spinoza ];
-
-  ci = [ x86_64 aarch64 ];
-}
--- default.nix
+++ default.nix
@@ -1,6 +1,14 @@
 { sources ? import ./nix/sources.nix {}
 , home-manager ? import (sources.home-manager + "/home-manager/home-manager.nix")
 , pkgs ? import sources.nixpkgs {}
-, system ? import ./system { inherit pkgs; }
-, home ? import ./home { inherit home-manager pkgs; }
-}: pkgs.lib.zipAttrs [ system home ]
+}: let
+  system = import ./system { inherit pkgs; };
+  home = import ./home { inherit home-manager pkgs; };
+  machines = pkgs.lib.zipAttrs [ system home ];
+in rec {
+  x86_64 = with machines; [ abel cantor foucault peano ];
+  aarch64 = with machines; [ bohr camus ];
+  darwin = with machines; [ spinoza ];
+
+  ci = [ x86_64 aarch64 ];
+} // machines
