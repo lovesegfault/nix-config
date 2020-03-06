e151cf23e00db9e0d68c839fea70a675e69f1221
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-05 18:33:14 -0800

    system/hardware/thinkpad-p1: use fetchpatch

--- system/hardware/thinkpad-p1.nix
+++ system/hardware/thinkpad-p1.nix
@@ -5,7 +5,6 @@
     ../modules/fwupd.nix
     ../modules/intel.nix
     ../modules/tlp.nix
-    ../pkgs/linux-5.5-fixes.nix
   ];
 
   boot = rec {
@@ -14,10 +13,19 @@
     extraModulePackages = with kernelPackages; [ acpi_call ];
     kernelModules = [ "acpi_call" "kvm-intel" ];
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelPatches = with pkgs; [
-      nouveau-gr-fix
-      nouveau-runpm-fix
-      xfs-2038-fix
+    kernelPatches =  [
+      (pkgs.fetchpatch {
+        url = "https://github.com/karolherbst/linux/commit/0a4d0a9f2ab29b4765ee819753fbbcbc2aa7da97.patch";
+        sha256 = "1k4lf1cnydckjn2fqdqiizba3rzjg27xa97xjaif4ss5m7mh4ckn";
+      })
+      (pkgs.fetchpatch {
+        url = "https://github.com/karolherbst/linux/commit/1e6cef9e6c4d17f6d893dae3cd7d442d8574b4b5.patch";
+        sha256 = "103myhwmi55f7vaxk9yqrl4diql6z32am5mzd6kvk89j9m02h528";
+      })
+      (pkgs.fetchpatch {
+        url = "https://lkml.org/lkml/diff/2019/12/26/349/1";
+        sha256 = "1jzxncv97w3ns60nk91b9b0a11bp1axng370qhv4fs7ik01yfsa4";
+      })
     ];
     kernelParams = [
       "log_buf_len=5M"
