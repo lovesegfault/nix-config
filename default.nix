182f7dd346e90e2c03882e1ca0e71b914f87fa90
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-05 16:50:59 -0800

    ci: also build the shell

--- default.nix
+++ default.nix
@@ -6,11 +6,33 @@ let
   system = import ./system { inherit pkgs; };
   home = import ./home { inherit home-manager pkgs; };
   machines = pkgs.lib.zipAttrs [ system home ];
+  shellHack = let
+    drv = import ./shell.nix {};
+    nixConfig = import <nix/config.nix>;
+  in derivation (
+    drv.drvAttrs // {
+      name = "shell-hack-${drv.name}";
+      origBuilder = drv.builder;
+      builder = builtins.storePath nixConfig.shell;
+      origSystem = drv.system;
+      system = builtins.currentSystem;
+      origPATH = drv.PATH or "";
+      PATH = builtins.storePath nixConfig.coreutils;
+      origArgs = drv.args;
+      args = [
+        (
+          builtins.toFile "keep-env-hack" ''
+            env > $out
+          ''
+        )
+      ];
+    }
+  );
 in
 rec {
   x86_64 = with machines; [ abel cantor foucault peano ];
   aarch64 = with machines; [ bohr camus ];
   darwin = with machines; [ spinoza ];
 
-  ci = [ x86_64 aarch64 ];
+  ci = [ x86_64 aarch64 shellHack ];
 } // machines
