eb8161c29b8038078bcc8d40cf07318415e5373f
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-05 16:02:42 -0800

    treewide: fmt

--- default.nix
+++ default.nix
@@ -1,11 +1,13 @@
 { sources ? import ./nix/sources.nix {}
 , home-manager ? import (sources.home-manager + "/home-manager/home-manager.nix")
 , pkgs ? import sources.nixpkgs {}
-}: let
+}:
+let
   system = import ./system { inherit pkgs; };
   home = import ./home { inherit home-manager pkgs; };
   machines = pkgs.lib.zipAttrs [ system home ];
-in rec {
+in
+rec {
   x86_64 = with machines; [ abel cantor foucault peano ];
   aarch64 = with machines; [ bohr camus ];
   darwin = with machines; [ spinoza ];
