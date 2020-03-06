5c233d286b69d470056dd03b4d49f69be2ea7ae8
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-05 18:17:08 -0800

    default: re-introduce system, home attrs

--- default.nix
+++ default.nix
@@ -30,6 +30,7 @@ let
   );
 in
 rec {
+  inherit system home;
   x86_64 = with machines; [ abel cantor foucault peano ];
   aarch64 = with machines; [ bohr camus ];
   darwin = with machines; [ spinoza ];
