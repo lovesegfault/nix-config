2931ff9eeb4e7d5a9d304dab4ed283f745ea3070
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-07 20:20:14 -0800

    treewide: fmt

--- users/default.nix
+++ users/default.nix
@@ -1,11 +1,13 @@
 { lib, pkgs, ... }: with builtins;
 let
   userDirs = attrNames (lib.filterAttrs (_: v: v == "directory") (readDir ./.));
-  mkUser = u: let
+  mkUser = u:
+    let
       mod = import (./. + "/${u}") { inherit lib pkgs; };
-  in {
-    "users.users.${u}" = mod.system;
-    "home-manager.users.${u}" = mod.home;
-  };
+    in {
+      "users.users.${u}" = mod.system;
+      "home-manager.users.${u}" = mod.home;
+    };
   users = (lib.genAttrs userDirs mkUser);
-in users
+in
+users
