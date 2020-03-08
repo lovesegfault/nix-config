c5b2ae6b9593a612380fb518b449960fe2521ebc
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-08 00:08:42 -0800

    users/template: init

---
+++ users/template/default.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }: {
+  users.users.template = {
+    createHome = true;
+    description = "I am a template";
+    extraGroups = [ "wheel" ];
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 00000000000000000000000000000000000000000000000000000000000000000000 yo soy un template"
+    ];
+  };
+  home-manager.users.bemeurer = {
+    home.packages = with pkgs; [ hello ];
+  };
+}
