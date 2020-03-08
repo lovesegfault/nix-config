95a76fdaf5ea618a9607c024dbdf70ced5d47502
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-07 22:19:59 -0800

    treewide: fmt

--- users/bemeurer/default.nix
+++ users/bemeurer/default.nix
@@ -8,12 +8,14 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIQgTWfmR/Z4Szahx/uahdPqvEP/e/KQ1dKUYLenLuY2 bemeurer.personal"
     ];
   };
-  home-manager.users.bemeurer = lib.mkMerge ([
-    (import ./core { inherit pkgs; })
-    (import ./dev)
-  ] ++ lib.optionals config.programs.sway.enable [
-    (import ./gpg)
-    (import ./sway)
-    (import ./music)
-  ]);
+  home-manager.users.bemeurer = lib.mkMerge (
+    [
+      (import ./core { inherit pkgs; })
+      (import ./dev)
+    ] ++ lib.optionals config.programs.sway.enable [
+      (import ./gpg)
+      (import ./sway)
+      (import ./music)
+    ]
+  );
 }
