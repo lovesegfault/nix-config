73f446552fb5ff7c095273c2ec5022f9438eb5b4
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-07 20:43:50 -0800

    treewide: fmt

--- users/default.nix
+++ users/default.nix
@@ -7,7 +7,8 @@ let
   mkUserPath = u: (./. + "/${u}");
   userAttrs = lib.genAttrs userDirs mkUserPath;
   userSingletons = lib.mapAttrs (_: v: (lib.singleton v)) userAttrs;
-in {
+in
+{
   ops = with userAttrs; [ bemeurer ];
   dev = with userAttrs; [ bemeurer ];
 } // userSingletons
