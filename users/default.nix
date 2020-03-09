eeb7b6a0b9be9a4a43df01102d1775c985061365
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-09 14:01:30 -0700

    users: nuke mkUserPath

--- users/default.nix
+++ users/default.nix
@@ -2,7 +2,6 @@ with builtins;
 let
   lib = import ../nix/lib.nix;
   userDirs = attrNames (lib.filterAttrs (_: v: v == "directory") (readDir ./.));
-  mkUserPath = u: (./. + "/${u}");
   mkUser = u: import (./. + "/${u}");
   users = lib.genAttrs userDirs mkUser;
 in
