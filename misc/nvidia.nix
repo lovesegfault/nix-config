78f9dc590f8aaf1f892988aca1a10079e83d1610
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-08 22:19:01 -0700

    misc/nvidia: proper openCL support

--- misc/nvidia.nix
+++ misc/nvidia.nix
@@ -1,15 +1,31 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }: let
+  nvidia_x11 = config.boot.kernelPackages.nvidia_x11;
+  nvidia_gl = nvidia_x11.out;
+  nvidia_gl_32 = nvidia_x11.lib32;
+in {
   boot = {
     blacklistedKernelModules = [ "nouveau" ];
-    extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+    extraModulePackages = [ nvidia_x11 ];
   };
 
-  environment.systemPackages = [ config.boot.kernelPackages.nvidia_x11 ];
+  environment.systemPackages = [ pkgs.cudatoolkit nvidia_x11 ];
 
-  hardware.nvidia.modesetting.enable = lib.mkForce false;
-  hardware.opengl.driSupport32Bit = true;
+  hardware = {
+    nvidia.modesetting.enable = lib.mkForce false;
+    opengl = {
+      enable = true;
+      driSupport32Bit = true;
+      extraPackages = [ nvidia_gl ];
+      extraPackages32 = [ nvidia_gl_32 ];
+    };
+  };
 
   services.xserver.videoDrivers = [ "nvidia" ];
+
+  systemd.services.nvidia-control-devices = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.ExecStart = "${nvidia_x11.bin}/bin/nvidia-smi";
+  };
 
   virtualisation.docker.enableNvidia = true;
 }
