3ecadea9140a2eaa835c3158bc783f85f8f34622
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-09 14:53:04 -0700

    hardware/thinkpad-p1: incorporate tlp

--- hardware/thinkpad-p1.nix
+++ hardware/thinkpad-p1.nix
@@ -4,7 +4,6 @@
     ../misc/bluetooth.nix
     ../misc/fwupd.nix
     ../misc/intel.nix
-    ../misc/tlp.nix
   ];
 
   boot = rec {
@@ -70,6 +69,36 @@
   services = {
     fstrim.enable = true;
     hardware.bolt.enable = true;
+    tlp = {
+      enable = true;
+      extraConfig = ''
+        CPU_ENERGY_PERF_POLICY_ON_AC=performance
+        CPU_ENERGY_PERF_POLICY_ON_BAT=power
+        CPU_MAX_PERF_ON_AC=100
+        CPU_MAX_PERF_ON_BAT=50
+        CPU_SCALING_GOVERNOR_ON_AC=performance
+        CPU_SCALING_GOVERNOR_ON_BAT=powersave
+
+        DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE="bluetooth wifi"
+        DEVICES_TO_ENABLE_ON_AC="bluetooth wifi"
+
+        DISK_APM_LEVEL_ON_AC="255 254"
+        DISK_APM_LEVEL_ON_BAT="128 1"
+        DISK_DEVICES="nvme0n1 sda"
+        DISK_IOSCHED="mq-deadline bfq"
+
+        MAX_LOST_WORK_SECS_ON_AC=15
+        MAX_LOST_WORK_SECS_ON_BAT=15
+
+        RUNTIME_PM_DRIVER_BLACKLIST="amdgpu nouveau nvidia radeon"
+
+        SOUND_POWER_SAVE_ON_AC=1
+        SOUND_POWER_SAVE_ON_BAT=1
+        SOUND_POWER_SAVE_CONTROLLER=Y
+
+        USB_WHITELIST="1050:0407 056a:5193"
+      '';
+    };
     xserver = {
       dpi = 96;
       libinput = {
