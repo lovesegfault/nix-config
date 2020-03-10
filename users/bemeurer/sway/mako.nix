53f0b693dbc00b20303c75d1f70b55b0aab54db3
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-10 15:43:58 -0700

    users/bemeurer: add pixmaps to mako

--- users/bemeurer/sway/mako.nix
+++ users/bemeurer/sway/mako.nix
@@ -2,14 +2,16 @@
   xdg.configFile.mako = {
     target = "mako/config";
     text = let
-      systemIcons = "/run/current-system/sw/share/icons/hicolor";
       homeIcons = "${config.home.homeDirectory}/.nix-profile/share/icons/hicolor";
+      homePixmaps = "${config.home.homeDirectory}/.nix-profile/share/pixmaps";
+      systemIcons = "/run/current-system/sw/share/icons/hicolor";
+      systemPixmaps = "/run/current-system/sw/share/pixmaps";
     in ''
       background-color=#0a0e14
       border-color=#53bdfa
       default-timeout=30000
       font=Hack 10
-      icon-path=${homeIcons}:${systemIcons}
+      icon-path=${homeIcons}:${systemIcons}:${homePixmaps}:${systemPixmaps}
       icons=1
       max-icon-size=32
       max-visible=3
