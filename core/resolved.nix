0de0fd7074e8d462f6d8433d5530376ddfdb751d
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-10 19:53:48 -0700

    core/resolved: rework config

--- core/resolved.nix
+++ core/resolved.nix
@@ -3,13 +3,12 @@
 
   services.resolved = {
     enable = true;
-    dnssec = "false";
+    dnssec = "allow-downgrade";
+    domains = [ "meurer.org" ];
+    fallbackDns = [ "8.8.8.8" "2001:4860:4860::8844" ];
     llmnr = "true";
     extraConfig = ''
-      DNS=1.1.1.1 8.8.8.8 1.0.0.1 8.8.4.4 2606:4700:4700::1111 2001:4860:4860::8888 2606:4700:4700::1001 2001:4860:4860::8844
-      Cache=yes
-      DNSStubListener=yes
-      ReadEtcHosts=yes
+      DNS=1.1.1.1 2606:4700:4700::1111
     '';
   };
 }
