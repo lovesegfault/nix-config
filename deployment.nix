98a610bb42985ce78f74c230842ca033041c8acf
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-11 01:31:35 -0700

    deployment: source hosts from default.nix

--- deployment.nix
+++ deployment.nix
@@ -1,13 +1,16 @@
+{ lib ? import ./nix/lib.nix }:
+with lib;
 let
-  mkSystem = cfg: system:
-    let
+  hosts = (import ./.).hosts;
+  mkSystem = name: arch: { ... }: let
+      system = arch;
       pkgs = import ./nix/nixpkgs.nix { inherit system; config.allowUnfree = true; };
-    in
-      { lib, ... }: {
-        imports = [ cfg ];
-        nixpkgs.pkgs = lib.mkForce pkgs;
-        deployment.substituteOnDestination = true;
-      };
+  in { ... }: {
+    imports = [ (./systems + "/${name}") ];
+    nixpkgs.pkgs = pkgs;
+    deployment.substituteOnDestination = true;
+  };
+  systems = mapAttrs mkSystem hosts;
 in
 {
   network = {
@@ -15,12 +18,12 @@ in
   };
 
   # Personal
-  "home.meurer.org" = mkSystem ./systems/bohr.nix "aarch64-linux";
-  "irc.meurer.org" = mkSystem ./systems/sartre.nix "x86_64-linux";
-  "localhost" = mkSystem ./systems/foucault.nix "x86_64-linux";
+  "home.meurer.org" = systems.bohr;
+  "irc.meurer.org" = systems.sartre;
+  "localhost" = systems.foucault;
 
   # Sc
-  "10.0.5.158" = mkSystem ./systems/cantor.nix "x86_64-linux";
-  "10.1.16.11" = mkSystem ./systems/peano.nix "x86_64-linux";
-  "10.1.16.12" = mkSystem ./systems/abel.nix "x86_64-linux";
+  "10.0.5.158" = systems.cantor;
+  "10.1.16.11" = systems.peano;
+  "10.1.16.12" = systems.abel;
 }
