9d634d6853e3efd6d936a3d029e548cbb0ec3a42
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-10 23:32:26 -0700

    default: cleanup

--- default.nix
+++ default.nix
@@ -1,32 +1,32 @@
+{ lib ? import ./nix/lib.nix }:
+with builtins; with lib;
 let
-  pkgs = import ./nix/nixpkgs.nix {};
-
-  mkGceImage = configuration: system:
+  mkNixOS = name: arch:
     let
-      nixos = import ./nix/nixos.nix;
-      eval = (nixos { inherit configuration system; });
-    in eval.config.system.build.googleComputeImage;
-  mkSystem = configuration: system:
-    let
-      nixos = import ./nix/nixos.nix;
-      eval = (nixos { inherit configuration system; });
-    in eval.system;
+      configuration = ./systems + "/${name}.nix";
+      system = arch;
+      nixos = import ./nix/nixos.nix { inherit configuration system; };
+    in nixos.config.system.build;
 
-  systems = {
-    abel = mkSystem ./systems/abel.nix "x86_64-linux";
-    bohr = mkSystem ./systems/bohr.nix "aarch64-linux";
-    camus = mkSystem ./systems/camus.nix "aarch64-linux";
-    cantor = mkSystem ./systems/cantor.nix "x86_64-linux";
-    foucault = mkSystem ./systems/foucault.nix "x86_64-linux";
-    peano = mkSystem ./systems/peano.nix "x86_64-linux";
+  mkSystem = name: arch: (mkNixOS name arch).toplevel;
+  mkGceImage = name: arch: (mkNixOS name arch).googleComputeImage;
+
+  systems = mapAttrs mkSystem {
+    abel = "x86_64-linux";
+    bohr = "aarch64-linux";
+    camus = "aarch64-linux";
+    cantor = "x86_64-linux";
+    foucault = "x86_64-linux";
+    peano = "x86_64-linux";
+    sartre = "x86_64-linux";
   };
 
-  gceImages = {
-    sartre = mkGceImage ./systems/sartre.nix "x86_64-linux";
+  gceImages = mapAttrs mkGceImage {
+    sartre = "x86_64-linux";
   };
 in
 {
-  inherit pkgs;
+  inherit gceImages;
   x86_64 = with systems; [ abel cantor foucault peano ];
   aarch64 = with systems; [ bohr camus ];
-} // systems // gceImages
+} // systems
