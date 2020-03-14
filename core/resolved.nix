ca7bb9a2da50c1c7e37224d6029b1c495afd1421
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-13 19:32:20 -0700

    core/resolved: don't force DNS, add fallbacks instead

--- core/resolved.nix
+++ core/resolved.nix
@@ -5,10 +5,7 @@
     enable = true;
     dnssec = "allow-downgrade";
     domains = [ "meurer.org" ];
-    fallbackDns = [ "8.8.8.8" "2001:4860:4860::8844" ];
+    fallbackDns = [ "1.1.1.1" "2606:4700:4700::1111" "8.8.8.8" "2001:4860:4860::8844" ];
     llmnr = "true";
-    extraConfig = ''
-      DNS=1.1.1.1 2606:4700:4700::1111
-    '';
   };
 }
