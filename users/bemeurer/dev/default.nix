935f38ffd8828976df2e013a7ea8c879b3476c7a
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-14 21:55:55 -0700

    users/bemeurer: add gdbinit

--- users/bemeurer/dev/default.nix
+++ users/bemeurer/dev/default.nix
@@ -5,5 +5,13 @@
     extraOutputsToInstall = [ "doc" "info" "devdoc" ];
     packages = with pkgs; [ lorri ];
   };
+
+  home.file.gdbinit = {
+    target = ".gdbinit";
+    text = ''
+      set auto-load safe-path /
+    '';
+  };
+
   programs.direnv.enable = true;
 }
