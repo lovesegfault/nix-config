ae3f9069d735d9343c68f67fe88f38aec9579186
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-16 22:37:39 -0700

    systems/aquinas: add dyndns

--- systems/aquinas.nix
+++ systems/aquinas.nix
@@ -26,6 +26,21 @@
     hostName = "aquinas";
   };
 
+  services.ddclient =
+    let
+      secretPath = ../secrets/ddclient-aquinas.nix;
+      secretCondition = (builtins.pathExists secretPath);
+      secret = lib.optionalAttrs secretCondition (import secretPath);
+    in
+      (
+        lib.recursiveUpdate {
+          enable = true;
+          ssl = true;
+          protocol = "googledomains";
+          domains = [ "aquinas.meurer.org" ];
+        } secret
+      );
+
   swapDevices =
     [ { device = "/dev/disk/by-uuid/57cf59f5-7ad6-49bc-b5b0-21796be1e617"; } ];
 
