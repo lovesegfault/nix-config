dd232cd63f3ea154656a2acd8300a541bf9c1a32
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-16 23:20:36 -0700

    systems/sartre: add ddclient

--- systems/sartre.nix
+++ systems/sartre.nix
@@ -12,5 +12,20 @@
     hostId = "7ecc3d2a";
   };
 
+  services.ddclient =
+    let
+      secretPath = ../secrets/ddclient-sartre.nix;
+      secretCondition = (builtins.pathExists secretPath);
+      secret = lib.optionalAttrs secretCondition (import secretPath);
+    in
+      (
+        lib.recursiveUpdate {
+          enable = true;
+          ssl = true;
+          protocol = "googledomains";
+          domains = [ "sartre.meurer.org" ];
+        } secret
+      );
+
   time.timeZone = "America/Los_Angeles";
 }
