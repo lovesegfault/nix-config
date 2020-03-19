83c1383914799bcfec2420a8248e42e701757ef7
Bernardo Meurer <meurerbernardo@gmail.com>
2020-03-19 12:24:13 -0700

    overlays/ffmpeg: also enable opengl for ffmpeg_3

--- overlays/ffmpeg.nix
+++ overlays/ffmpeg.nix
@@ -1,3 +1,4 @@
 self: super: {
+  ffmpeg_3 = super.ffmpeg_3.override { openglSupport = true; libmfxSupport = true; };
   ffmpeg_4 = super.ffmpeg_4.override { openglSupport = true; libmfxSupport = true; };
 }
