7fcbcebde37dfc96d150c695390992eb3028e67d
Bernardo Meurer <meurerbernardo@gmail.com>
2020-04-03 08:47:44 -0700

    hardware/thinkpad-p1: move back to latest kernel

--- hardware/thinkpad-p1.nix
+++ hardware/thinkpad-p1.nix
@@ -11,7 +11,7 @@
       [ "xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
     extraModulePackages = with kernelPackages; [ ddcci-driver ];
     kernelModules = [ "kvm-intel" ];
-    kernelPackages = pkgs.linuxPackages_5_5;
+    kernelPackages = pkgs.linuxPackages_latest;
     kernelPatches = [
       {
         name = "xfs-2038-fix";
