2de2cc38d59fb9bc4a72db80ce3275cee3da7b01
Bernardo Meurer <meurerbernardo@gmail.com>
2020-04-04 13:23:05 -0700

    core/resolved: don't set domains

--- core/resolved.nix
+++ core/resolved.nix
@@ -4,7 +4,6 @@
   services.resolved = {
     enable = true;
     dnssec = "allow-downgrade";
-    domains = [ "meurer.org" ];
     fallbackDns = [ "1.1.1.1" "2606:4700:4700::1111" "8.8.8.8" "2001:4860:4860::8844" ];
     llmnr = "true";
   };
