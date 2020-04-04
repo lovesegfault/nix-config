1d256929aa08fa17f2aacc8a8723723f7779ce04
Bernardo Meurer <meurerbernardo@gmail.com>
2020-04-04 15:36:28 -0700

    nix: change mkSystem callsite

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@ let
   mkSystem = name: arch: (mkNixOS name arch).toplevel;
   mkGceImage = name: arch: (mkNixOS name arch).googleComputeImage;
 
-  systemAttrs = mapAttrs mkSystem (import ./hosts.nix);
+  systemAttrs = (mapAttrs mkSystem (import ./hosts.nix));
   filterSystems = arch: attrValues (filterAttrs (_:v: v == arch) systemAttrs);
   x86_64Systems = filterSystems "x86_64-linux";
   aarch64Systems = filterSystems "aarch64-linux";
