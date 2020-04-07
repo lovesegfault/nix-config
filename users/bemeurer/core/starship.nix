44a45b5e25f3ec5a098bb0edf038ee63a58394a2
Bernardo Meurer <meurerbernardo@gmail.com>
2020-04-06 23:03:24 -0700

    users/bemeurer: explicit prompts in starship

--- users/bemeurer/core/starship.nix
+++ users/bemeurer/core/starship.nix
@@ -5,9 +5,25 @@
     target = "starship.toml";
     text = ''
       add_newline = false
-
-      [aws]
-      disabled = true
+      prompt_order = [
+        "username",
+        "hostname",
+        "directory",
+        "git_branch",
+        "git_commit",
+        "git_state",
+        "git_status",
+        "hg_branch",
+        "package",
+        "haskell",
+        "julia",
+        "python",
+        "rust",
+        "nix_shell",
+        "line_break",
+        "jobs",
+        "character",
+      ];
 
       [nix_shell]
       use_name = true
