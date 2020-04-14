98f2dd8c3a1cf9a53aee77fdaad41c8b6db9fbf4
Bernardo Meurer <meurerbernardo@gmail.com>
2020-04-13 21:49:20 -0700

    core/unbound: fix config

--- core/unbound.nix
+++ core/unbound.nix
@@ -8,12 +8,13 @@
     extraConfig = ''
       # This is part of the server clause
       infra-cache-slabs: 16
+      ip-ratelimit-slabs: 16
       key-cache-slabs: 16
       msg-cache-size: 50m
       msg-cache-slabs: 16
       num-threads: 12
       outgoing-range: 950
-      ratelimit-labs: 16
+      ratelimit-slabs: 16
       rrset-cache-size: 100m
       rrset-cache-slabs: 16
       so-reuseport: yes
