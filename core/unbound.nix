3fd72450cd64dd63ffffeba9c56e6943b537a133
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-07 14:37:39 -0700

    core/unbound: no more remote-control

--- core/unbound.nix
+++ core/unbound.nix
@@ -18,24 +18,6 @@
         rrset-cache-size: 100m
         rrset-cache-slabs: 16
         so-reuseport: yes
-
-      remote-control:
-        # Enable remote control with unbound-control(8) here.
-        # set up the keys and certificates with unbound-control-setup.
-        control-enable: yes
-        # what interfaces are listened to for remote control.
-        # give 0.0.0.0 and ::0 to listen to all interfaces.
-        control-interface: 127.0.0.1
-        # port number for remote control operations.
-        control-port: 8953
-        # unbound server key file.
-        server-key-file: "/var/lib/unbound/unbound_server.key"
-        # unbound server certificate file.
-        server-cert-file: "/var/lib/unbound/unbound_server.pem"
-        # unbound-control key file.
-        control-key-file: "/var/lib/unbound/unbound_control.key"
-        # unbound-control certificate file.
-        control-cert-file: "/var/lib/unbound/unbound_control.pem"
     '';
   };
 }
