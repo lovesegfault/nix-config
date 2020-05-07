1565b732258154f6b420f2b84fad478e5cb8b84a
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-07 13:44:03 -0700

    hardware/no-mitigations: init

---
+++ hardware/no-mitigations.nix
@@ -0,0 +1,16 @@
+{
+  boot.kernelParams = [
+    "l1tf=off"
+    "mds=off"
+    "mitigations=off"
+    "no_stf_barrier"
+    "noibpb"
+    "noibrs"
+    "nopti"
+    "nospec_store_bypass_disable"
+    "nospectre_v1"
+    "nospectre_v2"
+    "tsx=on"
+    "tsx_async_abort=off"
+  ];
+}
--- hardware/stcg-dc.nix
+++ hardware/stcg-dc.nix
@@ -1,24 +1,12 @@
 { lib, pkgs, ... }: {
 
-  imports = [ ./efi.nix ./nvidia.nix ];
+  imports = [ ./efi.nix ./no-mitigations.nix ./nvidia.nix ];
 
   boot = rec {
     initrd.availableKernelModules =
       [ "ahci" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
     kernelModules = [ "kvm-intel" ];
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelParams = [
-      "l1tf=off"
-      "mds=off"
-      "mitigations=off"
-      "no_stf_barrier"
-      "noibpb"
-      "noibrs"
-      "nopti"
-      "nospec_store_bypass_disable"
-      "nospectre_v1"
-      "nospectre_v2"
-    ];
   };
 
   environment.systemPackages = with pkgs; [ fahclient ];
