d6c6347a630c7efdb2553694f660e03670f02b7f
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-11 22:27:37 -0700

    overlays/ffmpeg: unify overrides

--- overlays/ffmpeg.nix
+++ overlays/ffmpeg.nix
@@ -1,4 +1,9 @@
-self: super: {
-  ffmpeg_3 = super.ffmpeg_3.override { openglSupport = true; libmfxSupport = true; };
-  ffmpeg_4 = super.ffmpeg_4.override { openglSupport = true; libmfxSupport = true; };
+self: super: let
+  ffmpegOverrides = {
+    libmfxSupport = true;
+    openglSupport = true;
+  };
+in {
+  ffmpeg_3 = super.ffmpeg_3.override ffmpegOverrides;
+  ffmpeg_4 = super.ffmpeg_4.override ffmpegOverrides;
 }
