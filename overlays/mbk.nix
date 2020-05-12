59e5bb303fd3462263bf7f841b8fcf01151ab056
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-11 21:10:57 -0700

    overlays/mbk: fmt

--- overlays/mbk.nix
+++ overlays/mbk.nix
@@ -1,36 +1,38 @@
 self: super: {
-  mbk = let
-    gsutil = "${self.google-cloud-sdk}/bin/gsutil";
-  in self.writeScriptBin "mbk" ''
-    #!${self.stdenv.shell}
+  mbk =
+    let
+      gsutil = "${self.google-cloud-sdk}/bin/gsutil";
+    in
+    self.writeScriptBin "mbk" ''
+      #!${self.stdenv.shell}
 
-    DOCUMENTS_BUCKET="gs://documents.meurer.org"
-    MISC_BUCKET="gs://misc.meurer.org"
-    MUSIC_BUCKET="gs://music.meurer.org"
-    PICTURES_BUCKET="gs://pictures.meurer.org"
+      DOCUMENTS_BUCKET="gs://documents.meurer.org"
+      MISC_BUCKET="gs://misc.meurer.org"
+      MUSIC_BUCKET="gs://music.meurer.org"
+      PICTURES_BUCKET="gs://pictures.meurer.org"
 
-    function backup() {
-        local src="$1"
-        local dst="$2"
+      function backup() {
+          local src="$1"
+          local dst="$2"
 
-        if [ -z ''${DEBUG+x} ]; then
-            ${gsutil} -m rsync -d -r "$src" "$dst"
-        else
-            ${gsutil} -m rsync -d -r -n "$src" "$dst"
-        fi
-    }
+          if [ -z ''${DEBUG+x} ]; then
+              ${gsutil} -m rsync -d -r "$src" "$dst"
+          else
+              ${gsutil} -m rsync -d -r -n "$src" "$dst"
+          fi
+      }
 
-    function main() {
-        # main dirs
-        backup "$HOME/documents" "$DOCUMENTS_BUCKET"
-        backup "$HOME/music" "$MUSIC_BUCKET"
-        backup "$HOME/pictures" "$PICTURES_BUCKET"
-        # weirder state dirs
-        backup "$HOME/.local/share/shotwell" "$MISC_BUCKET/shotwell"
-        backup "$HOME/.local/share/lollypop" "$MISC_BUCKET/lollypop"
-        backup "$HOME/.local/share/zsh_history" "$MISC_BUCKET"
-    }
+      function main() {
+          # main dirs
+          backup "$HOME/documents" "$DOCUMENTS_BUCKET"
+          backup "$HOME/music" "$MUSIC_BUCKET"
+          backup "$HOME/pictures" "$PICTURES_BUCKET"
+          # weirder state dirs
+          backup "$HOME/.local/share/shotwell" "$MISC_BUCKET/shotwell"
+          backup "$HOME/.local/share/lollypop" "$MISC_BUCKET/lollypop"
+          backup "$HOME/.local/share/zsh_history" "$MISC_BUCKET"
+      }
 
-    main
-  '';
+      main
+    '';
 }
