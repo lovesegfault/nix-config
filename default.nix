63153f6726abc0f36ed7531afd8e0ba06f59f1c1
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-18 18:01:09 -0700

    systems/goethe: init

--- default.nix
+++ default.nix
@@ -10,7 +10,9 @@ let
     nixos.config.system.build;
   mkSystem = name: arch: (mkNixOS name arch).toplevel;
   mkGceImage = name: arch: (mkNixOS name arch).googleComputeImage;
+
   systemAttrs = (mapAttrs mkSystem (import ./hosts.nix));
+
   filterSystems = arch: attrValues (filterAttrs (_:v: v.system == arch) systemAttrs);
   x86_64Systems = filterSystems "x86_64-linux";
   aarch64Systems = filterSystems "aarch64-linux";
---
+++ systems/goethe.nix
@@ -0,0 +1,54 @@
+{ lib, pkgs, ... }:
+{
+  imports = [
+    (import ../users).bemeurer
+    ../core
+
+    ../hardware/rpi4.nix
+  ];
+
+  console = {
+    font = "ter-v28n";
+    packages = with pkgs; [ terminus_font ];
+  };
+
+  environment.noXlibs = true;
+
+  networking = {
+    hostName = "goethe";
+    interfaces.eth0 = {
+      ipv4.addresses = [{
+          address = "192.168.2.1";
+          prefixLength = 24;
+      }];
+      useDHCP = lib.mkForce false;
+    };
+    wireless = {
+      iwd.enable = true;
+      networks."Tabachnik".psk = let
+          secretPath = ../secrets/wifi-tabachnik.nix;
+          secretCondition = (builtins.pathExists secretPath);
+          secret = lib.optionalString secretCondition (import secretPath);
+        in secret;
+    };
+  };
+
+  services.dhcpd4 = {
+    enable = true;
+    extraConfig = ''
+      subnet 192.168.2.0 netmask 255.255.255.0 {
+        authoritative;
+        option subnet-mask 255.255.255.0;
+        range 192.168.2.10 192.168.2.254;
+
+        #host camus {
+        #  hardware ethernet dc:a6:32:63:47:40;
+        #  fixed-address 192.168.2.1;
+        #}
+      }
+    '';
+    interfaces = [ "eth0" ];
+  };
+
+  time.timeZone = "America/Los_Angeles";
+}
