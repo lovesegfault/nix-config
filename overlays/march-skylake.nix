ed9bedb14008469705762250b70ae71488a071bc
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-26 23:01:48 -0700

    overlays/march-skylake: fix xen

--- overlays/march-skylake.nix
+++ overlays/march-skylake.nix
@@ -4,4 +4,8 @@ self: super: {
     x509-validation = super.x509-validation.overrideAttrs (oldAttrs: { doCheck = false; });
     tls = super.tls.overrideAttrs (oldAttrs: { doCheck = false; });
   });
+
+  xen = super.xen.overrideAttrs (oldAttrs: {
+    NIX_CFLAGS_COMPILE = (oldAttrs.NIX_CFLAGS_COMPILE or "") + " -march=x86-64";
+  });
 }
