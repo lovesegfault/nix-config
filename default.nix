70c1edcb89221f0b3650a9bb4bf2584288ff85b5
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-30 15:45:48 -0700

    default: nixus is the new normal

--- default.nix
+++ default.nix
@@ -1,19 +1,56 @@
-{ sources ? import ./nix, lib ? import sources.lib, pkgs ? import sources.nixpkgs { } }:
-with builtins; with lib;
+{ pkgs ? null }:
 let
-  mkNixOS = configuration:
-    let
-      nixos = import (pkgs.path + "/nixos") { inherit configuration; };
-    in
-    nixos.config.system.build;
-  mkSystem = name: (mkNixOS name).toplevel;
-  mkGceImage = name: (mkNixOS name).googleComputeImage;
-
-  # list of names of all my systems defined in ./systems
-  systems = map (n: removeSuffix ".nix" n) (attrNames (readDir ./systems));
-  # attrset of form { hostname = systemDrv; ... }
-  systemAttrs = genAttrs systems (n: mkSystem (./systems + "/${n}.nix"));
+  sources = import ./nix;
+  nixus = import sources.nixus;
+  nixpkgs = if pkgs == null then sources.nixpkgs else pkgs;
 in
-systemAttrs // {
-  systems = pkgs.linkFarmFromDrvs "systems" (attrValues systemAttrs);
-}
+nixus ({ ... }: {
+  defaults = { ... }: { inherit nixpkgs; };
+  nodes = {
+    # Personal
+    aquinas = { ... }: {
+      enabled = false;
+      host = "aquinas.meurer.org";
+      configuration = ./systems/aquinas.nix;
+    };
+    bohr = { ... }: {
+      hasFastConnection = true;
+      host = "bohr.meurer.org";
+      configuration = ./systems/bohr.nix;
+    };
+    foucault = { ... }: {
+      host = "localhost";
+      configuration = ./systems/foucault.nix;
+    };
+    goethe = { ... }: {
+      host = "goethe.meurer.org";
+      configuration = ./systems/goethe.nix;
+    };
+    sartre = { ... }: {
+      host = "sartre.meurer.org";
+      configuration = ./systems/sartre.nix;
+    };
+
+    # Work
+    cantor = { ... }: {
+      host = "10.0.5.217";
+      configuration = ./systems/cantor.nix;
+    };
+    abel = { ... }: {
+      host = "10.1.16.12";
+      configuration = ./systems/abel.nix;
+    };
+    peano = { ... }: {
+      host = "10.1.16.11";
+      configuration = ./systems/peano.nix;
+    };
+    feuerbach = { ... }: {
+      host = "10.1.5.211";
+      configuration = ./systems/feuerbach.nix;
+    };
+    hegel = { ... }: {
+      host = "147.75.47.54";
+      configuration = ./systems/hegel.nix;
+    };
+  };
+})
