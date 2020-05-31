5c1505ee2405b1592db8672993e25967c9f84da0
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-30 22:21:43 -0700

    overlays/mkSecret: fmt

--- overlays/mkSecret.nix
+++ overlays/mkSecret.nix
@@ -1,8 +1,10 @@
 self: super:
 with builtins; with self.lib;
 {
-  mkSecret = path: let
-    name = baseNameOf (toString path);
-    stub = toFile name "This is a stub!\n";
-  in if pathExists path then path else self.lib.warn "Using stub for secrets/${name}" stub;
+  mkSecret = path:
+    let
+      name = baseNameOf (toString path);
+      stub = toFile name "This is a stub!\n";
+    in
+    if pathExists path then path else self.lib.warn "Using stub for secrets/${name}" stub;
 }
