aa3d2f1ba28a3eab267c8579e6e6d9b037d0c336
Bernardo Meurer <meurerbernardo@gmail.com>
2020-06-02 12:53:07 -0700

    shell: move scripts out of buildInputs decl

--- shell.nix
+++ shell.nix
@@ -1,28 +1,35 @@
 let
-  sources = import ./nix;
-  pkgs = import sources.nixpkgs { };
+  pkgs = import <nixpkgs> { };
+  deploy = pkgs.writeScriptBin "deploy" ''
+    #!${pkgs.stdenv.shell}
+    set -o pipefail
+    set -o xtrace
+
+    function deploy() {
+      local cmd=("nix-build")
+      if [ $# -gt 0 ]; then
+        cmd+=("-A" "$1")
+      fi
+      "''${cmd[@]}" | ${pkgs.stdenv.shell}
+    }
+
+    deploy "$@"
+  '';
+  genci = pkgs.writeScriptBin "genci" ''
+    #!${pkgs.stdenv.shell}
+    set -o pipefail
+    set -o xtrace
+
+    nix-build ci.nix | ${pkgs.stdenv.shell}
+  '';
 in
 pkgs.mkShell {
   name = "nix-config";
   buildInputs = with pkgs; [
-    (writeScriptBin "deploy" ''
-      #!${pkgs.stdenv.shell}
-      set -o pipefail
-      set -o xtrace
-
-      function deploy() {
-        local cmd=("nix-build")
-        if [ $# -gt 0 ]; then
-          cmd+=("-A" "$1")
-        fi
-        "''${cmd[@]}" | ${pkgs.stdenv.shell}
-      }
-
-      deploy "$@"
-    ''
-    )
     (cachix.overrideAttrs (oldAttrs: { doCheck = false; }))
     niv
     nixpkgs-fmt
+    deploy
+    genci
   ];
 }
