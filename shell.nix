7548ba55ecec181513bbc8becf9a2eccaa729130
Bernardo Meurer <meurerbernardo@gmail.com>
2020-06-02 12:04:14 -0700

    shell: add tiny deploy wrapper

--- shell.nix
+++ shell.nix
@@ -5,6 +5,21 @@ in
 pkgs.mkShell {
   name = "nix-config";
   buildInputs = with pkgs; [
+    (writeScriptBin "deploy" ''
+      #!${pkgs.stdenv.shell}
+      set -o pipefail
+      set -o xtrace
+
+      function deploy() {
+        local cmd=("nix-build")
+        if [ $# -gt 0 ]; then
+          cmd+=("-A" "$1")
+        fi
+        "''${cmd[@]}" | ${pkgs.stdenv.shell}
+      }
+
+      deploy "$@"
+    '')
     (cachix.overrideAttrs (oldAttrs: { doCheck = false; }))
     niv
     nixpkgs-fmt
