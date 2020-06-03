8d29f12fe809ab3ffef3eab49e408bfa1ffaa216
Bernardo Meurer <meurerbernardo@gmail.com>
2020-06-03 10:40:48 -0700

    systems/bohr: re-enable networkd

--- systems/bohr.nix
+++ systems/bohr.nix
@@ -8,16 +8,17 @@
 
   environment.noXlibs = true;
 
-  networking = {
-    hostName = "bohr";
-    useNetworkd = lib.mkForce false;
-    interfaces.eth0.useDHCP = true;
-  };
+  networking.hostName = "bohr";
 
   secrets.ddclient-bohr.file = pkgs.mkSecret ../secrets/ddclient-bohr.conf;
   services.ddclient.configFile = config.secrets.ddclient-bohr;
 
   services.openssh.ports = [ 22 55889 ];
+
+  systemd.network.networks.eth0 = {
+    DHCP = "yes";
+    matchConfig.MACAddress = "b8:27:eb:2d:4c:18";
+  };
 
   time.timeZone = "America/Los_Angeles";
 }
