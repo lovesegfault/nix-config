651d4e6fd32a1256bb2a5f238c722b66d1378c93
Bernardo Meurer <meurerbernardo@gmail.com>
2020-06-03 17:51:31 -0700

    sway/sound: try to fix choppy audio

--- sway/sound.nix
+++ sway/sound.nix
@@ -3,12 +3,11 @@
     enable = true;
     daemon.config = {
       avoid-resampling = "yes";
-      alternate-sample-rate = 88200;
       default-fragment-size-msec = 125;
       default-fragments = 2;
       default-sample-channels = 2;
       default-sample-format = "s32le";
-      default-sample-rate = 96000;
+      default-sample-rate = 48000;
       enable-lfe-remixing = "no";
       realtime-scheduling = "yes";
       resample-method = "speex-float-10";
