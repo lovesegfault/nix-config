5425518da444b9762ed5945ef4eadac8c39ecd2d
Bernardo Meurer <meurerbernardo@gmail.com>
2020-06-04 23:50:59 -0700

    overlays/mkSecret: fix stubs

--- overlays/mkSecret.nix
+++ overlays/mkSecret.nix
@@ -4,12 +4,8 @@ with builtins; with self.lib;
   mkSecret = { file, ... }@args:
     let
       fileName = baseNameOf (toString file);
-      stub = toFile fileName "This is a stub!";
-      file =
-        if pathExists args.file then
-          args.file
-        else
-          self.lib.warn "Using stub for secrets/${fileName}" stub;
+      stub = self.lib.warn "Using stub for secrets/${fileName}" (toFile fileName "stub");
+      fileOrStub = if pathExists file then file else stub;
     in
-    args // { inherit file; };
+    args // { file = fileOrStub; };
 }
