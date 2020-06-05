c9181ab84faa0f18666305e9e152c9638d020760
Bernardo Meurer <meurerbernardo@gmail.com>
2020-06-04 23:38:59 -0700

    treewide: use secrets for system passwords

--- users/bemeurer/default.nix
+++ users/bemeurer/default.nix
@@ -16,7 +16,7 @@ with lib;
     ];
     shell = mkIf config.programs.zsh.enable pkgs.zsh;
     uid = 8888;
-    hashedPassword = "$6$rounds=65536$iIIt7MZ7K0ghK$HMPPLFtp7SpvpLAajlgZp.sH2rCNsOq41E1CDCGCaxyz/tXSqWngalatM0V5zsMbj/4klKdAzeoOw1rZj7fp6/";
+    passwordFile = config.secrets.files.bemeurer-password.file.outPath;
   };
 
   secrets.files.stcg-arcanist-config = pkgs.mkSecret {
