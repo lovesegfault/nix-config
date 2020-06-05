bdb2e5923f1d257897c93b61379edac847b15c50
Bernardo Meurer <meurerbernardo@gmail.com>
2020-06-05 12:39:54 -0700

    hardware/thinkpad-p1: more sysctl tuning

--- dev/stcg-cameras.nix
+++ dev/stcg-cameras.nix
@@ -1,7 +1,7 @@
 {
   boot.kernel.sysctl = {
-    "net.core.rmem_default" = 262144;
-    "net.core.wmem_default" = 262144;
+    "net.core.rmem_default" = 31457280;
+    "net.core.wmem_default" = 31457280;
     "net.core.rmem_max" = 2147483647;
     "net.core.wmem_max" = 2147483647;
   };
