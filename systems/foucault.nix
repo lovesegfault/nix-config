c7c8a1aa0897ec18e9296ce8c1d00f360ca7aaed
Bernardo Meurer <meurerbernardo@gmail.com>
2020-06-09 15:00:49 -0700

    systems/foucault: don't point openssh to /state as we bind mount to etc

--- systems/foucault.nix
+++ systems/foucault.nix
@@ -121,10 +121,6 @@
   ];
 
   services.keybase.enable = false;
-  services.openssh.hostKeys = [
-    { path = "/state/etc/ssh/ssh_host_ed25519_key"; type = "ed25519"; }
-    { path = "/state/etc/ssh/ssh_host_rsa_key"; type = "rsa"; bits = 4096; }
-  ];
 
   systemd.network = {
     links.enp0s31f6 = {
