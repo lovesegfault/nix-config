9010bd85f2b6e8737116443cef69e9418a24c511
Bernardo Meurer <meurerbernardo@gmail.com>
2020-06-09 19:46:47 -0700

    hardware/yubikey: fmt

--- hardware/yubikey.nix
+++ hardware/yubikey.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
   hardware.u2f.enable = true;
-  services.udev.packages = with pkgs; [ yubikey-personalization libu2f-host];
+  services.udev.packages = with pkgs; [ yubikey-personalization libu2f-host ];
   services.pcscd.enable = true;
 }
