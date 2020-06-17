f8f5684f196d5d83ee04b95d9deb8e4bec9853bc
Bernardo Meurer <bernardo@meurer.org>
2020-06-16 19:04:57 -0700

    overlays/weechat: fix pycrypto

--- overlays/weechat.nix
+++ overlays/weechat.nix
@@ -17,21 +17,26 @@ self: super:
                           name = "pycrypto-${version}";
                           version = "2.6.1";
 
-                          src = super.fetchurl {
+                          src = self.fetchurl {
                             url = "mirror://pypi/p/pycrypto/${name}.tar.gz";
                             sha256 = "0g0ayql5b9mkjam8hym6zyg6bv77lbh66rv1fyvgqb17kfc1xkpj";
                           };
 
-                          patches = super.stdenv.lib.singleton (
-                            super.fetchpatch {
+                          patches = [
+                            (self.fetchpatch {
                               name = "CVE-2013-7459.patch";
                               url = "https://anonscm.debian.org/cgit/collab-maint/python-crypto.git"
                                 + "/plain/debian/patches/CVE-2013-7459.patch?h=debian/2.6.1-7";
                               sha256 = "01r7aghnchc1bpxgdv58qyi2085gh34bxini973xhy3ks7fq3ir9";
-                            }
-                          );
+                            })
+                            (self.fetchpatch {
+                              name = "process_time.patch";
+                              url = "https://patch-diff.githubusercontent.com/raw/pycrypto/pycrypto/pull/307.patch";
+                              sha256 = "0000000000000000000000000000000000000000000000000000";
+                            })
+                          ];
 
-                          buildInputs = [ super.gmp ];
+                          buildInputs = [ self.gmp ];
 
                           preConfigure = ''
                             sed -i 's,/usr/include,/no-such-dir,' configure
