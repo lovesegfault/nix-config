c318068ac92843673af33ad06d4dde64a6308b64
Bernardo Meurer <bernardo@meurer.org>
2020-07-19 12:40:11 -0700

    sway/sound: fiddle with resampling

--- sway/sound.nix
+++ sway/sound.nix
@@ -2,18 +2,19 @@
   hardware.pulseaudio = {
     enable = true;
     daemon.config = {
+      alternate-sample-rate = 96000;
       avoid-resampling = "yes";
       default-fragment-size-msec = 125;
       default-fragments = 2;
       default-sample-channels = 2;
-      default-sample-format = "s32le";
-      default-sample-rate = 48000;
+      default-sample-format = "float32le";
+      default-sample-rate = 88200;
       enable-lfe-remixing = "no";
       high-priority = "yes";
       nice-level = -20;
       realtime-priority = 5;
       realtime-scheduling = "yes";
-      resample-method = "speex-float-10";
+      resample-method = "soxr-vhq";
       rlimit-rtprio = 5;
     };
     extraConfig = ''
