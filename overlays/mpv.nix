48167aabc3a1c359fec57d232bdc2d4b83f12b0d
Bernardo Meurer <bernardo@meurer.org>
2020-07-19 21:42:29 -0700

    overlays/mpv: fix rev

--- overlays/mpv.nix
+++ overlays/mpv.nix
@@ -3,7 +3,7 @@ self: super: {
     src = self.fetchFromGitHub {
       repo = "mpv";
       owner = "mpv-player";
-      rev = "70b991749df389bcc0a4e145b5687233a03b4ed7";
+      rev = "c9742413ac5eeabfdd46503f67b7393c9bd99f49";
       sha256 = "0000000000000000000000000000000000000000000000000000000000000000";
     };
   });
