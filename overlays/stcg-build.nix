957ffcc8a1bdf4456b82edad7a1172e52c083bdd
Bernardo Meurer <bernardo@meurer.org>
2020-09-23 19:45:13 -0700

    overlays/stcg-build: log build

--- overlays/stcg-build.nix
+++ overlays/stcg-build.nix
@@ -7,7 +7,7 @@ self: super: {
 
     export DRV=$(nix-instantiate "$@")
     nix-copy-closure -s --include-outputs --to stcg-us-0005-03 "$DRV"
-    ssh stcg-us-0005-03 "nix build $DRV"
+    ssh stcg-us-0005-03 "nix build -L $DRV"
 
     out=$(nix-store --query --binding out $DRV)
     nix-copy-closure -s --include-outputs --from stcg-us-0005-03 "$out"
