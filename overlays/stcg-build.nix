57e1bb1515b416f2bbfade2163607f5bc234e7c9
Bernardo Meurer <bernardo@meurer.org>
2020-09-30 11:59:19 -0700

    overlays/stcg-build: fix addr

--- overlays/stcg-build.nix
+++ overlays/stcg-build.nix
@@ -6,10 +6,10 @@ self: super: {
     set -o pipefail
 
     export DRV=$(nix-instantiate "$@")
-    nix-copy-closure -s --include-outputs --to stcg-us-0005-03 "$DRV"
+    nix-copy-closure -s --include-outputs --to stcg-us-0005-11 "$DRV"
     ssh stcg-us-0005-03 "nix build -L $DRV"
 
     out=$(nix-store --query --binding out $DRV)
-    nix-copy-closure -s --include-outputs --from stcg-us-0005-03 "$out"
+    nix-copy-closure -s --include-outputs --from stcg-us-0005-11 "$out"
   '';
 }
