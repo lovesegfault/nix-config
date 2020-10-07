179e6f2a5da624a1093c71261235013e4f7ce727
Bernardo Meurer <bernardo@meurer.org>
2020-10-07 00:03:37 -0700

    nix: use local nixpkgs

--- nix/default.nix
+++ nix/default.nix
@@ -8,7 +8,7 @@ rec {
   impermanence-sys = sources.impermanence + "/nixos.nix";
 
   gitignoreSource = (import sources.gitignore { inherit lib; }).gitignoreSource;
-  lib = import (sources.nixpkgs + "/lib");
+  lib = import (nixpkgs + "/lib");
   musnix = import sources.musnix;
   nix-pre-commit-hooks = import sources.nix-pre-commit-hooks;
   nixus = import sources.nixus { };
