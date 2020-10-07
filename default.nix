35365e99aef5d02cf4b8cf67c96df10f784efd1b
Bernardo Meurer <bernardo@meurer.org>
2020-10-06 21:17:43 -0700

    systems/{cantor, feuerbach}: nuke

--- default.nix
+++ default.nix
@@ -17,41 +17,30 @@ in
   deploy = sources.nixus {
     defaults = { ... }: { nixpkgs = sources.nixpkgs; };
     nodes = {
-      # Personal
       foucault = { ... }: {
         host = "localhost";
         configuration = ./systems/foucault.nix;
       };
+
       fourier = { ... }: {
-        hasFastConnection = true;
         host = "10.0.0.3";
         configuration = ./systems/fourier.nix;
       };
+
       goethe = { ... }: {
         hasFastConnection = true;
         host = "192.168.2.1";
         configuration = ./systems/goethe.nix;
       };
+
       aurelius = { ... }: {
-        ignoreFailingSystemdUnits = true;
-        hasFastConnection = true;
         host = "10.0.0.13";
         configuration = ./systems/aurelius.nix;
       };
+
       sartre = { ... }: {
         host = "sartre.meurer.org";
         configuration = ./systems/sartre.nix;
-      };
-
-      # Work
-      cantor = { ... }: {
-        host = "stcg-us-0005-03";
-        configuration = ./systems/cantor.nix;
-      };
-      feuerbach = { ... }: {
-        enabled = false;
-        host = "stcg-us-0005-11";
-        configuration = ./systems/feuerbach.nix;
       };
     };
   };
