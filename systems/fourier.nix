43d45e643e8c64dad4eccc701f091edb22b5c029
Bernardo Meurer <bernardo@meurer.org>
2020-10-14 01:23:43 -0700

    systems/fourier: use latest kernel

--- systems/fourier.nix
+++ systems/fourier.nix
@@ -19,6 +19,7 @@
     initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "sd_mod" ];
     extraModulePackages = with config.boot.kernelPackages; [ r8125 ];
     kernelModules = [ "kvm-amd" "r8125" ];
+    kernelPackages = pkgs.linuxPackages_latest;
     zfs = {
       extraPools = [ "tank" ];
       requestEncryptionCredentials = false;
