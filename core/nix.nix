8222949630f8a4fd2d2d74e59bf59e2527c6a9dd
Bernardo Meurer <bernardo@meurer.org>
2020-10-19 20:46:20 -0700

    core/nix: back to stable Nix

--- core/nix.nix
+++ core/nix.nix
@@ -1,18 +1,18 @@
-{ config, pkgs, ... }: {
+{ config, ... }: {
   nix = {
     allowedUsers = [ "@wheel" ];
     daemonIONiceLevel = 5;
     daemonNiceLevel = 10;
     extraOptions = ''
       builders-use-substitutes = true
-      experimental-features = nix-command flakes
+      # experimental-features = nix-command flakes
     '';
     nrBuildUsers = config.nix.maxJobs * 2;
     optimise = {
       automatic = true;
       dates = [ "01:10" "12:10" ];
     };
-    package = pkgs.nixUnstable;
+    # package = pkgs.nixUnstable;
     trustedUsers = [ "root" "@wheel" ];
   };
 }
