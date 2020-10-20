2418a8db0fcd50589773fb6730f67d3da67d85fd
Bernardo Meurer <bernardo@meurer.org>
2020-10-20 02:33:19 -0700

    core/resolved: disable llmnr

--- core/resolved.nix
+++ core/resolved.nix
@@ -4,6 +4,7 @@
   services.resolved = {
     enable = true;
     dnssec = "false";
+    llmnr = "false";
     extraConfig = ''
       DNS=1.1.1.1 2606:4700:4700::1111 8.8.8.8 2001:4860:4860::8844
     '';
