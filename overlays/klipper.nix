7e2b1f4357e0df3a67d99964f4343407c58f6107
Bernardo Meurer <bernardo@meurer.org>
2020-10-30 02:34:45 -0700

    overlays/klipper: bump

--- overlays/klipper.nix
+++ overlays/klipper.nix
@@ -3,8 +3,8 @@ self: super: {
     src = self.fetchFromGitHub {
       repo = "klipper";
       owner = "KevinOConnor";
-      rev = "11bf83b49877f9db3f90072be73a3e078003c6a2";
-      sha256 = "sha256-azZe3UmCthX9P7SEKabXujuSkpYhiUvahXu4xCM5Ohs=";
+      rev = "79877acb14c01720130c469a25e86f0d06c539f3";
+      sha256 = "0fkglg24xh8hlrygvanvh5q56fz1ay06kqs1lq7782dsl9kd1vfx";
     };
   });
 }
