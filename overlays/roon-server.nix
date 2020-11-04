aa4343a02774b0a9301b68ee123dccd3d32456eb
Bernardo Meurer <bernardo@meurer.org>
2020-11-04 15:01:52 -0800

    overlays/roon-server: update

--- overlays/roon-server.nix
+++ overlays/roon-server.nix
@@ -2,7 +2,7 @@ self: super: {
   roon-server = super.roon-server.overrideAttrs (_: {
     src = self.fetchurl {
       url = "https://download.roonlabs.com/builds/RoonServer_linuxx64.tar.bz2";
-      sha256 = "0vrdcf6nbybmxfgmqyc3wn2kj3cz9x5frcysp5f5z2r0p6ksx5mj";
+      sha256 = "17gzwzh141ga2ywdiin2g5cwzidgcd4a3qjgzcfjkms7xkgz60jl";
     };
   });
 }
