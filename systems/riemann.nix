ce371924ad5eba19fb3b089ad280fa52323a8959
Bernardo Meurer <bernardo@meurer.org>
2020-11-06 12:07:59 -0800

    systems/riemann: increase endstop position

--- systems/riemann.nix
+++ systems/riemann.nix
@@ -172,7 +172,7 @@
         step_pin = "PB0";
 
         endstop_pin = "PC2";
-        position_endstop = "0.200";
+        position_endstop = "0.300";
         position_max = 120;
         position_min = -1;
 
