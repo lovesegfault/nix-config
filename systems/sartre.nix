6200d9e1d44cc53d065ee7a7b64e0ab808963abd
Bernardo Meurer <bernardo@meurer.org>
2020-11-08 01:27:12 -0800

    systems/sartre: enable automatic gc

--- systems/sartre.nix
+++ systems/sartre.nix
@@ -13,6 +13,8 @@
     hostId = "7ecc3d2a";
   };
 
+  nix.gc.automatic = true;
+
   secrets.files.ddclient-sartre = pkgs.mkSecret { file = ../secrets/ddclient-sartre.conf; };
   services.ddclient.configFile = config.secrets.files.ddclient-sartre.file;
 
