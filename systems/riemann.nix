751ed0089acd06cbb38a04aa5a567e2d77664352
Bernardo Meurer <bernardo@meurer.org>
2020-11-11 17:15:02 -0800

    systems/riemann: recalibrate pressure advance

--- systems/riemann.nix
+++ systems/riemann.nix
@@ -220,7 +220,7 @@
         max_temp = 300;
         min_extrude_temp = 220;
         min_temp = 0;
-        pressure_advance = "0.3194";
+        pressure_advance = "0.2846";
         pressure_advance_smooth_time = 0.040;
       };
 
