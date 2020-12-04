a0c47b54fc5a934414d6b070cf665ed1a4dee066
Bernardo Meurer <bernardo@meurer.org>
2020-12-03 23:09:55 -0800

    core/openssh: remove extraConfig

--- core/openssh.nix
+++ core/openssh.nix
@@ -5,11 +5,6 @@
     enable = true;
     challengeResponseAuthentication = false;
     permitRootLogin = lib.mkDefault "no";
-    extraConfig = ''
-      # Specifies whether to remove an existing Unix-domain socket file for local or remote port forwarding before creating a new one
-      StreamLocalBindUnlink yes
-      MaxSessions  100
-    '';
   };
 
   services.sshguard.enable = true;
