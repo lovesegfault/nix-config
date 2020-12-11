4da99ad9e22aa68ffcf1d7672653571b7d691fa0
Bernardo Meurer <bernardo@meurer.org>
2020-12-10 17:51:08 -0800

    users/bemeurer: coc.nvim

--- users/bemeurer/core/neovim.nix
+++ users/bemeurer/core/neovim.nix
@@ -22,15 +22,17 @@
           set rtp^=${plugin.rtp}
           set rtp+=${plugin.rtp}/after
         '';
-        brokenPlugins = with pkgs.vimPlugins; [ deoplete-nvim rust-vim vimtex ];
+        brokenPlugins = with pkgs.vimPlugins; [
+          rust-vim
+        ];
       in
       {
         enable = true;
         plugins = with pkgs.vimPlugins; [
           # Completion/IDE
-          ale # Linting
-          # deoplete-nvim # Completion
-          LanguageClient-neovim # LSP
+          coc-nvim
+          coc-rust-analyzer
+          coc-json
 
           # Colorscheme
           ayu-vim
@@ -40,15 +42,13 @@
           fugitive # Git
           fzf-vim # Search
           goyo # Distraction-free writing
-          lightline-ale # Linter integration
           lightline-vim # Statusbar
           tagbar # Code navigation
           vim-gist # Gist integration
           vim-indent-guides # Indentation highlighting
           vim-multiple-cursors # Sublime-like multipel cursors
           vim-surround
-          vim-trailing-whitespace # Highlight trailing whitespaces
-          # vimtex # LaTeX integration
+          vim-better-whitespace # Highlight trailing whitespaces
           webapi-vim # Gist dependency
 
           # Syntax
@@ -58,6 +58,8 @@
           # rust-vim # Rust 2018 syntax
           vim-nix # Nix syntax highlighting
         ];
+        withRuby = false;
+        extraPackages = with pkgs; [ nodejs ];
         extraConfig =
           let
             # FIXME: Workaround for broken handling of packpath by vim8/neovim for ftplugins
@@ -129,10 +131,10 @@
               set ruler
 
               " Height of the command bar
-              set cmdheight=1
+              set cmdheight=2
 
               " A buffer becomes hidden when it is abandoned
-              set hid
+              set hidden
 
               " Configure backspace so it acts as it should act
               set backspace=eol,start,indent
@@ -433,16 +435,17 @@
               """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
               " => ALE
               """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+              let g:ale_disable_lsp = 1
+              " Only run linters named in ale_linters settings.
+              let g:ale_linters_explicit = 1
               " Always apply ALEFix on save
               let g:ale_fix_on_save = 1
-              noremap <leader>f :ALEFix <CR>
-              nmap <silent> <C-j> <Plug>(ale_next_wrap)
+              nmap <silent> <C-e> <Plug>(ale_next_wrap)
               let g:ale_linters = {
                   \ 'c': ['cppcheck', 'flawfinder'],
                   \ 'cpp': ['cppcheck', 'flawfinder'],
                   \ 'markdown': ['mdl', 'proselint', 'languagetool'],
                   \ 'python': ['bandit', 'pylama', 'vulture'],
-                  \ 'rust': [],
                   \ 'text': ['proselint', 'languagetool'],
               \ }
               let g:ale_fixers = {
@@ -462,37 +465,167 @@
               let g:ayucolor="dark"
               colorscheme ayu
             '';
+            cocConfig = ''
+              """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+              " => CoC
+              """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+              " Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
+              " delays and poor user experience.
+              set updatetime=300
+
+              " Don't pass messages to |ins-completion-menu|.
+              set shortmess+=c
+
+              " Always show the signcolumn, otherwise it would shift the text each time
+              " diagnostics appear/become resolved.
+              if has("patch-8.1.1564")
+                " Recently vim can merge signcolumn and number column into one
+                set signcolumn=number
+              else
+                set signcolumn=yes
+              endif
+
+              " Use tab for trigger completion with characters ahead and navigate.
+              " NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
+              " other plugin before putting this into your config.
+              inoremap <silent><expr> <TAB>
+                    \ pumvisible() ? "\<C-n>" :
+                    \ <SID>check_back_space() ? "\<TAB>" :
+                    \ coc#refresh()
+              inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+
+              function! s:check_back_space() abort
+                let col = col('.') - 1
+                return !col || getline('.')[col - 1]  =~# '\s'
+              endfunction
+
+              " Use <c-space> to trigger completion.
+              if has('nvim')
+                inoremap <silent><expr> <c-space> coc#refresh()
+              else
+                inoremap <silent><expr> <c-@> coc#refresh()
+              endif
+
+              " Make <CR> auto-select the first completion item and notify coc.nvim to
+              " format on enter, <cr> could be remapped by other vim plugin
+              inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm()
+                                            \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+
+              " Use `[g` and `]g` to navigate diagnostics
+              " Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
+              nmap <silent> [g <Plug>(coc-diagnostic-prev)
+              nmap <silent> ]g <Plug>(coc-diagnostic-next)
+
+              " GoTo code navigation.
+              nmap <silent> gd <Plug>(coc-definition)
+              nmap <silent> gy <Plug>(coc-type-definition)
+              nmap <silent> gi <Plug>(coc-implementation)
+              nmap <silent> gr <Plug>(coc-references)
+
+              " Use K to show documentation in preview window.
+              nnoremap <silent> K :call <SID>show_documentation()<CR>
+
+              function! s:show_documentation()
+                if (index(['vim','help'], &filetype) >= 0)
+                  execute 'h '.expand('<cword>')
+                elseif (coc#rpc#ready())
+                  call CocActionAsync('doHover')
+                else
+                  execute '!' . &keywordprg . " " . expand('<cword>')
+                endif
+              endfunction
+
+              " Highlight the symbol and its references when holding the cursor.
+              autocmd CursorHold * silent call CocActionAsync('highlight')
+
+              " Symbol renaming.
+              nmap <leader>rn <Plug>(coc-rename)
+
+              " Formatting selected code.
+              xmap <leader>f  <Plug>(coc-format-selected)
+              nmap <leader>f  <Plug>(coc-format-selected)
+
+              augroup mygroup
+                autocmd!
+                " Setup formatexpr specified filetype(s).
+                autocmd FileType typescript,json setl formatexpr=CocAction('formatSelected')
+                " Update signature help on jump placeholder.
+                autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
+              augroup end
+
+              " Applying codeAction to the selected region.
+              " Example: `<leader>aap` for current paragraph
+              xmap <leader>a  <Plug>(coc-codeaction-selected)
+              nmap <leader>a  <Plug>(coc-codeaction-selected)
+
+              " Remap keys for applying codeAction to the current buffer.
+              nmap <leader>ac  <Plug>(coc-codeaction)
+              " Apply AutoFix to problem on the current line.
+              nmap <leader>qf  <Plug>(coc-fix-current)
+
+              " Map function and class text objects
+              " NOTE: Requires 'textDocument.documentSymbol' support from the language server.
+              xmap if <Plug>(coc-funcobj-i)
+              omap if <Plug>(coc-funcobj-i)
+              xmap af <Plug>(coc-funcobj-a)
+              omap af <Plug>(coc-funcobj-a)
+              xmap ic <Plug>(coc-classobj-i)
+              omap ic <Plug>(coc-classobj-i)
+              xmap ac <Plug>(coc-classobj-a)
+              omap ac <Plug>(coc-classobj-a)
+
+              " Remap <C-f> and <C-b> for scroll float windows/popups.
+              if has('nvim-0.4.0') || has('patch-8.2.0750')
+                nnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
+                nnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
+                inoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
+                inoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
+                vnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
+                vnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
+              endif
+
+              " Use CTRL-S for selections ranges.
+              " Requires 'textDocument/selectionRange' support of language server.
+              nmap <silent> <C-s> <Plug>(coc-range-select)
+              xmap <silent> <C-s> <Plug>(coc-range-select)
+
+              " Add `:Format` command to format current buffer.
+              command! -nargs=0 Format :call CocAction('format')
+
+              " Add `:Fold` command to fold current buffer.
+              command! -nargs=? Fold :call     CocAction('fold', <f-args>)
+
+              " Add `:OR` command for organize imports of the current buffer.
+              command! -nargs=0 OR   :call     CocAction('runCommand', 'editor.action.organizeImport')
+
+              " Add (Neo)Vim's native statusline support.
+              " NOTE: Please see `:h coc-status` for integrations with external plugins that
+              " provide custom statusline: lightline.vim, vim-airline.
+              set statusline^=%{coc#status()}%{get(b:,'coc_current_function',''')}
+
+              " Mappings for CoCList
+              " Show all diagnostics.
+              nnoremap <silent><nowait> <space>a  :<C-u>CocList diagnostics<cr>
+              " Manage extensions.
+              nnoremap <silent><nowait> <space>e  :<C-u>CocList extensions<cr>
+              " Show commands.
+              nnoremap <silent><nowait> <space>c  :<C-u>CocList commands<cr>
+              " Find symbol of current document.
+              nnoremap <silent><nowait> <space>o  :<C-u>CocList outline<cr>
+              " Search workspace symbols.
+              nnoremap <silent><nowait> <space>s  :<C-u>CocList -I symbols<cr>
+              " Do default action for next item.
+              nnoremap <silent><nowait> <space>j  :<C-u>CocNext<CR>
+              " Do default action for previous item.
+              nnoremap <silent><nowait> <space>k  :<C-u>CocPrev<CR>
+              " Resume latest coc list.
+              nnoremap <silent><nowait> <space>p  :<C-u>CocListResume<CR>
+            '';
             editorConfigConfig = ''
               """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
               " => editorconfig
               """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
               let g:EditorConfig_exclude_patterns = ['fugitive://.*', 'scp://.*']
-            '';
-            deopleteConfig = ''
-              """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-              " => deoplete
-              """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-              let g:deoplete#enable_at_startup = 1
-              inoremap <silent><expr> <TAB>
-                  \ pumvisible() ? "\<C-n>" :
-                  \ <SID>check_back_space() ? "\<TAB>" :
-                  \ deoplete#manual_complete()
-
-              function! s:check_back_space() abort "{{{
-              let col = col('.') - 1
-              return !col || getline('.')[col - 1]  =~ '\s'
-              endfunction"}}}
-
-              function g:Multiple_cursors_before()
-                  call deoplete#custom#buffer_option('auto_complete', v:false)
-              endfunction
-              function g:Multiple_cursors_after()
-                  call deoplete#custom#buffer_option('auto_complete', v:true)
-              endfunction
-
-              " Disable the truncate feature.
-              call deoplete#custom#source('_', 'max_abbr_width', 20)
-              call deoplete#custom#source('_', 'max_menu_width', 80)
             '';
             fzfConfig = ''
               """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -521,45 +654,6 @@
               " Always show indent guides
               let g:indent_guides_enable_on_vim_startup = 1
             '';
-            languageClientConfig = ''
-              """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-              " => LanguageClient-neovim
-              """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-              " Required for operations modifying multiple buffers like rename.
-              set hidden
-
-              let g:LanguageClient_serverCommands = {
-                  \ 'c': ['ccls'],
-                  \ 'cpp': ['ccls'],
-                  \ 'haskell': ['ghcide', '--lsp'],
-                  \ 'nix': ['rnix-lsp'],
-                  \ 'python': ['pyls'],
-                  \ 'rust': ['rust-analyzer'],
-                  \ 'tex': ['texlab'],
-              \ }
-
-              nnoremap <space> :call LanguageClient_contextMenu()<CR>
-              nnoremap <silent> K :call LanguageClient#textDocument_hover()<CR>
-              nnoremap <silent> gd :call LanguageClient#textDocument_definition()<CR>
-              nnoremap <silent> gi :call LanguageClient#textDocument_implementation()<CR>
-              nnoremap <silent> gr :call LanguageClient#textDocument_references()<CR>
-              nnoremap <silent> gy :call LanguageClient#textDocument_typeDefinition()<CR>
-
-              " Rename - rn => rename
-              noremap <leader>rn :call LanguageClient#textDocument_rename()<CR>
-
-              " Rename - rc => rename camelCase
-              noremap <leader>rc :call LanguageClient#textDocument_rename(
-                  \ {'newName': Abolish.camelcase(expand('<cword>'))})<CR>
-
-              " Rename - rs => rename snake_case
-              noremap <leader>rs :call LanguageClient#textDocument_rename(
-                  \ {'newName': Abolish.snakecase(expand('<cword>'))})<CR>
-
-              " Rename - ru => rename UPPERCASE
-              noremap <leader>ru :call LanguageClient#textDocument_rename(
-                  \ {'newName': Abolish.uppercase(expand('<cword>'))})<CR>
-            '';
             lightlineConfig = ''
               """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
               " => lightline
@@ -568,57 +662,27 @@
                   \ 'colorscheme': 'ayu',
                   \ 'active': {
                   \   'left': [
-                  \       ['mode', 'paste'],
-                  \       ['filename', 'modified']
+                  \     [ 'mode', 'paste' ],
+                  \     [ 'cocstatus', 'readonly', 'filename', 'modified' ]
                   \   ],
                   \   'right': [
-                  \       ['lineinfo'],
-                  \       ['percent'],
-                  \       ['fileformat', 'fileencoding', 'filetype', 'readonly'],
-                  \       ['linter_checking', 'linter_errors', 'linter_warnings', 'linter_ok']
+                  \     [ 'lineinfo' ],
+                  \     [ 'percent' ],
+                  \     [ 'fileformat', 'fileencoding', 'filetype' ]
                   \   ]
                   \ },
-                  \ 'component_expand': {
-                  \   'linter_checking': 'lightline#ale#checking',
-                  \   'linter_warnings': 'lightline#ale#warnings',
-                  \   'linter_errors': 'lightline#ale#errors',
-                  \   'linter_ok': 'lightline#ale#ok',
-                  \ },
-                  \ 'component_type': {
-                  \   'linter_checking': 'left',
-                  \   'linter_warnings': 'warning',
-                  \   'linter_errors': 'error',
-                  \   'linter_ok': 'left',
+                  \ 'component_function': {
+                  \   'cocstatus': 'coc#status'
                   \ }
               \ }
+              " Use auocmd to force lightline update.
+              autocmd User CocStatusChange,CocDiagnosticChange call lightline#update()
             '';
             tagbarConfig = ''
               """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
               " => tagbar
               """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
               noremap <leader>t :TagbarToggle<CR>
-            '';
-            vimtexConfig = ''
-              """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-              " => vimtex
-              """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-              " Enable shellescape for the LaTeX compiler
-              " NB: needed for syntax highlighting pkgs.
-              let g:tex_flavor = 'latex'
-              let g:vimtex_compiler_latexmk = {
-                  \ 'options' : [
-                  \   '-pdf',
-                  \   '-shell-escape',
-                  \   '-verbose',
-                  \   '-file-line-error',
-                  \   '-synctex=1',
-                  \   '-interaction=nonstopmode',
-                  \ ],
-              \ }
-              if has('nvim')
-                let g:vimtex_compiler_progname = 'nvr'
-              endif
-              let g:vimtex_view_general_viewer = 'evince'
             '';
           in
           ''
@@ -627,14 +691,12 @@
 
             ${aleConfig}
             ${ayuConfig}
-            ${deopleteConfig}
+            ${cocConfig}
             ${editorConfigConfig}
             ${fzfConfig}
             ${indentGuidesConfig}
-            ${languageClientConfig}
             ${lightlineConfig}
             ${tagbarConfig}
-            ${vimtexConfig}
           '';
       };
   };
