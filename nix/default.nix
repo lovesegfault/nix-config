01116135d8673e489eb36ff316d099b3076d524f
Bernardo Meurer <bernardo@meurer.org>
2020-12-14 14:36:02 -0800

    nix: simplify some imports

--- nix/default.nix
+++ nix/default.nix
@@ -3,11 +3,10 @@ let
 in
 rec {
   inherit (sources) nixpkgs;
-  gitignoreSource = (import sources.gitignore { inherit lib; }).gitignoreSource;
+  gitignoreSource = (import sources.gitignore { }).gitignoreSource;
   home-manager = import (sources.home-manager + "/nixos");
   impermanence-home = sources.impermanence + "/home-manager.nix";
   impermanence-nixos = sources.impermanence + "/nixos.nix";
-  lib = import (nixpkgs + "/lib");
-  nix-pre-commit-hooks = (import (sources.nix-pre-commit-hooks + "/nix") { inherit nixpkgs; }).packages;
+  nix-pre-commit-hooks = (import (sources.nix-pre-commit-hooks + "/nix") { }).packages;
   nixus = import sources.nixus { };
 }
