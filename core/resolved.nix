0377ae54bad8029475f9cc31c4a6fcf03392c847
Bernardo Meurer <bernardo@meurer.org>
2020-12-14 18:47:21 -0800

    core/resolved: use tailscale magicDNS

--- core/resolved.nix
+++ core/resolved.nix
@@ -6,7 +6,9 @@
     dnssec = "false";
     llmnr = "false";
     extraConfig = ''
-      DNS=1.1.1.1 2606:4700:4700::1111 8.8.8.8 2001:4860:4860::8844
+      DNS=100.100.100.100
+      FallbackDNS=1.1.1.1 2606:4700:4700::1111 8.8.8.8 2001:4860:4860::8844
+      Domains=meurer.org.beta.tailscale.net.
     '';
   };
 }
