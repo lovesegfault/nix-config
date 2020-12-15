2c3a77b348afcac8c046d065203b33d7c6abedfc
Bernardo Meurer <bernardo@meurer.org>
2020-12-14 17:20:35 -0800

    nix: actually fix gitignoreSource

--- nix/default.nix
+++ nix/default.nix
@@ -3,12 +3,12 @@ let
 in
 rec {
   inherit (sources) nixpkgs;
-  gitignoreSource = (import sources.gitignore { lib = nixpkgs + "/lib"; }).gitignoreSource;
+  gitignoreSource = (import sources.gitignore { lib = import (nixpkgs + "/lib"); }).gitignoreSource;
   home-manager = import (sources.home-manager + "/nixos");
   impermanence-home = sources.impermanence + "/home-manager.nix";
   impermanence-nixos = sources.impermanence + "/nixos.nix";
   nix-pre-commit-hooks = (import (sources.nix-pre-commit-hooks + "/nix") { }).packages;
   nixus = import sources.nixus { };
-  sops-nixos = import (sources.sops-nix + "/modules/sops");
   sops-nix = import sources.sops-nix;
+  sops-nixos = import (sources.sops-nix + "/modules/sops");
 }
