9d2fe79092bc8f0b699494edc1bec5b64ac77788
Bernardo Meurer <bernardo@meurer.org>
2020-12-18 14:59:21 -0800

    core/resolved: allow-downgrade

--- core/resolved.nix
+++ core/resolved.nix
@@ -3,12 +3,10 @@
 
   services.resolved = {
     enable = true;
-    dnssec = "true";
+    dnssec = "allow-downgrade";
     llmnr = "false";
     extraConfig = ''
-      DNSStubListener=false
       FallbackDNS=1.1.1.1 2606:4700:4700::1111 8.8.8.8 2001:4860:4860::8844
-      Domains=meurer.org.beta.tailscale.net.
     '';
   };
 }
