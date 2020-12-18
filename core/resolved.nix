a82cd313a9ae998d2868e0f433a9d8634069c9d9
Bernardo Meurer <bernardo@meurer.org>
2020-12-18 14:32:41 -0800

    core/resolved: set DNSStubListener to false

--- core/resolved.nix
+++ core/resolved.nix
@@ -6,6 +6,7 @@
     dnssec = "true";
     llmnr = "false";
     extraConfig = ''
+      DNSStubListener=false
       FallbackDNS=1.1.1.1 2606:4700:4700::1111 8.8.8.8 2001:4860:4860::8844
       Domains=meurer.org.beta.tailscale.net.
     '';
