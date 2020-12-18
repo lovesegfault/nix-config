3ffa913886ee70eab90071517191b6f42ea35534
Bernardo Meurer <bernardo@meurer.org>
2020-12-18 04:14:06 -0800

    overlays/roon-server: bump sha

--- overlays/roon-server.nix
+++ overlays/roon-server.nix
@@ -2,7 +2,7 @@ self: super: {
   roon-server = super.roon-server.overrideAttrs (_: {
     src = self.fetchurl {
       url = "https://download.roonlabs.com/builds/RoonServer_linuxx64.tar.bz2";
-      sha256 = "17gzwzh141ga2ywdiin2g5cwzidgcd4a3qjgzcfjkms7xkgz60jl";
+      sha256 = "sha256-S1c3RvN/HE8zwpGZARYRy/Gu4OTG0CjWbxJhZ3cKoVE=";
     };
   });
 }
