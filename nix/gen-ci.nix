c2596ebf197e9d3450e6a3ff1ada1eae0c8e381a
Bernardo Meurer <bernardo@meurer.org>
2020-12-20 09:37:56 -0800

    nix/gen-ci: use dependent jobs

--- nix/gen-ci.nix
+++ nix/gen-ci.nix
@@ -6,57 +6,66 @@
 , hosts
 }:
 let
-  mkGenericJob = extraSteps: {
+  checkoutStep = {
+    uses = "actions/checkout@v2";
+    "with".fetch-depth = 0;
+  };
+  nixStep = {
+    uses = "cachix/install-nix-action@v12";
+    "with" = {
+      nix_path = "nixpkgs=channel:nixos-unstable-small";
+      install_url = "https://github.com/numtide/nix-flakes-installer/releases/download/nix-3.0pre20201007_5257a25/install";
+      extra_nix_config = "experimental-features = nix-command flakes";
+    };
+  };
+  aarch64Step = {
+    name = "AArch64";
+    run = ''
+      # first create the ssh config dir for root
+      sudo mkdir -p /root/.ssh
+
+      # now add the key for the builder
+      echo "''${{ secrets.AARCH64_BOX_KEY }}" |
+          sudo tee /root/.ssh/aarch64.community.nixos > /dev/null
+      sudo chmod 0600 /root/.ssh/aarch64.community.nixos
+
+      # and make it a known host
+      echo "''${{ secrets.KNOWN_HOSTS }}" |
+          sudo tee -a /root/.ssh/known_hosts > /dev/null
+
+      # lastly register the builder with nix
+      builder_cfg=(
+        lovesegfault@aarch64.nixos.community # user/addr
+        aarch64-linux                        # arch
+        /root/.ssh/aarch64.community.nixos   # key
+        64                                   # maxJobs
+        1                                    # speed factor
+        big-parallel                         # features
+      )
+      echo "''${builder_cfg[*]}" |
+          sudo tee /etc/nix/machines > /dev/null
+    '';
+  };
+  cachixStep = {
+    uses = "cachix/cachix-action@v8";
+    "with" = {
+      name = "nix-config";
+      signingKey = "'\${{ secrets.CACHIX_SIGNING_KEY }}'";
+    };
+  };
+
+  mkGenericJob = dependencies: extraSteps: {
     runs-on = "ubuntu-latest";
+    needs = dependencies;
     steps = [
-      { uses = "actions/checkout@v2"; }
-      {
-        uses = "cachix/install-nix-action@v12";
-        "with" = {
-          nix_path = "nixpkgs=channel:nixos-unstable-small";
-          install_url = "https://github.com/numtide/nix-flakes-installer/releases/download/nix-3.0pre20201007_5257a25/install";
-          extra_nix_config = "experimental-features = nix-command flakes";
-        };
-      }
-      {
-        name = "AArch64";
-        run = ''
-          # first create the ssh config dir for root
-          sudo mkdir -p /root/.ssh
-
-          # now add the key for the builder
-          echo "''${{ secrets.AARCH64_BOX_KEY }}" |
-              sudo tee /root/.ssh/aarch64.community.nixos > /dev/null
-          sudo chmod 0600 /root/.ssh/aarch64.community.nixos
-
-          # and make it a known host
-          echo "''${{ secrets.KNOWN_HOSTS }}" |
-              sudo tee -a /root/.ssh/known_hosts > /dev/null
-
-          # lastly register the builder with nix
-          builder_cfg=(
-            lovesegfault@aarch64.nixos.community # user/addr
-            aarch64-linux                        # arch
-            /root/.ssh/aarch64.community.nixos   # key
-            64                                   # maxJobs
-            1                                    # speed factor
-            big-parallel                         # features
-          )
-          echo "''${builder_cfg[*]}" |
-              sudo tee /etc/nix/machines > /dev/null
-        '';
-      }
-      {
-        uses = "cachix/cachix-action@v8";
-        "with" = {
-          name = "nix-config";
-          signingKey = "'\${{ secrets.CACHIX_SIGNING_KEY }}'";
-        };
-      }
+      checkoutStep
+      nixStep
+      aarch64Step
+      cachixStep
     ] ++ extraSteps;
   };
 
-  mkHostJob = host: mkGenericJob [{
+  mkHostJob = host: mkGenericJob [ "check-ci" ] [{
     name = "Build";
     run = ''
       nix run nixpkgs#nix-build-uncached -- \
@@ -71,14 +80,15 @@ let
     };
     name = "CI";
     jobs = (lib.genAttrs hosts mkHostJob) // {
-      check-ci = mkGenericJob [{
+      check-ci = mkGenericJob [ ] [{
         run = ''
           cp ./.github/workflows/ci.yml /tmp/ci.yml.old
           nix run .#gen-ci
           diff ./.github/workflows/ci.yml /tmp/ci.yml.old || exit 1
         '';
       }];
-      check-flake = mkGenericJob [{
+
+      check-flake = mkGenericJob hosts [{
         run = "nix flake check";
       }];
     };
