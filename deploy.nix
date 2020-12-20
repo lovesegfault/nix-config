da32daec6c5a19933dc71baa72d3e437c3ade7cb
Bernardo Meurer <bernardo@meurer.org>
2020-12-20 08:43:51 -0800

    deploy: init

---
+++ deploy.nix
@@ -0,0 +1,70 @@
+{ self
+, deploy-rs
+, home-manager
+, impermanence
+, nixpkgs
+, sops-nix
+, staging
+, ...
+}@inputs:
+let
+  inherit (builtins) attrNames mapAttrs readDir;
+
+  overlays = map (f: import (./overlays + "/${f}")) (attrNames (readDir ./overlays));
+
+  mkHost = name: system:
+    nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ({ nixpkgs = { inherit overlays; }; })
+        ({ systemd.package = (import staging { inherit system; }).systemd; })
+        impermanence.nixosModules.impermanence
+        home-manager.nixosModules.home-manager
+        sops-nix.nixosModules.sops
+
+        (./hosts + "/${name}")
+      ];
+      specialArgs.inputs = inputs;
+    };
+
+  mkPath = name: system: deploy-rs.lib.${system}.activate.nixos (mkHost name system);
+in
+{
+  deploy = {
+    autoRollback = true;
+    magicRollback = true;
+    user = "root";
+    nodes = {
+      # aurelius = {
+      #   hostname = "aurelius";
+      #   profiles.system.path = mkPath "aurelius" "aarch64-linux";
+      # };
+      feuerbach = {
+        hostname = "stcg-us-0005-11";
+        profiles.system.path = mkPath "feuerbach" "x86_64-linux";
+      };
+      foucault = {
+        hostname = "100.67.182.67";
+        profiles.system.path = mkPath "foucault" "x86_64-linux";
+        magicRollback = false;
+      };
+      fourier = {
+        hostname = "100.113.42.46";
+        profiles.system.path = mkPath "fourier" "x86_64-linux";
+      };
+      goethe = {
+        hostname = "100.125.185.48";
+        profiles.system.path = mkPath "goethe" "aarch64-linux";
+      };
+      riemann = {
+        hostname = "100.99.75.64";
+        profiles.system.path = mkPath "riemann" "aarch64-linux";
+      };
+      sartre = {
+        hostname = "100.97.215.77";
+        profiles.system.path = mkPath "sartre" "x86_64-linux";
+      };
+    };
+  };
+  checks = mapAttrs (_: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+}
--- output.nix
+++ output.nix
@@ -8,70 +8,10 @@
 , staging
 , ...
 }@inputs:
-let
-  inherit (builtins) attrNames mapAttrs readDir;
-  inherit (nixpkgs.lib) mapAttrsToList;
-
-  overlays = map (f: import (./overlays + "/${f}")) (attrNames (readDir ./overlays));
-
-  mkHost = name: system:
-    nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        ({ nixpkgs = { inherit overlays; }; })
-        ({ systemd.package = (import staging { inherit system; }).systemd; })
-        impermanence.nixosModules.impermanence
-        home-manager.nixosModules.home-manager
-        sops-nix.nixosModules.sops
-
-        (./hosts + "/${name}")
-      ];
-      specialArgs.inputs = inputs;
-    };
-
-  mkPath = name: system: deploy-rs.lib.${system}.activate.nixos (mkHost name system);
-in
-{
-  deploy = {
-    autoRollback = true;
-    magicRollback = true;
-    user = "root";
-    nodes = {
-      # aurelius = {
-      #   hostname = "aurelius";
-      #   profiles.system.path = mkPath "aurelius" "aarch64-linux";
-      # };
-      feuerbach = {
-        hostname = "stcg-us-0005-11";
-        profiles.system.path = mkPath "feuerbach" "x86_64-linux";
-      };
-      foucault = {
-        hostname = "100.67.182.67";
-        profiles.system.path = mkPath "foucault" "x86_64-linux";
-        magicRollback = false;
-      };
-      fourier = {
-        hostname = "100.113.42.46";
-        profiles.system.path = mkPath "fourier" "x86_64-linux";
-      };
-      goethe = {
-        hostname = "100.125.185.48";
-        profiles.system.path = mkPath "goethe" "aarch64-linux";
-      };
-      riemann = {
-        hostname = "100.99.75.64";
-        profiles.system.path = mkPath "riemann" "aarch64-linux";
-      };
-      sartre = {
-        hostname = "100.97.215.77";
-        profiles.system.path = mkPath "sartre" "x86_64-linux";
-      };
-    };
-  };
-
-  checks = mapAttrs (_: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
-} // flake-utils.lib.eachDefaultSystem (system:
+(flake-utils.lib.eachDefaultSystem (system:
   let
+    inherit (builtins) attrNames;
+    inherit (nixpkgs.lib) mapAttrsToList;
     pkgs = import nixpkgs { inherit system; };
   in
   {
@@ -99,4 +39,4 @@ in
       inherit (sops-nix.packages.${system}) ssh-to-pgp sops-pgp-hook;
       inherit (deploy-rs.packages.${system}) deploy-rs;
     };
-  })
+})) // (import ./deploy.nix inputs)
