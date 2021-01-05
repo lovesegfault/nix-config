d9e03c27243d9277a6533df15a08a527e92798ea
Bernardo Meurer <bernardo@meurer.org>
2021-01-05 10:55:23 -0800

    changed: don't set QT_QPA_PLATFORM

--- sway/sway.nix
+++ sway/sway.nix
@@ -12,7 +12,6 @@
       export ECORE_EVAS_ENGINE=wayland_egl
       export ELM_ENGINE=wayland_egl
       export MOZ_ENABLE_WAYLAND=1
-      export QT_QPA_PLATFORM=wayland
       export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
       export QT_WAYLAND_FORCE_DPI=physical
       export SDL_VIDEODRIVER=wayland
