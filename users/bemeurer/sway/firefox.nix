0eea861be730df584b4f30a0e3fcb01b2e030e3a
Bernardo Meurer <bernardo@meurer.org>
2021-01-13 22:34:10 -0800

    changed: use firefox-bin

--- users/bemeurer/sway/firefox.nix
+++ users/bemeurer/sway/firefox.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox-wayland;
+    package = pkgs.firefox-bin;
   };
   programs.newsboat.browser = "${config.programs.firefox.package}/bin/firefox";
 }
