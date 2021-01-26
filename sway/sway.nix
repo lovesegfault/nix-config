ff5e89375da955221a25f4042a6a2a9745c1f719
Bernardo Meurer <bernardo@meurer.org>
2021-01-26 14:34:03 -0800

    changed: set QT_QPA_PLATFORM to xcb

--- sway/sway.nix
+++ sway/sway.nix
@@ -12,6 +12,7 @@
       export ECORE_EVAS_ENGINE=wayland_egl
       export ELM_ENGINE=wayland_egl
       export MOZ_ENABLE_WAYLAND=1
+      export QT_QPA_PLATFORM=xcb
       export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
       export QT_WAYLAND_FORCE_DPI=physical
       export SDL_VIDEODRIVER=wayland
