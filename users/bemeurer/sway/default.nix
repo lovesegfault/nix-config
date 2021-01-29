af0f2efc7246dd78a5be17f38cac7a441763503f
Bernardo Meurer <bernardo@meurer.org>
2021-01-28 18:12:09 -0800

    changed: dump sway logs to /tmp/sway.log

--- users/bemeurer/sway/default.nix
+++ users/bemeurer/sway/default.nix
@@ -51,7 +51,7 @@
   programs.zsh.profileExtra = ''
     # If running from tty1 start sway
     if [ "$(tty)" = "/dev/tty1" ]; then
-        exec sway
+        exec sway > /tmp/sway.log
     fi
   '';
 
