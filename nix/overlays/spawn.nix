5f5b35311bf656ca1a4ccdc9ac01e13a5dbd709a
Bernardo Meurer <bernardo@meurer.org>
2021-01-31 20:58:09 -0800

    changed: convert spawn to writeShellScriptBin

--- nix/overlays/spawn.nix
+++ nix/overlays/spawn.nix
@@ -1,16 +1,26 @@
 self: _: {
-  spawn = self.writeScriptBin "spawn" ''
-    #!${self.stdenv.shell}
-    set -o errexit
-    set -o nounset
-    set -o pipefail
-    set -o xtrace
+  spawn = self.callPackage
+    (
+      { writeShellScriptBin
+      , coreutils
+      , lib
+      , systemd
+      , util-linux
+      }: writeShellScriptBin "spawn" ''
+        set -o errexit
+        set -o nounset
+        set -o pipefail
+        set -o xtrace
 
-    [ "$#" -ge 1 ] || exit 1
-    read -ra cmd <<<"$*"
-    program="''${cmd[0]}"
-    name="$(${self.coreutils}/bin/basename "$program")"
-    uuid="$(${self.utillinux}/bin/uuidgen)"
-    exec ${self.systemd}/bin/systemd-run --user --scope --unit "run-$name-$uuid" "''${cmd[@]}"
-  '';
+        export PATH="${lib.makeBinPath [ coreutils systemd util-linux ]}:$PATH"
+
+        [ "$#" -ge 1 ] || exit 1
+        read -ra cmd <<<"$*"
+        program="''${cmd[0]}"
+        name="$(basename "$program")"
+        uuid="$(uuidgen)"
+        exec systemd-run --user --scope --unit "run-$name-$uuid" "''${cmd[@]}"
+      ''
+    )
+    { };
 }
