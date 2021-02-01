15723dcebde3739e3894eb450e96f83212f9f09b
Bernardo Meurer <bernardo@meurer.org>
2021-01-31 22:17:30 -0800

    changed: migrate spawn to writeSaneShellScriptBin

--- nix/overlays/spawn.nix
+++ nix/overlays/spawn.nix
@@ -1,26 +1,24 @@
 self: _: {
   spawn = self.callPackage
     (
-      { writeShellScriptBin
+      { writeSaneShellScriptBin
       , coreutils
-      , lib
       , systemd
       , util-linux
-      }: writeShellScriptBin "spawn" ''
-        set -o errexit
-        set -o nounset
-        set -o pipefail
-        set -o xtrace
+      }: writeSaneShellScriptBin {
+        name = "spawn";
 
-        export PATH="${lib.makeBinPath [ coreutils systemd util-linux ]}:$PATH"
+        buildInputs = [ coreutils systemd util-linux ];
 
-        [ "$#" -ge 1 ] || exit 1
-        read -ra cmd <<<"$*"
-        program="''${cmd[0]}"
-        name="$(basename "$program")"
-        uuid="$(uuidgen)"
-        exec systemd-run --user --scope --unit "run-$name-$uuid" "''${cmd[@]}"
-      ''
+        script = ''
+          [ "$#" -ge 1 ] || exit 1
+          read -ra cmd <<<"$*"
+          program="''${cmd[0]}"
+          name="$(basename "$program")"
+          uuid="$(uuidgen)"
+          exec systemd-run --user --scope --unit "run-$name-$uuid" "''${cmd[@]}"
+        '';
+      }
     )
     { };
 }
