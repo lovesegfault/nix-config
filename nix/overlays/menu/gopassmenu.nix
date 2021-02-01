264f177153031230ca63ebba13d952ce69b0a4ce
Bernardo Meurer <bernardo@meurer.org>
2021-01-31 22:27:14 -0800

    changed: migrate gopassmenu to writeSaneShellScriptBin

--- nix/overlays/menu/gopassmenu.nix
+++ nix/overlays/menu/gopassmenu.nix
@@ -5,61 +5,72 @@
 , libnotify
 , ripgrep
 , wl-clipboard
-, writeScriptBin
+, writeSaneShellScriptBin
 
 , name
 , filter
 , getter
 , terminal
 }:
-writeScriptBin name ''
-  #!${stdenv.shell}
-  export PATH="${lib.makeBinPath [ fzf gopass libnotify ripgrep wl-clipboard ]}:$PATH"
+writeSaneShellScriptBin {
+  inherit name;
 
-  gopassmenu_path="$(readlink -f "$0")"
-  gopassmenu_fifo="/tmp/gopassmenu_fifo"
-  gopassmenu_lock="/tmp/gopassmenu_lock"
+  buildInputs = [
+    fzf
+    gopass
+    libnotify
+    ripgrep
+    wl-clipboard
+  ];
 
-  function gopassmenu_lock() {
-      if [[ -f "$gopassmenu_lock" ]]; then
-          notify-send "✖️ gopassmenu already running"
-          exit 1
-      else
-          touch "$gopassmenu_lock"
-      fi
-  }
+  src = ''
+    gopassmenu_path="$(readlink -f "$0")"
+    gopassmenu_fifo="/tmp/gopassmenu_fifo"
+    gopassmenu_lock="/tmp/gopassmenu_lock"
 
-  function gopassmenu_unlock() {
-      if [[ -f "$gopassmenu_lock" ]]; then
-          rm -f "$gopassmenu_lock"
-      fi
-  }
+    function gopassmenu_lock() {
+        if [[ -f "$gopassmenu_lock" ]]; then
+            notify-send "✖️ gopassmenu already running"
+            exit 1
+        else
+            touch "$gopassmenu_lock"
+        fi
+    }
 
-  function gopassmenu_frontend() {
-      name="$(gopass ls -f | rg "${filter}" | fzf)"
-      echo "$name" > "$gopassmenu_fifo"
-  }
+    function gopassmenu_unlock() {
+        if [[ -f "$gopassmenu_lock" ]]; then
+            rm -f "$gopassmenu_lock"
+        fi
+    }
 
-  function gopassmenu_backend() {
-      gopassmenu_lock
-      export GOPASSMENU_FRONTEND=1
-      ${terminal} -t ${name} -e "$gopassmenu_path"
+    function gopassmenu_frontend() {
+        name="$(gopass ls -f | rg "${filter}" | fzf)"
+        echo "$name" > "$gopassmenu_fifo"
+    }
 
-      name="$(cat "$gopassmenu_fifo")"
-      rm -f "$gopassmenu_fifo"
-      if [ "$name" == "" ]; then
-          gopassmenu_unlock
-          exit 1
-      fi
+    function gopassmenu_backend() {
+        gopassmenu_lock
+        export GOPASSMENU_FRONTEND=1
+        ${terminal} -t ${name} -e "$gopassmenu_path"
 
-      local password="$(gopass ${getter})"
-      wl-copy -o "$password"
-      gopassmenu_unlock
-  }
+        name="$(cat "$gopassmenu_fifo")"
+        rm -f "$gopassmenu_fifo"
+        if [ "$name" == "" ]; then
+            gopassmenu_unlock
+            exit 1
+        fi
 
-  if [[ -v GOPASSMENU_FRONTEND ]]; then
-      gopassmenu_frontend
-  else
-      gopassmenu_backend
-  fi
-''
+        local password
+        password="$(gopass ${getter})"
+
+        wl-copy -o "$password"
+        gopassmenu_unlock
+    }
+
+    if [[ -v GOPASSMENU_FRONTEND ]]; then
+        gopassmenu_frontend
+    else
+        gopassmenu_backend
+    fi
+  '';
+}
