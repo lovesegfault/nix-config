e4b0c986369be278920250a3e93cec47c38c254d
Bernardo Meurer <bernardo@meurer.org>
2021-01-31 22:11:37 -0800

    changed: migrate mbk to writeSaneShellScriptBin

--- nix/overlays/mbk.nix
+++ nix/overlays/mbk.nix
@@ -1,44 +1,41 @@
 self: _: {
   mbk = self.callPackage
     (
-      { writeShellScriptBin
-      , google-cloud-sdk
-      , lib
-      }: writeShellScriptBin "mbk" ''
-        set -o errexit
-        set -o nounset
-        set -o pipefail
+      { writeSaneShellScriptBin, google-cloud-sdk }: writeSaneShellScriptBin {
+        name = "mbk";
 
-        export PATH="${lib.makeBinPath [ google-cloud-sdk ]}:$PATH"
+        buildInputs = [ google-cloud-sdk ];
 
-        DOCUMENTS_BUCKET="gs://documents.meurer.org"
-        MISC_BUCKET="gs://misc.meurer.org"
-        MUSIC_BUCKET="gs://music.meurer.org"
-        PICTURES_BUCKET="gs://pictures.meurer.org"
+        script = ''
+          DOCUMENTS_BUCKET="gs://documents.meurer.org"
+          MISC_BUCKET="gs://misc.meurer.org"
+          MUSIC_BUCKET="gs://music.meurer.org"
+          PICTURES_BUCKET="gs://pictures.meurer.org"
 
-        function backup() {
-            local src="$1"
-            local dst="$2"
+          function backup() {
+              local src="$1"
+              local dst="$2"
 
-            if [ -z ''${DEBUG+x} ]; then
-                gsutil -m rsync -d -r "$src" "$dst"
-            else
-                gsutil -m rsync -d -r -n "$src" "$dst"
-            fi
-        }
+              if [ -z ''${DEBUG+x} ]; then
+                  gsutil -m rsync -d -r "$src" "$dst"
+              else
+                  gsutil -m rsync -d -r -n "$src" "$dst"
+              fi
+          }
 
-        function main() {
-            # main dirs
-            backup "$HOME/documents" "$DOCUMENTS_BUCKET"
-            backup "$HOME/music" "$MUSIC_BUCKET"
-            backup "$HOME/pictures" "$PICTURES_BUCKET"
-            # weirder state dirs
-            backup "$HOME/.local/share/shotwell" "$MISC_BUCKET/shotwell"
-            backup "$HOME/.local/share/lollypop" "$MISC_BUCKET/lollypop"
-        }
+          function main() {
+              # main dirs
+              backup "$HOME/documents" "$DOCUMENTS_BUCKET"
+              backup "$HOME/music" "$MUSIC_BUCKET"
+              backup "$HOME/pictures" "$PICTURES_BUCKET"
+              # weirder state dirs
+              backup "$HOME/.local/share/shotwell" "$MISC_BUCKET/shotwell"
+              backup "$HOME/.local/share/lollypop" "$MISC_BUCKET/lollypop"
+          }
 
-        main
-      ''
+          main
+        '';
+      }
     )
     { };
 }
