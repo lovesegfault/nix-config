805b9def086bebf3635453683211f15e9ec6e114
Bernardo Meurer <bernardo@meurer.org>
2021-02-07 16:27:32 -0800

    changed: only allow a maximum of 15 boot entries

--- hardware/efi.nix
+++ hardware/efi.nix
@@ -1,7 +1,10 @@
 { lib, ... }: {
   boot.loader = {
     efi.canTouchEfiVariables = true;
-    systemd-boot.enable = true;
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 15;
+    };
     timeout = lib.mkDefault 2;
   };
   console.earlySetup = true;
