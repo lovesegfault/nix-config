c40375f4eb561583606af0ad3dbe47378a2e9470
Bernardo Meurer <bernardo@meurer.org>
2021-02-19 22:38:35 -0800

    changed: use experimental nix-zsh-completions with flake support

---
+++ nix/overlays/nix-zsh-completions.nix
@@ -0,0 +1,10 @@
+self: super: {
+  nix-zsh-completions = super.nix-zsh-completions.overrideAttrs (oldAttrs: {
+    src = self.fetchFromGitHub {
+      owner = "Ma27";
+      repo = "nix-zsh-completions";
+      rev = "939c48c182e9d018eaea902b1ee9d00a415dba86";
+      sha256 = "sha256-3HVYez/wt7EP8+TlhTppm968Wl8x5dXuGU0P+8xNDpo=";
+    };
+  });
+}
