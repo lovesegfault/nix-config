dfff66a703a052d82240e226e13bf8ec0b626405
Bernardo Meurer <bernardo@meurer.org>
2021-02-22 12:01:36 -0800

    users/bemeurer: enable sway debug logs

--- users/bemeurer/sway/default.nix
+++ users/bemeurer/sway/default.nix
@@ -49,7 +49,7 @@
   programs.zsh.profileExtra = ''
     # If running from tty1 start sway
     if [ "$(tty)" = "/dev/tty1" ]; then
-        exec sway > /tmp/sway.log 2>&1
+        exec sway --debug -V > /tmp/sway.log 2>&1
     fi
   '';
 
