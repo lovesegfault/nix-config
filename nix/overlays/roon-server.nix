79f4b7b25e6093e4220191010c1f3449a995960b
Bernardo Meurer <bernardo@meurer.org>
2021-02-23 14:15:56 -0800

    nix/overlays/roon-server: update hash

--- nix/overlays/roon-server.nix
+++ nix/overlays/roon-server.nix
@@ -2,7 +2,7 @@ self: super: {
   roon-server = super.roon-server.overrideAttrs (_: {
     src = self.fetchurl {
       url = "https://download.roonlabs.com/builds/RoonServer_linuxx64.tar.bz2";
-      sha256 = "sha256-uas1vqIDWlYr7jgsrlBeJSPjMxwzVnrkCD9jJljkFZs=";
+      sha256 = "sha256-YvQLG6DKXQs97GtC/szQ7fTOwOvwk1svJe6mEXTO8ns=";
     };
   });
 }
