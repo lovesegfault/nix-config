f567839a7e4f282a3bf384efc10dde4e836c0ba6
Bernardo Meurer <bernardo@meurer.org>
2021-03-02 21:29:53 -0800

    hardware/intel: fiddle with opengl

--- hardware/intel.nix
+++ hardware/intel.nix
@@ -5,9 +5,10 @@
     cpu.intel.updateMicrocode = true;
     opengl = {
       extraPackages = with pkgs; [
-        libva-full
-        intel-media-driver
-        intel-compute-runtime
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiVdpau
+        libvdpau-va-gl
       ];
     };
   };
