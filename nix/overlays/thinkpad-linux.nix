c418e72c33231de32c381a38c718ee0b4e677e6a
Bernardo Meurer <bernardo@meurer.org>
2021-03-09 10:00:59 -0800

    nix/overlays/thinkpad-linux: 5.11.3 -> 5.11.5

--- nix/overlays/thinkpad-linux.nix
+++ nix/overlays/thinkpad-linux.nix
@@ -1,11 +1,11 @@
 self: super: {
   thinkpadLinux = self.linux_5_11.override {
     argsOverride = rec {
-      version = "5.11.3";
+      version = "5.11.5";
       modDirVersion = version;
       src = self.fetchurl {
         url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-        sha256 = "sha256-NVjHC7OAV2PCUN87LCkiXO386ElIOv1Oy6J+Keyxy/4=";
+        sha256 = "sha256-IgjkYXtjqIk3P7P4XvVqc666g8EXUWv8yS70Swi9PWs=";
       };
     };
   };
