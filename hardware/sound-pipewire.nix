0a954219c5175ad34134de06f382b9a4b640f92d
Bernardo Meurer <bernardo@meurer.org>
2021-03-10 11:44:33 -0800

    hardware/sound-pipewire: try to configure clock rate

--- hardware/sound-pipewire.nix
+++ hardware/sound-pipewire.nix
@@ -8,14 +8,14 @@
     alsa.enable = true;
     pulse.enable = true;
     config = {
-      pipewire.context.properties.rate = "192000";
-      pipewire-pulse.stream.properties.resample.quality = 15;
-      client.stream.properties.resample.quality = 15;
-      client-rt.stream.properties.resample.quality = 15;
+      pipewire."context.properties"."default.clock.rate" = "192000";
+      pipewire-pulse."stream.properties"."resample.quality" = 15;
+      client."stream.properties"."resample.quality" = 15;
+      client-rt."stream.properties"."resample.quality" = 15;
     };
     media-session.config.bluez-monitor.properties = {
-      headset-roles = [ "hsp_hs" "hsp_ag" ];
-      codecs = [ "aac" "ldac" "aptx_hd" ];
+      "bluez5.headset-roles" = [ "hsp_hs" "hsp_ag" ];
+      "bluez5.codecs" = [ "aac" "ldac" "aptx_hd" ];
     };
   };
 
