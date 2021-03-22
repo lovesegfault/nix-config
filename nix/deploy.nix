a259e1b5ed940c491774a02ba75bf6850709362d
Bernardo Meurer <bernardo@meurer.org>
2021-03-22 00:56:24 -0700

    nix/hosts: spin-off nix/deploy

--- nix/deploy.nix
+++ nix/deploy.nix
@@ -10,14 +10,14 @@ let
   inherit (nixpkgs.lib) foldl' foldr;
   inherit (builtins) elemAt mapAttrs;
 
-  mkHost = name: system: import ./mk-host.nix { inherit inputs name system; };
+  mkHost = { name, system, ... }: import ./mk-host.nix { inherit inputs name system; };
 
-  mkPath = name: system: deploy-rs.lib.${system}.activate.nixos (mkHost name system);
+  mkPath = { name, system, ... }@host: deploy-rs.lib.${system}.activate.nixos (mkHost host);
 
-  mkNode = name: system: hostname: {
+  mkNode = { name, system, hostname, ... }@host: {
     ${name} = {
       inherit hostname;
-      profiles.system.path = mkPath name system;
+      profiles.system.path = mkPath host;
     };
   };
 in
@@ -30,18 +30,8 @@ in
       (a: b: a // b)
       { }
       (map
-        (s: foldl' (f: x: f x) mkNode s)
-        [
-          # [ "aurelius" "aarch64-linux" "aurelius" ]
-          [ "cantor" "x86_64-linux" "100.76.151.127" ]
-          [ "feuerbach" "x86_64-linux" "100.99.22.81" ]
-          [ "foucault" "x86_64-linux" "100.67.182.67" ]
-          [ "fourier" "x86_64-linux" "100.113.42.46" ]
-          [ "goethe" "aarch64-linux" "100.125.185.48" ]
-          [ "hegel" "x86_64-linux" "100.102.43.14" ]
-          # [ "riemann" "aarch64-linux" "100.99.75.64" ]
-          [ "sartre" "x86_64-linux" "100.97.215.77" ]
-        ]
+        (h: mkNode h)
+        (import ./hosts.nix)
       );
   };
 
---
+++ nix/hosts.nix
@@ -0,0 +1,11 @@
+[
+  # { name = "aurelius";  system = "aarch64-linux"; hostname = "aurelius"; }
+  { name = "cantor"; system = "x86_64-linux"; hostname = "100.76.151.127"; }
+  { name = "feuerbach"; system = "x86_64-linux"; hostname = "100.99.22.81"; }
+  { name = "foucault"; system = "x86_64-linux"; hostname = "100.67.182.67"; }
+  { name = "fourier"; system = "x86_64-linux"; hostname = "100.113.42.46"; }
+  { name = "goethe"; system = "aarch64-linux"; hostname = "100.125.185.48"; }
+  { name = "hegel"; system = "x86_64-linux"; hostname = "100.102.43.14"; }
+  # { name = "riemann";   system = "aarch64-linux"; hostname = "100.99.75.64"; }
+  { name = "sartre"; system = "x86_64-linux"; hostname = "100.97.215.77"; }
+]
