16c9c07d7097b15d8b0b1edbabd95206356c9cbc
Bernardo Meurer <bernardo@meurer.org>
2021-03-22 01:11:11 -0700

    nix/hosts: turn into attrset

--- nix/deploy.nix
+++ nix/deploy.nix
@@ -10,29 +10,21 @@ let
   inherit (nixpkgs.lib) foldl' foldr;
   inherit (builtins) elemAt mapAttrs;
 
-  mkHost = { name, system, ... }: import ./mk-host.nix { inherit inputs name system; };
+  mkHost = name: system: import ./mk-host.nix { inherit inputs name system; };
 
-  mkPath = { name, system, ... }@host: deploy-rs.lib.${system}.activate.nixos (mkHost host);
-
-  mkNode = { name, system, hostname, ... }@host: {
-    ${name} = {
-      inherit hostname;
-      profiles.system.path = mkPath host;
-    };
-  };
+  mkPath = name: system: deploy-rs.lib.${system}.activate.nixos (mkHost name system);
 in
 {
   deploy = {
     autoRollback = true;
     magicRollback = true;
     user = "root";
-    nodes = foldr
-      (a: b: a // b)
-      { }
-      (map
-        (h: mkNode h)
-        (import ./hosts.nix)
-      );
+    nodes = mapAttrs
+      (n: v: {
+        inherit (v) hostname;
+        profiles.system.path = mkPath n v.system;
+      })
+      (import ./hosts.nix);
   };
 
   checks = mapAttrs (_: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
--- nix/hosts.nix
+++ nix/hosts.nix
@@ -1,11 +1,38 @@
-[
-  # { name = "aurelius";  system = "aarch64-linux"; hostname = "aurelius"; }
-  { name = "cantor"; system = "x86_64-linux"; hostname = "100.76.151.127"; }
-  { name = "feuerbach"; system = "x86_64-linux"; hostname = "100.99.22.81"; }
-  { name = "foucault"; system = "x86_64-linux"; hostname = "100.67.182.67"; }
-  { name = "fourier"; system = "x86_64-linux"; hostname = "100.113.42.46"; }
-  { name = "goethe"; system = "aarch64-linux"; hostname = "100.125.185.48"; }
-  { name = "hegel"; system = "x86_64-linux"; hostname = "100.102.43.14"; }
-  # { name = "riemann";   system = "aarch64-linux"; hostname = "100.99.75.64"; }
-  { name = "sartre"; system = "x86_64-linux"; hostname = "100.97.215.77"; }
-]
+{
+  # aurelius = {
+  #   system = "aarch64-linux";
+  #   hostname = "aurelius";
+  # };
+  cantor = {
+    system = "x86_64-linux";
+    hostname = "100.76.151.127";
+  };
+  feuerbach = {
+    system = "x86_64-linux";
+    hostname = "100.99.22.81";
+  };
+  foucault = {
+    system = "x86_64-linux";
+    hostname = "100.67.182.67";
+  };
+  fourier = {
+    system = "x86_64-linux";
+    hostname = "100.113.42.46";
+  };
+  goethe = {
+    system = "aarch64-linux";
+    hostname = "100.125.185.48";
+  };
+  hegel = {
+    system = "x86_64-linux";
+    hostname = "100.102.43.14";
+  };
+  # riemann = {
+  #   system = "aarch64-linux";
+  #   hostname = "100.99.75.64";
+  # };
+  sartre = {
+    system = "x86_64-linux";
+    hostname = "100.97.215.77";
+  };
+}
