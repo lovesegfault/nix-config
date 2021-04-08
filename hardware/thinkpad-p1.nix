996b4cb1046d23bd8e34c261b91dd2b616fbe34a
Bernardo Meurer <bernardo@meurer.org>
2021-04-08 09:09:13 -0700

    hardware/thinkpad-p1: use lists in TLP config

--- hardware/thinkpad-p1.nix
+++ hardware/thinkpad-p1.nix
@@ -46,27 +46,28 @@
     tlp = {
       enable = true;
       settings = {
-        DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "bluetooth wifi";
-        DEVICES_TO_ENABLE_ON_AC = "bluetooth wifi";
+        DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = [ "bluetooth" "wifi" ];
+        DEVICES_TO_ENABLE_ON_AC = [ "bluetooth" "wifi" ];
 
-        DISK_APM_LEVEL_ON_AC = "\"255 254\"";
-        DISK_APM_LEVEL_ON_BAT = "\"128 1\"";
-        DISK_DEVICES = "\"nvme0n1 sda\"";
-        DISK_IOSCHED = "\"none bfq\"";
+        DISK_APM_LEVEL_ON_AC = [ "255" "255" ];
+        DISK_APM_LEVEL_ON_BAT = [ "128" "1" ];
+
+        DISK_DEVICES = [ "nvme0n1" "sda" ];
+        DISK_IOSCHED = [ "none" "bfq" ];
 
         MAX_LOST_WORK_SECS_ON_AC = 15;
         MAX_LOST_WORK_SECS_ON_BAT = 15;
 
         RUNTIME_PM_ON_AC = "auto";
         RUNTIME_PM_ON_BAT = "auto";
-        RUNTIME_PM_DRIVER_BLACKLIST = "\"\"";
+        RUNTIME_PM_DRIVER_BLACKLIST = [ ];
 
         SOUND_POWER_SAVE_ON_AC = "1";
         SOUND_POWER_SAVE_ON_BAT = "1";
         SOUND_POWER_SAVE_CONTROLLER = "Y";
 
-        #                sd-card   yubikey   wacom
-        USB_WHITELIST = "\"0bda:0328 1050:0407 056a:5193\"";
+        #                 sd-card     yubikey     wacom
+        USB_WHITELIST = [ "0bda:0328" "1050:0407" "056a:5193" ];
       };
     };
 
