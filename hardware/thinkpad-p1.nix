88467ef5ece2c7a59cd4f424aed8741999dd7b16
Bernardo Meurer <bernardo@meurer.org>
2021-04-07 19:05:50 -0700

    hardware/thinkpad-p1: fix DISK settings

--- hardware/thinkpad-p1.nix
+++ hardware/thinkpad-p1.nix
@@ -49,10 +49,10 @@
         DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "bluetooth wifi";
         DEVICES_TO_ENABLE_ON_AC = "bluetooth wifi";
 
-        DISK_APM_LEVEL_ON_AC = "255 254";
-        DISK_APM_LEVEL_ON_BAT = "128 1";
-        DISK_DEVICES = "nvme0n1 sda";
-        DISK_IOSCHED = "none bfq";
+        DISK_APM_LEVEL_ON_AC = "\"255 254\"";
+        DISK_APM_LEVEL_ON_BAT = "\"128 1\"";
+        DISK_DEVICES = "\"nvme0n1 sda\"";
+        DISK_IOSCHED = "\"none bfq\"";
 
         MAX_LOST_WORK_SECS_ON_AC = 15;
         MAX_LOST_WORK_SECS_ON_BAT = 15;
