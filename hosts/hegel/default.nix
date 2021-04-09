741fb9e35e6d0479bb99f465baa02bcd6f6e8a2e
Bernardo Meurer <bernardo@meurer.org>
2021-04-09 00:42:00 -0700

    hosts/hegel: persist signal state

--- hosts/hegel/default.nix
+++ hosts/hegel/default.nix
@@ -57,6 +57,7 @@
       "/home/bemeurer/.cache/thunderbird"
       "/home/bemeurer/.cache/zoom"
       "/home/bemeurer/.cache/zsh"
+      "/home/bemeurer/.config/Signal"
       "/home/bemeurer/.config/Slack"
       "/home/bemeurer/.config/TabNine"
       "/home/bemeurer/.config/coc"
