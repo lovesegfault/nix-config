e340ed7ba150c2087072717104a7d5e29f2e07ef
Bernardo Meurer <bernardo@meurer.org>
2021-04-19 13:38:26 -0700

    hosts/foucault: fmt

--- hosts/foucault/state.nix
+++ hosts/foucault/state.nix
@@ -53,23 +53,21 @@
     ];
   };
 
-  home-manager.users.bemeurer = { ... }: {
-    home.persistence."/state/home/bemeurer" = {
-      allowOther = true;
-      files = [
-        ".arcrc"
-        ".cache/.sway-launcher-desktop-wrapped-history.txt"
-        ".cache/cargo/credentials"
-        ".config/beets/config.yaml"
-        ".config/cachix/cachix.dhall"
-        ".config/gh/hosts.yml"
-        ".config/zoomus.conf"
-        ".gist-vim"
-        ".newsboat/cache.db"
-        ".newsboat/history.search"
-        ".vault-token"
-        ".wall"
-      ];
-    };
+  home-manager.users.bemeurer.home.persistence."/state/home/bemeurer" = {
+    allowOther = true;
+    files = [
+      ".arcrc"
+      ".cache/.sway-launcher-desktop-wrapped-history.txt"
+      ".cache/cargo/credentials"
+      ".config/beets/config.yaml"
+      ".config/cachix/cachix.dhall"
+      ".config/gh/hosts.yml"
+      ".config/zoomus.conf"
+      ".gist-vim"
+      ".newsboat/cache.db"
+      ".newsboat/history.search"
+      ".vault-token"
+      ".wall"
+    ];
   };
 }
--- hosts/foucault/sway.nix
+++ hosts/foucault/sway.nix
@@ -1,64 +1,62 @@
 {
-  home-manager.users.bemeurer = { ... }: {
-    wayland.windowManager.sway = {
-      extraSessionCommands = ''
-        export WLR_DRM_DEVICES=/dev/dri/card0
-      '';
-      config = {
-        input = {
-          "10730:258:Kinesis_Advantage2_Keyboard" = {
-            xkb_layout = "us";
-          };
-          "2:7:SynPS/2_Synaptics_TouchPad" = {
-            accel_profile = "adaptive";
-            click_method = "button_areas";
-            dwt = "disabled";
-            natural_scroll = "enabled";
-            scroll_method = "two_finger";
-            tap = "enabled";
-          };
-
-          "1739:0:Synaptics_TM3418-002" = {
-            accel_profile = "adaptive";
-            click_method = "button_areas";
-            dwt = "disabled";
-            natural_scroll = "enabled";
-            scroll_method = "two_finger";
-            tap = "enabled";
-          };
-
-          "2:10:TPPS/2_Elan_TrackPoint" = {
-            accel_profile = "adaptive";
-            dwt = "enabled";
-          };
-
-          "1133:16495:Logitech_MX_Ergo" = {
-            accel_profile = "adaptive";
-            click_method = "button_areas";
-            natural_scroll = "enabled";
-          };
-
-          "1133:45085:Logitech_MX_Ergo_Multi-Device_Trackball" = {
-            accel_profile = "adaptive";
-            click_method = "button_areas";
-            natural_scroll = "enabled";
-          };
+  home-manager.users.bemeurer.wayland.windowManager.sway = {
+    extraSessionCommands = ''
+      export WLR_DRM_DEVICES=/dev/dri/card0
+    '';
+    config = {
+      input = {
+        "10730:258:Kinesis_Advantage2_Keyboard" = {
+          xkb_layout = "us";
         };
-        output = {
-          "Unknown 0x32EB 0x00000000" = {
-            mode = "3840x2160@60Hz";
-            position = "0,0";
-            scale = "2";
-            subpixel = "rgb";
-          };
+        "2:7:SynPS/2_Synaptics_TouchPad" = {
+          accel_profile = "adaptive";
+          click_method = "button_areas";
+          dwt = "disabled";
+          natural_scroll = "enabled";
+          scroll_method = "two_finger";
+          tap = "enabled";
+        };
+
+        "1739:0:Synaptics_TM3418-002" = {
+          accel_profile = "adaptive";
+          click_method = "button_areas";
+          dwt = "disabled";
+          natural_scroll = "enabled";
+          scroll_method = "two_finger";
+          tap = "enabled";
+        };
+
+        "2:10:TPPS/2_Elan_TrackPoint" = {
+          accel_profile = "adaptive";
+          dwt = "enabled";
+        };
+
+        "1133:16495:Logitech_MX_Ergo" = {
+          accel_profile = "adaptive";
+          click_method = "button_areas";
+          natural_scroll = "enabled";
+        };
+
+        "1133:45085:Logitech_MX_Ergo_Multi-Device_Trackball" = {
+          accel_profile = "adaptive";
+          click_method = "button_areas";
+          natural_scroll = "enabled";
         };
       };
-      extraConfig = ''
-        bindswitch --locked --reload lid:on output eDP-1 disable
-        bindswitch --locked --reload lid:off output eDP-1 enable
-        focus output eDP-1
-        workspace 0:α
-      '';
+      output = {
+        "Unknown 0x32EB 0x00000000" = {
+          mode = "3840x2160@60Hz";
+          position = "0,0";
+          scale = "2";
+          subpixel = "rgb";
+        };
+      };
     };
+    extraConfig = ''
+      bindswitch --locked --reload lid:on output eDP-1 disable
+      bindswitch --locked --reload lid:off output eDP-1 enable
+      focus output eDP-1
+      workspace 0:α
+    '';
   };
 }
