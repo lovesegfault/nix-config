43cd644b30764728d64586119586c597db4d592a
Bernardo Meurer <bernardo@meurer.org>
2021-04-19 13:31:35 -0700

    hosts/foucault: split

--- hosts/foucault/default.nix
+++ hosts/foucault/default.nix
@@ -17,63 +17,12 @@
     ../../sway/trusted.nix
 
     ../../users/bemeurer
+
+    ./state.nix
+    ./sway.nix
   ];
 
   environment.systemPackages = with pkgs; [ cntr wireguard mullvad-vpn ];
-
-  environment.persistence."/state" = {
-    directories = [
-      "/var/lib/bluetooth"
-      "/var/lib/boltd"
-      "/var/lib/iwd"
-      "/var/lib/libvirt"
-      "/var/lib/tailscale"
-
-      "/home/bemeurer/.cache/lollypop"
-      "/home/bemeurer/.cache/mozilla"
-      "/home/bemeurer/.cache/nvim"
-      "/home/bemeurer/.cache/shotwell"
-      "/home/bemeurer/.cache/thunderbird"
-      "/home/bemeurer/.cache/zoom"
-      "/home/bemeurer/.cache/zsh"
-      "/home/bemeurer/.config/Slack"
-      "/home/bemeurer/.config/TabNine"
-      "/home/bemeurer/.config/coc"
-      "/home/bemeurer/.config/cog"
-      "/home/bemeurer/.config/dconf"
-      "/home/bemeurer/.config/discord"
-      "/home/bemeurer/.config/gcloud"
-      "/home/bemeurer/.config/gopass"
-      "/home/bemeurer/.config/pipewire"
-      "/home/bemeurer/.config/shotwell"
-      "/home/bemeurer/.gnupg"
-      "/home/bemeurer/.gsutil"
-      "/home/bemeurer/.local/share/TabNine"
-      "/home/bemeurer/.local/share/bash"
-      "/home/bemeurer/.local/share/iwctl"
-      "/home/bemeurer/.local/share/keyrings"
-      "/home/bemeurer/.local/share/lollypop"
-      "/home/bemeurer/.local/share/nvim"
-      "/home/bemeurer/.local/share/shotwell"
-      "/home/bemeurer/.local/share/zsh"
-      "/home/bemeurer/.mozilla"
-      "/home/bemeurer/.password-store"
-      "/home/bemeurer/.ssh"
-      "/home/bemeurer/.thunderbird"
-      "/home/bemeurer/.zoom"
-      "/home/bemeurer/documents"
-      "/home/bemeurer/opt"
-      "/home/bemeurer/src"
-      "/home/bemeurer/tmp"
-    ];
-    files = [
-      "/etc/machine-id"
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
-    ];
-  };
 
   fileSystems = {
     "/" = {
@@ -109,84 +58,6 @@
       ../../users/bemeurer/music
       ../../users/bemeurer/trusted
     ];
-    home.persistence."/state/home/bemeurer" = {
-      allowOther = true;
-      files = [
-        ".arcrc"
-        ".cache/.sway-launcher-desktop-wrapped-history.txt"
-        ".cache/cargo/credentials"
-        ".config/beets/config.yaml"
-        ".config/cachix/cachix.dhall"
-        ".config/gh/hosts.yml"
-        ".config/zoomus.conf"
-        ".gist-vim"
-        ".newsboat/cache.db"
-        ".newsboat/history.search"
-        ".vault-token"
-        ".wall"
-      ];
-    };
-
-    wayland.windowManager.sway = {
-      extraSessionCommands = ''
-        export WLR_DRM_DEVICES=/dev/dri/card0
-      '';
-      config = {
-        input = {
-          "10730:258:Kinesis_Advantage2_Keyboard" = {
-            xkb_layout = "us";
-          };
-          "2:7:SynPS/2_Synaptics_TouchPad" = {
-            accel_profile = "adaptive";
-            click_method = "button_areas";
-            dwt = "disabled";
-            natural_scroll = "enabled";
-            scroll_method = "two_finger";
-            tap = "enabled";
-          };
-
-          "1739:0:Synaptics_TM3418-002" = {
-            accel_profile = "adaptive";
-            click_method = "button_areas";
-            dwt = "disabled";
-            natural_scroll = "enabled";
-            scroll_method = "two_finger";
-            tap = "enabled";
-          };
-
-          "2:10:TPPS/2_Elan_TrackPoint" = {
-            accel_profile = "adaptive";
-            dwt = "enabled";
-          };
-
-          "1133:16495:Logitech_MX_Ergo" = {
-            accel_profile = "adaptive";
-            click_method = "button_areas";
-            natural_scroll = "enabled";
-          };
-
-          "1133:45085:Logitech_MX_Ergo_Multi-Device_Trackball" = {
-            accel_profile = "adaptive";
-            click_method = "button_areas";
-            natural_scroll = "enabled";
-          };
-        };
-        output = {
-          "Unknown 0x32EB 0x00000000" = {
-            mode = "3840x2160@60Hz";
-            position = "0,0";
-            scale = "2";
-            subpixel = "rgb";
-          };
-        };
-      };
-      extraConfig = ''
-        bindswitch --locked --reload lid:on output eDP-1 disable
-        bindswitch --locked --reload lid:off output eDP-1 enable
-        focus output eDP-1
-        workspace 0:α
-      '';
-    };
   };
 
   networking = {
