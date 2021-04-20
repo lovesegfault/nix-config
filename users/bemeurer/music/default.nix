6e86c27e9a4e82918b48aa27e51d29add7277f7e
Bernardo Meurer <bernardo@meurer.org>
2021-04-20 13:26:24 -0700

    users/bemeurer/music: add mediainfo

--- users/bemeurer/music/default.nix
+++ users/bemeurer/music/default.nix
@@ -2,8 +2,9 @@
   home.packages = with pkgs; [
     (beets.override { enableAlternatives = true; })
     bimp
+    essentia-extractor
     fixart
     imagemagick7
-    essentia-extractor
+    mediainfo
   ];
 }
