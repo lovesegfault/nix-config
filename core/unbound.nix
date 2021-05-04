cadfaa646981ff2ef987a8a82c015e3dae63da56
Bernardo Meurer <bernardo@meurer.org>
2021-05-04 10:39:24 -0700

    core/unbound: update to settings config format

--- core/unbound.nix
+++ core/unbound.nix
@@ -5,20 +5,24 @@
   services.unbound = {
     enable = true;
     enableRootTrustAnchor = true;
-    forwardAddresses = [ "1.1.1.1" "2606:4700:4700::1111" "8.8.8.8" "2001:4860:4860::8844" ];
-    extraConfig = ''
-      # This is part of the server clause
-      infra-cache-slabs: 16
-      ip-ratelimit-slabs: 16
-      key-cache-slabs: 16
-      msg-cache-size: 50m
-      msg-cache-slabs: 16
-      num-threads: 12
-      outgoing-range: 950
-      ratelimit-slabs: 16
-      rrset-cache-size: 100m
-      rrset-cache-slabs: 16
-      so-reuseport: yes
-    '';
+    settings = {
+      forward-zone = [{
+        name = ".";
+        forward-addr = [ "1.1.1.1" "2606:4700:4700::1111" "8.8.8.8" "2001:4860:4860::8844" ];
+      }];
+      server = {
+        infra-cache-slabs = 16;
+        ip-ratelimit-slabs = 16;
+        key-cache-slabs = 16;
+        msg-cache-size = "50m";
+        msg-cache-slabs = 16;
+        num-threads = 12;
+        outgoing-range = 950;
+        ratelimit-slabs = 16;
+        rrset-cache-size = "100m";
+        rrset-cache-slabs = 16;
+        so-reuseport = "yes";
+      };
+    };
   };
 }
