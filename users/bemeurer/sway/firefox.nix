f3448f603231aad1b90f65249f705397b21b4dc5
Bernardo Meurer <bernardo@meurer.org>
2021-05-06 16:52:30 -0700

    users/bemeurer/sway/firefox: only enable on x86_64-linux

--- users/bemeurer/sway/firefox.nix
+++ users/bemeurer/sway/firefox.nix
@@ -1,9 +1,6 @@
 { pkgs, ... }: {
   programs.firefox = {
-    enable = true;
-    package =
-      if pkgs.hostPlatform.system == "x86_64-linux"
-      then pkgs.firefox-bin
-      else pkgs.firefox;
+    enable = (pkgs.hostPlatform.system == "x86_64-linux");
+    package = pkgs.firefox-bin;
   };
 }
