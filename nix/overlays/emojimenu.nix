aa24e09cb7a2241c1f7f56adbe1400ea74a9756d
Bernardo Meurer <bernardo@meurer.org>
2021-05-12 21:49:36 -0700

    nix/overlays/menu/emojimenu -> nix/overlays/emojimenu

---
+++ nix/overlays/emojimenu.nix
@@ -0,0 +1,49 @@
+self: _: {
+  emojimenu = self.callPackage
+    (
+      { fetchurl
+      , jq
+      , runCommand
+      , wl-clipboard
+      , wofi
+      , writeSaneShellScriptBin
+      }:
+      let
+        emoji_json = fetchurl {
+          name = "emojis.json";
+          url = "https://raw.githubusercontent.com/github/gemoji/b1c7878afeb260d2ff6dc6655bf3aa7dee498e9c/db/emoji.json";
+          sha256 = "sha256-bL+4ft3yzVUnVxDg7cwoS2hvbeqEKvhx77QjEgR+Yhk=";
+        };
+        emojis = runCommand "emojis.txt"
+          { nativeBuildInputs = [ jq ]; } ''
+          cat ${emoji_json} | jq -r '.[] | "\(.emoji) \t   \(.description)"' | sed -e 's,\\t,\t,g' > $out
+        '';
+      in
+      writeSaneShellScriptBin {
+        name = "emojimenu";
+
+        buildInputs = [ wl-clipboard wofi ];
+
+        src = ''
+          if [ -z ''${XDG_CACHE_HOME+x} ]; then
+            cache_file="$HOME/.cache/emojimenu"
+          else
+            cache_file="$XDG_CACHE_HOME/emojimenu"
+          fi
+
+          emoji="$(wofi \
+            --show dmenu \
+            --cache-file="$cache_file" \
+            -D "allow_markup=false" \
+            < ${emojis} \
+            | cut -f1 -d" "\
+          )"
+
+          wl-copy -n <<< "$emoji"
+        '';
+      }
+    )
+    { };
+}
+
+
--- nix/overlays/menu/default.nix
+++ nix/overlays/menu/default.nix
@@ -1,6 +1,4 @@
 self: _:
 {
-  emojimenu = self.callPackage ./emojimenu.nix { };
-
   otpmenu = self.callPackage ./otpmenu.nix { };
 }
