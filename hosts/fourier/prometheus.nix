0e55c2da2ef7e7f0f8a1f8921cdae6993b088108
Bernardo Meurer <bernardo@meurer.org>
2021-05-14 19:35:25 -0700

    hosts/fourier: scrape other systems' node exporters

--- hosts/fourier/prometheus.nix
+++ hosts/fourier/prometheus.nix
@@ -4,16 +4,24 @@
   services.prometheus = {
     enable = true;
     extraFlags = [ "--storage.tsdb.retention.time=1y" ];
-    scrapeConfigs = [{
-      job_name = "node";
-      scrape_interval = "2500ms";
-      static_configs = [{ targets = [ "127.0.0.1:9091" ]; }];
-    }
+    scrapeConfigs = [
+      {
+        job_name = "node";
+        scrape_interval = "5s";
+        static_configs = [{
+          targets = [
+            "127.0.0.1:9091"
+            "cantor:9091"
+            "feuerbach:9091"
+          ];
+        }];
+      }
       {
         job_name = "prometheus";
         scrape_interval = "30s";
         static_configs = [{ targets = [ "127.0.0.1:9090" ]; }];
-      }];
+      }
+    ];
     exporters.node = {
       enable = true;
       listenAddress = "127.0.0.1";
