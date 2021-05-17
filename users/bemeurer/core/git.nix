07fb3497ec5a4282e8c3d00ae3a9e136d278b206
Bernardo Meurer <bernardo@meurer.org>
2021-05-16 22:43:44 -0700

    users/bemeure/core/git: new log alias

--- users/bemeurer/core/git.nix
+++ users/bemeurer/core/git.nix
@@ -29,7 +29,7 @@
     gdt = "git difftool";
     gdts = "git difftool --staged";
     gf = "git fetch --all --prune --tags";
-    gl = "git log --graph --decorate";
+    gl = "git log --decorate --pretty=format:'%C(auto)%h%d %C(green)(%as)%C(reset) %s'";
     gm = "git merge";
     gma = "git merge --abort";
     gmc = "git merge --continue";
