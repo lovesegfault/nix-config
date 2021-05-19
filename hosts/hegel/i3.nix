138d5f63fb8e4bd62a678abd06a6addc969c6ed0
Bernardo Meurer <bernardo@meurer.org>
2021-05-19 01:32:53 -0700

    hosts/hegel: correctly set monitor position

--- hosts/hegel/i3.nix
+++ hosts/hegel/i3.nix
@@ -1,7 +1,22 @@
 {
   home-manager.users.bemeurer.xsession.windowManager.i3.config.startup = [
     {
-      command = "xrandr --output DisplayPort-1 --auto --rotate left --right-of DisplayPort-0";
+      command = ''
+        xrandr \
+            --output DisplayPort-0 \
+                --primary \
+                --mode 3840x2160 \
+                --pos 0x840 \
+                --rotate normal \
+            --output DisplayPort-1 \
+                --mode 3840x2160 \
+                --pos 3840x0 \
+                --rotate left \
+            --output DisplayPort-2 \
+                --off \
+            --output HDMI-A-0 \
+                --off
+      '';
       always = true;
       notification = false;
     }
