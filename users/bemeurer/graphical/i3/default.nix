3296bebdd93c17d260956d2666b9d8367aaa7e11
Bernardo Meurer <bernardo@meurer.org>
2021-05-19 02:06:51 -0700

    users/bemeurer/graphical/i3: set MOZ_USE_XINPUT2 and _JAVA_OPTIONS

--- users/bemeurer/graphical/i3/default.nix
+++ users/bemeurer/graphical/i3/default.nix
@@ -8,7 +8,12 @@
 
   xsession = {
     enable = true;
-    profileExtra = "xrdb ~/.Xresources";
+    profileExtra = ''
+      export MOZ_USE_XINPUT2=1
+      export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true"
+
+      xrdb ~/.Xresources
+    '';
     pointerCursor.size = lib.mkForce 16;
   };
 }
