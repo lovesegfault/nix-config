9d7535f5f807ba198698c0760cfbb6b341d37d04
Bernardo Meurer <bernardo@meurer.org>
2021-05-18 21:53:56 -0700

    nix/overlays/drunmenu: prepare for x11 split

---
+++ nix/overlays/drunmenu.nix
@@ -0,0 +1,46 @@
+let
+  drunmenu =
+    { spawn
+    , writeSaneShellScriptBin
+
+    , displayCmd
+    , extraInputs ? [ ]
+    }:
+    writeSaneShellScriptBin {
+      name = "drunmenu";
+
+      buildInputs = [ spawn ] ++ extraInputs;
+
+      src = ''
+        program="$(${displayCmd})"
+
+        spawn "$program"
+      '';
+    };
+in
+self: _: {
+  drunmenu-wayland = self.callPackage drunmenu {
+    displayCmd = ''
+      wofi \
+        --allow-images \
+        --allow-markup \
+        --insensitive \
+        --define "drun-print_command=true" \
+        --term=foot \
+        --cache-file="$cache_file" \
+        --show=drun |
+        sed "s/%[a-zA-Z]//g"
+    '';
+    extraInputs = with self; [ gnused wofi ];
+  };
+
+  drunmenu-x11 = self.callPackage drunmenu {
+    displayCmd = ''
+      rofi \
+        -cache-dir "$XDG_CACHE_HOME/rofi/drunmenu" \
+        -run-command "echo {cmd}" \
+        -show drun
+    '';
+    extraInputs = with self; [ rofi ];
+  };
+}
