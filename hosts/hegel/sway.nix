88e5a0e08303616b2517894d22caa8aacb73ff81
Bernardo Meurer <bernardo@meurer.org>
2021-05-19 00:15:22 -0700

    hosts/hegel/sway: use /etc/sway/config.d

--- hosts/hegel/sway.nix
+++ hosts/hegel/sway.nix
@@ -1,48 +1,47 @@
 {
-  home-manager.users.bemeurer.wayland.windowManager.sway = {
-    extraSessionCommands = ''
-      export WLR_DRM_DEVICES=/dev/dri/card0
-    '';
-    config = {
-      input = {
-        "10730:258:Kinesis_Advantage2_Keyboard" = {
-          xkb_layout = "us";
-        };
+  environment.etc."sway/config.d/inputs.conf".text = ''
+    input "10730:258:Kinesis_Advantage2_Keyboard" {
+      xkb_layout us
+    }
 
-        "1133:16511:Logitech_G502" = {
-          accel_profile = "adaptive";
-          natural_scroll = "enabled";
-        };
+    input "1133:16495:Logitech_MX_Ergo" {
+      accel_profile adaptive
+      natural_scroll enabled
+    }
 
-        "1133:16495:Logitech_MX_Ergo" = {
-          accel_profile = "adaptive";
-          natural_scroll = "enabled";
-        };
+    input "1133:16511:Logitech_G502" {
+      accel_profile adaptive
+      natural_scroll enabled
+    }
 
-        "1133:45085:Logitech_MX_Ergo_Multi-Device_Trackball" = {
-          accel_profile = "adaptive";
-          natural_scroll = "enabled";
-        };
-      };
-      output = {
-        "Goldstar Company Ltd LG Ultra HD 0x00000B08" = {
-          adaptive_sync = "on";
-          mode = "3840x2160@60Hz";
-          position = "0,720";
-          subpixel = "rgb";
-        };
-        "Goldstar Company Ltd LG Ultra HD 0x00009791" = {
-          adaptive_sync = "on";
-          mode = "3840x2160@60Hz";
-          position = "3840,0";
-          subpixel = "rgb";
-          transform = "270";
-        };
-      };
-    };
-    extraConfig = ''
-      focus output DP-1
-      workspace 0:α
-    '';
-  };
+    input "1133:45085:Logitech_MX_Ergo_Multi-Device_Trackball" {
+      accel_profile adaptive
+      natural_scroll enabled
+    }
+  '';
+
+  environment.etc."sway/config.d/outputs.conf".text = ''
+    output "Goldstar Company Ltd LG Ultra HD 0x00000B08" {
+      adaptive_sync on
+      mode 3840x2160@60Hz
+      position 0,720
+      subpixel rgb
+    }
+
+    output "Goldstar Company Ltd LG Ultra HD 0x00009791" {
+      adaptive_sync on
+      mode 3840x2160@60Hz
+      position 3840,0
+      subpixel rgb
+      transform 270
+    }
+
+    workspace 0 output "Goldstar Company Ltd LG Ultra HD 0x00000B08"
+    workspace 1 output "Goldstar Company Ltd LG Ultra HD 0x00009791"
+    focus output "Goldstar Company Ltd LG Ultra HD 0x00000B08"
+  '';
+
+  home-manager.users.bemeurer.wayland.windowManager.sway.extraSessionCommands = ''
+    export WLR_DRM_DEVICES=/dev/dri/card0
+  '';
 }
