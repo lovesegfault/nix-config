8b127a57fc9c768d70a185ff286df0b7451b5848
Bernardo Meurer <bernardo@meurer.org>
2021-05-21 11:41:28 -0700

    users/bemeurer/graphical/i3/dunst: match mako

--- users/bemeurer/graphical/i3/dunst.nix
+++ users/bemeurer/graphical/i3/dunst.nix
@@ -1,22 +1,31 @@
-{ pkgs, ... }: {
+{ config, ... }: {
   services.dunst = {
     enable = true;
-    iconTheme = {
-      name = "Adwaita";
-      package = pkgs.gnome3.adwaita-icon-theme;
-      size = "32x32";
-    };
     settings = {
       global = {
+        browser = "${config.programs.firefox.package}/bin/firefox";
+        dmenu = "${config.programs.rofi.package}/bin/rofi -dmenu -p dunst";
         follow = "keyboard";
         font = "Hack 10";
         format = ''<b>%s</b>\n%b'';
         frame_color = "#53BDFA";
-        geometry = "500x100-5+5";
-        horizontal_padding = 5;
-        line_height = 4;
-        padding = 5;
-        transparency = 100;
+        frame_width = 1;
+        geometry = "500x50-15+15";
+        hide_duplicates_count = "yes";
+        horizontal_padding = 6;
+        icon_position = "left";
+        idle_threshold = 0;
+        ignore_newline = "no";
+        line_height = 3;
+        markup = "yes";
+        max_icon_size = 80;
+        padding = 6;
+        plain_text = "no";
+        separator_color = "frame";
+        separator_height = 2;
+        shrink = "no";
+        stack_duplicates = "yes";
+        word_wrap = "yes";
       };
       urgency_normal = {
         background = "#0A0E14";
