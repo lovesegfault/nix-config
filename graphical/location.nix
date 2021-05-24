4a6947607f545a3a8f5bb13b09794b53a02d8c5a
Bernardo Meurer <bernardo@meurer.org>
2021-05-23 22:22:32 -0700

    graphical/location: allow gammastep

--- graphical/location.nix
+++ graphical/location.nix
@@ -1,4 +1,10 @@
 {
   location.provider = "geoclue2";
-  services.geoclue2.enable = true;
+  services.geoclue2 = {
+    enable = true;
+    appConfig.gammastep = {
+      isAllowed = true;
+      isSystem = true;
+    };
+  };
 }
