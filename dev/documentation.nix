ad5ccb54a0c99f981a7cb4fb3423a87147b7ee0c
Bernardo Meurer <bernardo@meurer.org>
2021-05-23 23:23:45 -0700

    dev/documentation: work around https://github.com/NixOS/nixpkgs/issues/124215

--- dev/documentation.nix
+++ dev/documentation.nix
@@ -1,7 +1,8 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   environment.systemPackages = with pkgs; [ man-pages ];
   documentation = {
     dev.enable = true;
     man.enable = true;
+    info.enable = lib.mkForce false;
   };
 }
