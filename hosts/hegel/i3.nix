4810bd126785b8efc71a1026673f64a1ef9672c6
Bernardo Meurer <bernardo@meurer.org>
2021-05-27 00:26:31 -0700

    fix(hosts/hegel/i3): bind workspaces 1/10 to DP-0/1 respective and match sway behavior

--- hosts/hegel/i3.nix
+++ hosts/hegel/i3.nix
@@ -1,17 +1,24 @@
 { pkgs, ... }: {
-  home-manager.users.bemeurer.systemd.user.services.xrandr = {
-    Unit = {
-      Description = "XRanR Display Configuration";
-      PartOf = [ "i3-session.target" ];
-      After = [ "i3-session.target" ];
+  home-manager.users.bemeurer = {
+    systemd.user.services.xrandr = {
+      Unit = {
+        Description = "XRanR Display Configuration";
+        PartOf = [ "i3-session.target" ];
+        After = [ "i3-session.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.xorg.xrandr}/bin/xrandr --output DisplayPort-0 --primary --mode 3840x2160 --pos 0x840 --rotate normal --output DisplayPort-1 --mode 3840x2160 --pos 3840x0 --rotate left --output DisplayPort-2 --off --output HDMI-A-0 --off";
+        RemainAfterExit = true;
+        Type = "oneshot";
+      };
+      Install = {
+        WantedBy = [ "i3-session.target" ];
+      };
     };
-    Service = {
-      ExecStart = "${pkgs.xorg.xrandr}/bin/xrandr --output DisplayPort-0 --primary --mode 3840x2160 --pos 0x840 --rotate normal --output DisplayPort-1 --mode 3840x2160 --pos 3840x0 --rotate left --output DisplayPort-2 --off --output HDMI-A-0 --off";
-      RemainAfterExit = true;
-      Type = "oneshot";
-    };
-    Install = {
-      WantedBy = [ "i3-session.target" ];
-    };
+
+    xsession.windowManager.i3.extraConfig = ''
+      workspace 1 output DisplayPort-0
+      workspace 10 output DisplayPort-1
+    '';
   };
 }
