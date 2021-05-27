2e9b5cfb449319258052790757fa8e7c17e38918
Bernardo Meurer <bernardo@meurer.org>
2021-05-26 19:51:48 -0700

    graphical/gnome: enable enough for calendar to work

--- graphical/gnome.nix
+++ graphical/gnome.nix
@@ -1,7 +1,12 @@
 {
   programs.dconf.enable = true;
-  services.gnome = {
-    gnome-keyring.enable = true;
-    gnome-online-accounts.enable = true;
+  services = {
+    accounts-daemon.enable = true;
+    gnome = {
+      evolution-data-server.enable = true;
+      gnome-keyring.enable = true;
+      gnome-online-accounts.enable = true;
+      gnome-online-miners.enable = true;
+    };
   };
 }
