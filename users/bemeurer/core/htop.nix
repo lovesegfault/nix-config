a8b9e356e06059dd79f9ea022e6080ea5807b2d6
Bernardo Meurer <bernardo@meurer.org>
2021-06-03 12:54:50 -0700

    fix(users/bemeurer/core/htop): use new syntax for configuring bars

--- users/bemeurer/core/htop.nix
+++ users/bemeurer/core/htop.nix
@@ -4,20 +4,16 @@
     settings = {
       delay = 10;
       show_program_path = false;
-    } // (with config.lib.htop;
-      leftMeters {
-        AllCPUs2 = modes.Bar;
-        Memory = modes.Bar;
-        Swap = modes.Bar;
-      }
-    ) // (with config.lib.htop;
-      rightMeters {
-        Hostname = modes.Text;
-        Tasks = modes.Text;
-        LoadAverage = modes.Text;
-        Uptime = modes.Text;
-        Systemd = modes.Text;
-      }
-    );
+    } // (with config.lib.htop; leftMeters [
+      (bar "AllCPUs2")
+      (bar "Memory")
+      (bar "Swap")
+    ]) // (with config.lib.htop; rightMeters [
+      (text "Hostname")
+      (text "Tasks")
+      (text "LoadAverage")
+      (text "Uptime")
+      (text "Systemd")
+    ]);
   };
 }
