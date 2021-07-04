f5093784d8c635649d9aa8dd3445c6265d03ca35
Bernardo Meurer <bernardo@meurer.org>
2021-07-03 21:21:15 -0700

    fix(hosts/goethe/dhcpd4): write config verbatim usinb configFile

--- hosts/goethe/dhcpd4.nix
+++ hosts/goethe/dhcpd4.nix
@@ -1,11 +1,19 @@
-{
+{ pkgs, ... }: {
   services.dhcpd4 = {
     enable = true;
-    extraConfig = ''
+    configFile = pkgs.writeText "dhcpd4.conf" ''
       subnet 192.168.2.0 netmask 255.255.255.0 {
         authoritative;
+        range 192.168.2.1 192.168.2.254;
+        default-lease-time 3600;
+        max-lease-time 3600;
         option subnet-mask 255.255.255.0;
-        range 192.168.2.2 192.168.2.254;
+        option broadcast-address 192.168.2.255;
+
+        host goethe {
+          hardware ethernet dc:a6:32:a7:29:c3;
+          fixed-address 192.168.2.1;
+        }
       }
     '';
     interfaces = [ "eth0" ];
