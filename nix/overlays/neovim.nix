552132dcb5929242da1ab67d34b8884c7f06b325
Bernardo Meurer <bernardo@meurer.org>
2021-07-08 13:17:47 -0700

    feat(nix/overlays/neovim): heavily optimize neovim build

---
+++ nix/overlays/neovim.nix
@@ -0,0 +1,34 @@
+let
+  optimizeDrv = drv: drv.overrideAttrs (old: rec {
+    _OPT_DRV_FLAGS = [
+      # Enable all safe optimizations
+      "-O3"
+      # Graphite specific optimizations and other optimizations that are
+      # disabled at O3 but don't influence the compiler's judgement.
+      "-fgraphite-identity"
+      "-floop-nest-optimize"
+      # This option increases compile times, but can potentially produce better
+      # binaries, especially with LTO. Essentially, it allows the compiler to
+      # look into called function bodies when performing alias analysis
+      "-fipa-pta"
+      # This allows GCC to perform devirtualization across object file
+      # boundaries using LTO.
+      "-fdevirtualize-at-ltrans"
+      # This option omits the PLT from the executable, making calls go through
+      # the GOT directly. It inhibits lazy binding, so this is not enabled by
+      # default. If you use prelink, this is strictly better than lazy binding.
+      "-fno-plt"
+      # Safe subset of -ffast-math
+      # INFO: https://gcc.gnu.org/ml/gcc/2017-09/msg00079.html
+      "-fno-math-errno"
+      "-fno-trapping-math"
+    ];
+
+    NIX_CFLAGS_COMPILE = (old.NIX_CFLAGS_COMPILE or "") + (toString _OPT_DRV_FLAGS);
+    NIX_CFLAGS_LINK = (old.NIX_CFLAGS_LINK or "") + (toString _OPT_DRV_FLAGS);
+  });
+in
+_: super:
+{
+  neovim-unwrapped = optimizeDrv super.neovim-unwrapped;
+}
