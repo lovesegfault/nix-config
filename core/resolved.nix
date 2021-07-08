956770f6f93b216fb298195743a0d3b41b401e9a
Bernardo Meurer <bernardo@meurer.org>
2021-07-07 18:33:38 -0700

    fix(core/resolved): force usage of known DNS servers

--- core/resolved.nix
+++ core/resolved.nix
@@ -4,9 +4,10 @@
   services.resolved = {
     enable = true;
     dnssec = "allow-downgrade";
-    llmnr = "false";
     extraConfig = ''
-      FallbackDNS=1.1.1.1 2606:4700:4700::1111 8.8.8.8 2001:4860:4860::8844
+      DNS=1.1.1.1 2606:4700:4700::1111 8.8.8.8 2001:4860:4860::8844
+      Domains=~.
+      DNSOverTLS=opportunistic
     '';
   };
 }
