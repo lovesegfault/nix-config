5eccca90eb975b4d5ed2368a8f63b50abd9089c0
Bernardo Meurer <bernardo@meurer.org>
2021-07-07 20:24:47 -0700

    feat(users/bemeurer/core/neovim): fix indent_blankline's char

--- users/bemeurer/core/neovim/lua/lovesegfault/indent-blankline.lua
+++ users/bemeurer/core/neovim/lua/lovesegfault/indent-blankline.lua
@@ -1,4 +1,5 @@
 local g = vim.g
-g.indent_blankline_filetype_exclude = {'help', 'packer'}
+g.indent_blankline_char = "▏";
+g.indent_blankline_filetype_exclude = {'help'}
 g.indent_blankline_buftype_exclude = {'terminal', 'nofile'}
 g.indent_blankline_char_highlight = 'LineNr'
