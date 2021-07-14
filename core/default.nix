8ba8c3a1e34538c90dfff2bd2c042f2825e0f5de
Bernardo Meurer <bernardo@meurer.org>
2021-07-13 18:45:17 -0700

    feat(core): add bpytop to systemPackages

--- core/default.nix
+++ core/default.nix
@@ -24,7 +24,7 @@ in
 
   environment = {
     etc."nixos/configuration.nix".source = dummyConfig;
-    systemPackages = with pkgs; [ rsync foot.terminfo ];
+    systemPackages = with pkgs; [ rsync foot.terminfo bpytop ];
   };
 
   home-manager = {
