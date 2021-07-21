789361eebdc5c1584197f7094060b5873c7ccf3b
Bernardo Meurer <bernardo@meurer.org>
2021-07-21 10:42:59 -0700

    refactor(users/bemeurer/core/starship): use settings instead of writeText

--- users/bemeurer/core/starship.nix
+++ users/bemeurer/core/starship.nix
@@ -1,35 +1,28 @@
-{
+{ lib, ... }: {
   programs.starship = {
     enable = true;
     enableBashIntegration = false;
     enableFishIntegration = false;
-  };
-
-  xdg.configFile.starship = {
-    target = "starship.toml";
-    text = ''
-      add_newline = false
-
-      format = """\
-        $username\
-        $hostname\
-        $directory\
-        $git_branch\
-        $git_commit\
-        $git_state\
-        $git_status\
-        $package\
-        $haskell\
-        $python\
-        $rust\
-        $nix_shell\
-        $line_break\
-        $jobs\
-        $character\
-      """
-
-      [git_status]
-      disabled=true
-    '';
+    settings = {
+      add_newline = false;
+      format = lib.concatStrings [
+        "$username"
+        "$hostname"
+        "$directory"
+        "$git_branch"
+        "$git_commit"
+        "$git_state"
+        "$git_status"
+        "$package"
+        "$haskell"
+        "$python"
+        "$rust"
+        "$nix_shell"
+        "$line_break"
+        "$jobs"
+        "$character"
+      ];
+      nix_shell.symbol = "❄️";
+    };
   };
 }
