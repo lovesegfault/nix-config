978584a070939fe060767878a2bdbf95ccd58a8e
Bernardo Meurer <bernardo@meurer.org>
2021-08-04 17:22:32 -0700

    refactor(hosts/deleuze): remove pi-hole setup (moved to fourier)

--- hosts/deleuze/default.nix
+++ hosts/deleuze/default.nix
@@ -1,4 +1,3 @@
-{ lib, pkgs, ... }:
 {
   imports = [
     ../../core
@@ -6,46 +5,19 @@
     ../../hardware/rpi4.nix
 
     ../../users/bemeurer
-
-    ./unbound.nix
-    ./pi-hole.nix
   ];
 
-  console = {
-    font = "ter-v28n";
-    packages = with pkgs; [ terminus_font ];
-  };
-
-  networking = {
-    hostName = "deleuze";
-    firewall = {
-      allowedTCPPorts = [ 80 53 ];
-      allowedUDPPorts = [ 53 ];
-    };
-  };
+  networking.hostName = "deleuze";
 
   nix.gc = {
     automatic = true;
     options = "-d";
   };
 
-  services = {
-    chrony = {
-      enable = true;
-      servers = [ "time.nist.gov" "time.cloudflare.com" "time.google.com" "tick.usnogps.navy.mil" ];
-      extraConfig = ''
-        allow 10.0.0.0/24
-      '';
-    };
-    timesyncd.enable = lib.mkForce false;
-  };
-
   systemd.network.networks.lan = {
     DHCP = "yes";
     matchConfig.MACAddress = "dc:a6:32:c1:37:1b";
   };
-
-  systemd.services.unbound.after = [ "chronyd.service" ];
 
   time.timeZone = "America/Los_Angeles";
 
