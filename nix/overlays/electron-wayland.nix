3928363317b2eeebd14d9b0c8385b221c2ee335b
Bernardo Meurer <bernardo@meurer.org>
2021-08-25 12:31:08 -0700

    feat(nix/overlays/electron-wayland): make electron apps use wayland

---
+++ nix/overlays/electron-wayland.nix
@@ -0,0 +1,17 @@
+self:
+let
+  enableWayland = drv: bin: drv.overrideAttrs (old: {
+    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.makeWrapper ];
+    installPhase = (old.installPhase or "") + ''
+      wrapProgram $out/bin/${bin} \
+        --add-flags "--enable-features=UseOzonePlatform" \
+        --add-flags "--ozone-platform=wayland"
+    '';
+  });
+in
+super:
+{
+  slack = enableWayland super.slack "slack";
+  signal-desktop = enableWayland super.signal-desktop "signal-desktop";
+  element-desktop = enableWayland super.element-desktop "element-desktop";
+}
