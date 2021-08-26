f49a438d24f64d74c306852846967dd384650fb2
Bernardo Meurer <bernardo@meurer.org>
2021-08-26 16:58:57 -0700

    fix(nix/overlays/electron-wayland): move additional wrap to postFixup

--- nix/overlays/electron-wayland.nix
+++ nix/overlays/electron-wayland.nix
@@ -2,7 +2,7 @@ self:
 let
   enableWayland = drv: bin: drv.overrideAttrs (old: {
     nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.makeWrapper ];
-    installPhase = (old.installPhase or "") + ''
+    postFixup = (old.postFixup or "") + ''
       wrapProgram $out/bin/${bin} \
         --add-flags "--enable-features=UseOzonePlatform" \
         --add-flags "--ozone-platform=wayland"
