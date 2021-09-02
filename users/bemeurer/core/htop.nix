09b4dd927e44131e564ee2f8b3e631f24e90f001
Bernardo Meurer <bernardo@meurer.org>
2021-09-01 21:15:10 -0700

    refactor(users/bemeurer/core/htop): enable temp/freq reporting

--- users/bemeurer/core/htop.nix
+++ users/bemeurer/core/htop.nix
@@ -4,6 +4,9 @@
     settings = {
       delay = 10;
       show_program_path = false;
+      show_cpu_frequency = true;
+      show_cpu_temperature = true;
+      hide_kernel_threads = true;
     } // (with config.lib.htop; leftMeters [
       (bar "AllCPUs2")
       (bar "Memory")
