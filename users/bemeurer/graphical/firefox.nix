c5adfa3adfd4391a135c52b5f7d8386762108e81
Bernardo Meurer <bernardo@meurer.org>
2021-09-13 08:58:42 -0700

    revert: "refactor(users/bemeurer/graphical/firefox): use firefox-wayland"
    This reverts commit 2444057c5e0ccf56912315a779d84676d59a600d.

--- users/bemeurer/graphical/firefox.nix
+++ users/bemeurer/graphical/firefox.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   programs.firefox = {
     enable = (pkgs.hostPlatform.system == "x86_64-linux");
-    package = pkgs.firefox-wayland;
+    package = pkgs.firefox-bin;
   };
 
   xdg.mimeApps.defaultApplications = {
