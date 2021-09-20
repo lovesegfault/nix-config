2163058c7fba62c5745cc9d737bb598a70e5ad20
Bernardo Meurer <bernardo@meurer.org>
2021-09-19 18:41:10 -0700

    feat(users/bemeurer/dev): unify direnv nix cache location

--- users/bemeurer/dev/default.nix
+++ users/bemeurer/dev/default.nix
@@ -17,6 +17,16 @@
         enable = true;
         enableFlakes = true;
       };
+      stdlib = ''
+        : ''${XDG_CACHE_HOME:=$HOME/.cache}
+        declare -A direnv_layout_dirs
+        direnv_layout_dir() {
+            echo "''${direnv_layout_dirs[$PWD]:=$(
+                echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+                echo -n "$PWD" | shasum | cut -d ' ' -f 1
+            )}"
+        }
+      '';
     };
 
     gh = {
