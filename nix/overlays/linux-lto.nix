29627feb88f3cfbaa18258926f28ac82dd9e32fb
Bernardo Meurer <bernardo@meurer.org>
2021-09-26 13:21:33 -0700

    fix(nix/overlays/linux-lto): disable DEBUG_INFO_BTF

--- nix/overlays/linux-lto.nix
+++ nix/overlays/linux-lto.nix
@@ -57,6 +57,8 @@ let
       argsOverride.structuredExtraConfig = kernel.structuredExtraConfig // {
         LTO_CLANG_FULL = yes;
         LTO_NONE = no;
+        # XXX: https://www.mail-archive.com/linux-kernel@vger.kernel.org/msg2519405.html
+        DEBUG_INFO_BTF = lib.mkForce no;
       } // extraConfig;
     };
 
