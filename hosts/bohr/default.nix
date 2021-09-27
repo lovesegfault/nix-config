3fb6651f636432cc8a9624524d1c46c34b94e15a
Bernardo Meurer <bernardo@meurer.org>
2021-09-27 12:44:00 -0700

    fix(hosts/bohr): disable lowlatency USB audio

--- hosts/bohr/default.nix
+++ hosts/bohr/default.nix
@@ -17,9 +17,12 @@
   boot = {
     initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
     kernelModules = [ "kvm-amd" ];
-    kernelPackages = pkgs.linuxPackages;
+    kernelPackages = pkgs.linuxPackages_xanmod_lto_zen3;
     tmpOnTmpfs = true;
     zfs.extraPools = [ "tank" ];
+    extraModprobeConfig = ''
+      options snd_usb_audio lowlatency=0
+    '';
   };
 
   fileSystems = {
