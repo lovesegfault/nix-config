5e7297d1271cbab8f980360818d6caddd85ba0ec
Bernardo Meurer <bernardo@meurer.org>
2021-10-01 01:23:08 -0700

    refactor(users/bemeurer/core/neovim): add lua lsp support

--- users/bemeurer/core/neovim/lua/lovesegfault/lsp.lua
+++ users/bemeurer/core/neovim/lua/lovesegfault/lsp.lua
@@ -41,6 +41,35 @@ for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup({ on_attach = on_attach, capabilities = capabilities, flags = flags })
 end
 
+nvim_lsp["sumneko_lua"].setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  flags = flags,
+  cmd = { "lua-language-server" },
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+        -- Setup your lua path
+        -- path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { "vim" },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
+
 -- Map :Format to vim.lsp.buf.formatting()
 vim.cmd([[ command! Format execute "lua vim.lsp.buf.formatting()" ]])
 
