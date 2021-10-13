162b27dfd55be31769eb6729b8438c3e879a8367
Bernardo Meurer <bernardo@meurer.org>
2021-10-13 12:05:05 -0700

    feat(dev): enable dictd

--- dev/default.nix
+++ dev/default.nix
@@ -6,6 +6,8 @@
     systemPackages = with pkgs; [ git neovim tmate upterm nixpkgs-review nix-update ];
   };
 
+  services.dictd.enable = true;
+
   services.udev.extraRules = ''
     ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6010", MODE="0660", GROUP="dialout", TAG+="uaccess"
   '';
