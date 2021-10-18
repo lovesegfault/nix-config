3c8d8d70ed4dac32ae054722ecc5a9c48067c46b
Bernardo Meurer <bernardo@meurer.org>
2021-10-18 13:20:22 -0700

    fix(users/bemeurer/core/neovim): bind <Right> to closing compe

--- users/bemeurer/core/neovim/lua/lovesegfault/cmp.lua
+++ users/bemeurer/core/neovim/lua/lovesegfault/cmp.lua
@@ -19,6 +19,7 @@ cmp.setup({
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
+    ["<Right>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
