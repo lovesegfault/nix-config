5212c648c6f68dc56a0eac213caf5f2ed53b07d7
Bernardo Meurer <bernardo@meurer.org>
2021-10-21 15:22:36 -0700

    feat(hosts/bohr): revert to hqplayerd in NixOS

--- hosts/bohr/hqplayerd.nix
+++ hosts/bohr/hqplayerd.nix
@@ -1,20 +1,5 @@
 {
   networking.firewall.allowedTCPPorts = [ 8088 4321 ];
 
-  virtualisation.oci-containers.containers.hqplayerd = {
-    autoStart = true;
-    image = "lovesegfault/hqplayerd:latest";
-    volumes = [
-      "/etc/hqplayer:/etc/hqplayer"
-      "/var/lib/hqplayer:/var/lib/hqplayer"
-      "/srv/music:/srv/music:ro"
-    ];
-    extraOptions = [
-      "--network=host"
-      "--device=/dev/snd"
-      "--ulimit=rtprio=99"
-      "--cap-add=sys_nice"
-      "--gpus=all"
-    ];
-  };
+  services.hqplayerd.enable = true;
 }
