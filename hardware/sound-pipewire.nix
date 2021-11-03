52fcbcaf1f12607aa842f328eabd6f8467d75e8f
Bernardo Meurer <bernardo@meurer.org>
2021-11-02 21:21:26 -0700

    refactor(hardware/sound-pipewire): set allowed-rates

--- hardware/sound-pipewire.nix
+++ hardware/sound-pipewire.nix
@@ -9,7 +9,18 @@
     jack.enable = true;
     pulse.enable = true;
     config = {
-      pipewire."context.properties"."default.clock.rate" = "192000";
+      pipewire."context.properties"."default.clock.allowed-rates" = [
+        44100
+        48000
+        88200
+        96000
+        176400
+        192000
+        358000
+        384000
+        716000
+        768000
+      ];
       pipewire-pulse."stream.properties"."resample.quality" = 15;
       client."stream.properties"."resample.quality" = 15;
       client-rt."stream.properties"."resample.quality" = 15;
