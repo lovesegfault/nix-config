772f62a6c784ef29047e5bc0443cff6ff602e23a
Bernardo Meurer <bernardo@meurer.org>
2021-12-06 12:39:37 +0000

    style(nix/checks): fmt

--- nix/checks.nix
+++ nix/checks.nix
@@ -8,48 +8,44 @@ system:
 with self.nixpkgs.${system};
 
 {
-  pre-commit-check = pre-commit-hooks.lib.${system}.run {
-    src = gitignoreSource ../.;
-    hooks = {
-      nixpkgs-fmt.enable = true;
-      nix-linter.enable = true;
-      nix-parse = {
-        enable = true;
-        entry = "nix-instantiate --json --parse";
-        types = [ "file" "nix" ];
+  pre-commit-check = pre-commit-hooks.lib.${system}.run
+    {
+      src = gitignoreSource ../.;
+      hooks = {
+        nixpkgs-fmt.enable = true;
+        nix-linter.enable = true;
+        stylua = {
+          enable = true;
+          types = [ "file" "lua" ];
+          entry = "${stylua}/bin/stylua";
+        };
+        luacheck = {
+          enable = true;
+          types = [ "file" "lua" ];
+          entry = "${luajitPackages.luacheck}/bin/luacheck --std luajit --globals vim -- ";
+        };
       };
-      stylua = {
-        enable = true;
-        entry = "${stylua}/bin/stylua";
-        types = [ "file" "lua" ];
-      };
-      luacheck = {
-        enable = true;
-        entry = "${luajitPackages.luacheck}/bin/luacheck --std luajit --globals vim -- ";
-        types = [ "file" "lua" ];
-      };
+      settings.nix-linter.checks = [
+        "DIYInherit"
+        "EmptyInherit"
+        "EmptyLet"
+        "EtaReduce"
+        "LetInInheritRecset"
+        "ListLiteralConcat"
+        "NegateAtom"
+        "SequentialLet"
+        "SetLiteralUpdate"
+        "UnfortunateArgName"
+        "UnneededRec"
+        "UnusedArg"
+        "UnusedLetBind"
+        "UpdateEmptySet"
+        "BetaReduction"
+        "EmptyVariadicParamSet"
+        "UnneededAntiquote"
+        "no-FreeLetInFunc"
+        "no-AlphabeticalArgs"
+        "no-AlphabeticalBindings"
+      ];
     };
-    settings.nix-linter.checks = [
-      "DIYInherit"
-      "EmptyInherit"
-      "EmptyLet"
-      "EtaReduce"
-      "LetInInheritRecset"
-      "ListLiteralConcat"
-      "NegateAtom"
-      "SequentialLet"
-      "SetLiteralUpdate"
-      "UnfortunateArgName"
-      "UnneededRec"
-      "UnusedArg"
-      "UnusedLetBind"
-      "UpdateEmptySet"
-      "BetaReduction"
-      "EmptyVariadicParamSet"
-      "UnneededAntiquote"
-      "no-FreeLetInFunc"
-      "no-AlphabeticalArgs"
-      "no-AlphabeticalBindings"
-    ];
-  };
 } // (deploy-rs.lib.deployChecks self.deploy)
