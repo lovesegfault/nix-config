2db1979fa685313501ddffb691dc6d690087dbbf
Bernardo Meurer <bernardo@meurer.org>
2021-12-20 19:44:50 -0300

    refactor(hardware/thinkpad-p1): use vanilla latest kernel

--- hardware/thinkpad-p1.nix
+++ hardware/thinkpad-p1.nix
@@ -12,7 +12,7 @@
     initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
     kernel.sysctl = { "vm.swappiness" = 1; };
     kernelModules = [ "kvm-intel" "i2c_dev" "ddcci-backlight" "tcp_bbr" "kvm_intel" ];
-    kernelPackages = pkgs.linuxPackages_xanmod_lto_skylake;
+    kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = [ "acpi_mask_gpe=0x69" ];
     tmpOnTmpfs = true;
   };
