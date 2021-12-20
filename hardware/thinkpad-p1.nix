cfcd7feb34ed29df17cf0e83e9e5d23e189d4cfd
Bernardo Meurer <bernardo@meurer.org>
2021-12-20 19:09:20 -0300

    refactor(hardware/thinkpad-p1): set cpu frequency governor to powersave

--- hardware/thinkpad-p1.nix
+++ hardware/thinkpad-p1.nix
@@ -38,6 +38,8 @@
     systemFeatures = [ "benchmark" "nixos-test" "big-parallel" "kvm" "gccarch-skylake" ];
   };
 
+  powerManagement.cpuFreqGovernor = "powersave";
+
   services = {
     fstrim.enable = true;
     fwupd.enable = true;
@@ -46,7 +48,7 @@
       enable = true;
       settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
         DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = [ "bluetooth" "wifi" ];
         DEVICES_TO_ENABLE_ON_AC = [ "bluetooth" "wifi" ];
