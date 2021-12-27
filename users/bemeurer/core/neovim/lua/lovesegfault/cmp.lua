ef3c91c73492b41e0993e2ec6d46d25a567525e7
Bernardo Meurer <bernardo@meurer.org>
2021-12-26 23:41:38 -0300

    fix(users/bemeurer/core/neovim): fix tab-completion

--- users/bemeurer/core/neovim/lua/lovesegfault/cmp.lua
+++ users/bemeurer/core/neovim/lua/lovesegfault/cmp.lua
@@ -1,6 +1,11 @@
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noselect"
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 -- luasnip setup
 local luasnip = require("luasnip")
 
@@ -24,24 +29,27 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
-    ["<Tab>"] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
-    end,
-    ["<S-Tab>"] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+        luasnip.jump(-1)
       else
         fallback()
       end
-    end,
+    end, { "i", "s" }),
   },
   formatting = {
     format = function(entry, vim_item)
