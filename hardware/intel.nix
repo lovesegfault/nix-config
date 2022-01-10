d06021db9fea78c3fdb36c6c26a66231718a8f24
Bernardo Meurer <bernardo@meurer.org>
2022-01-10 14:09:35 -0300

    feat(hardware/intel): use intel-compute-runtime for OpenCL support

--- hardware/intel.nix
+++ hardware/intel.nix
@@ -10,6 +10,7 @@
     cpu.intel.updateMicrocode = true;
     opengl = {
       extraPackages = with pkgs; [
+        intel-compute-runtime # OpenCL
         intel-media-driver # LIBVA_DRIVER_NAME=iHD
         vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
         vaapiVdpau
