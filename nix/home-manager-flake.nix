6fa25211b6277a2fbe8572bee532d32dd6757d19
Bernardo Meurer <bernardo@meurer.org>
2022-01-31 12:22:46 -0800

    refactor(nix/home-manager-flake): use upstream nix.registry cfg

--- nix/home-manager-flake.nix
+++ nix/home-manager-flake.nix
@@ -1,5 +1,10 @@
-{ nixpkgs, ... }@inputs:
-{ config, lib, ... }: {
+{ nixpkgs, templates, ... }:
+{ config, ... }: {
+  nix.registry = {
+    templates.flake = templates;
+    nixpkgs.flake = nixpkgs;
+  };
+
   systemd.user.sessionVariables.NIX_PATH = "nixpkgs=${config.xdg.dataHome}/nixpkgs\${NIX_PATH:+:}$NIX_PATH";
 
   xdg.dataFile."nixpkgs".source = nixpkgs;
@@ -7,25 +12,4 @@
   xdg.configFile."nix/nix.conf".text = ''
     flake-registry = ${config.xdg.configHome}/nix/registry.json
   '';
-
-  xdg.configFile."nix/registry.json".text = builtins.toJSON {
-    version = 2;
-    flakes =
-      let
-        mkFlakeRef = id: {
-          from = { inherit id; type = "indirect"; };
-          to = ({
-            type = "path";
-            path = inputs.${id}.outPath;
-          } // lib.filterAttrs
-            (n: _: n == "lastModified" || n == "rev" || n == "revCount" || n == "narHash")
-            inputs.${id});
-        };
-      in
-      [
-        (mkFlakeRef "self")
-        (mkFlakeRef "nixpkgs")
-        (mkFlakeRef "templates")
-      ];
-  };
 }
