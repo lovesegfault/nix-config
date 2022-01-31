eb44256849f9518f94be02fb69adfcc72de599eb
Bernardo Meurer <bernardo@meurer.org>
2022-01-31 12:28:05 -0800

    refactor(nix/home-manager-flake): add overlays to NIX_PATH

--- nix/home-manager-flake.nix
+++ nix/home-manager-flake.nix
@@ -5,11 +5,15 @@
     nixpkgs.flake = nixpkgs;
   };
 
-  systemd.user.sessionVariables.NIX_PATH = "nixpkgs=${config.xdg.dataHome}/nixpkgs\${NIX_PATH:+:}$NIX_PATH";
+  systemd.user.sessionVariables.NIX_PATH = "nixpkgs=${config.xdg.dataHome}/nixpkgs:nixpkgs-overlays=${config.xdg.dataHome}/overlays\${NIX_PATH:+:}$NIX_PATH";
 
-  xdg.dataFile."nixpkgs".source = nixpkgs;
-
-  xdg.configFile."nix/nix.conf".text = ''
-    flake-registry = ${config.xdg.configHome}/nix/registry.json
-  '';
+  xdg = {
+    dataFile = {
+      nixpkgs.source = nixpkgs;
+      overlays.source = ../nix/overlays;
+    };
+    configFile."nix/nix.conf".text = ''
+      flake-registry = ${config.xdg.configHome}/nix/registry.json
+    '';
+  };
 }
