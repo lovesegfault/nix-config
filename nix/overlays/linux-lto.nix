a589f668eb2468694bff86607b66c8a60eaef893
Bernardo Meurer <bernardo@meurer.org>
2022-02-28 11:21:29 -0800

    refactor(nix/overlays/linux-lto): use pure graysky patch url

--- nix/overlays/linux-lto.nix
+++ nix/overlays/linux-lto.nix
@@ -65,7 +65,7 @@ let
       name = "more-uarches-for-kernel-5.15";
       patch = final.fetchpatch {
         name = "more-uarches-for-kernel-5.15";
-        url = "https://raw.githubusercontent.com/graysky2/kernel_compiler_patch/master/more-uarches-for-kernel-5.15%2B.patch";
+        url = "https://raw.githubusercontent.com/graysky2/kernel_compiler_patch/9c9c7e817dd2718566ec95f7742b162ab125316f/more-uarches-for-kernel-5.15%2B.patch";
         sha256 = "sha256-WSN+1t8Leodt7YRosuDF7eiSL5/8PYseXzxquf0LtP8=";
       };
     };
