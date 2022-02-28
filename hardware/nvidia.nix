379d3b57b4fe67de6c5b68f88db5985c3172de48
Bernardo Meurer <bernardo@meurer.org>
2022-02-28 13:14:53 -0800

    refactor(hardware/nvidia): remove old cruft

--- hardware/nvidia.nix
+++ hardware/nvidia.nix
@@ -1,25 +1,14 @@
-{ config, ... }:
-let
-  nvidia_x11 = config.boot.kernelPackages.nvidia_x11;
-  nvidia_gl = nvidia_x11.out;
-  nvidia_gl_32 = nvidia_x11.lib32;
-in
 {
-  boot = {
-    blacklistedKernelModules = [ "nouveau" ];
-    extraModulePackages = [ nvidia_x11 ];
-  };
-
-  environment.systemPackages = [ nvidia_x11 ];
+  boot.blacklistedKernelModules = [ "nouveau" ];
 
   hardware = {
-    nvidia.package = nvidia_x11;
-    nvidia.modesetting.enable = true;
+    nvidia = {
+      modesetting.enable = true;
+      nvidiaPersistenced = true;
+    };
     opengl = {
       enable = true;
       driSupport32Bit = true;
-      extraPackages = [ nvidia_gl ];
-      extraPackages32 = [ nvidia_gl_32 ];
     };
   };
 
