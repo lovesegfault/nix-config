b0202c234141d36e19d6f93aae9ef31bb2d56285
Bernardo Meurer <bernardo@meurer.org>
2022-02-27 16:53:43 -0800

    refactor(hardware/thinkpad-p1): don't override runtime pm defaults

--- hardware/thinkpad-p1.nix
+++ hardware/thinkpad-p1.nix
@@ -64,8 +64,6 @@
         MAX_LOST_WORK_SECS_ON_AC = 15;
         MAX_LOST_WORK_SECS_ON_BAT = 15;
 
-        RUNTIME_PM_ON_AC = "auto";
-        RUNTIME_PM_ON_BAT = "auto";
         RUNTIME_PM_DRIVER_DENYLIST = [ "nouveau" "nvidia" ];
 
         SOUND_POWER_SAVE_ON_AC = "1";
