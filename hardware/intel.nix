2b3b69a66a7da94d0f5bd1b9afdc2fcaabcee38b
Bernardo Meurer <bernardo@meurer.org>
2022-03-21 12:06:47 -0700

    refactor(hardware/intel): don't install vaapiVdpau

--- hardware/intel.nix
+++ hardware/intel.nix
@@ -20,7 +20,6 @@
         intel-compute-runtime # OpenCL
         intel-media-driver # LIBVA_DRIVER_NAME=iHD
         vaapiIntel # LIBVA_DRIVER_NAME=i965 (older)
-        vaapiVdpau
         libvdpau-va-gl
       ];
     };
