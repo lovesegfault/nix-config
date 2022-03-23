18486ff4e1b1c1f36e402345c37904f4845a7929
Bernardo Meurer <bernardo@meurer.org>
2022-03-23 16:23:56 -0700

    feat(core/resolved): working mDNS+LLMNR support

--- core/resolved.nix
+++ core/resolved.nix
@@ -1,5 +1,11 @@
-{
-  networking.networkmanager.dns = "systemd-resolved";
+{ lib, ... }: {
+  networking = {
+    firewall = {
+      allowedTCPPorts = [ 5355 ];
+      allowedUDPPorts = [ 5353 5355 ];
+    };
+    networkmanager.dns = "systemd-resolved";
+  };
 
   services.resolved = {
     enable = true;
@@ -7,8 +13,13 @@
     extraConfig = ''
       DNS=1.1.1.1 2606:4700:4700::1111 8.8.8.8 2001:4860:4860::8844
       Domains=~.
-      MulticastDNS=true
       LLMNR=true
+      MulticastDNS=true
     '';
   };
+
+  system.nssDatabases.hosts = lib.mkMerge [
+    (lib.mkBefore [ "mdns_minimal [NOTFOUND=return]" ])
+    (lib.mkAfter [ "mdns" ])
+  ];
 }
