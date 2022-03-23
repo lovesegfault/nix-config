ab9fdde7277cebdd6c6ecb9e058ee16261d188b8
Bernardo Meurer <bernardo@meurer.org>
2022-03-23 15:44:05 -0700

    refactor(core/resolved): enable mDNS and LLMNR

--- core/resolved.nix
+++ core/resolved.nix
@@ -3,10 +3,12 @@
 
   services.resolved = {
     enable = true;
-    dnssec = "false";
+    dnssec = "allow-downgrade";
     extraConfig = ''
       DNS=1.1.1.1 2606:4700:4700::1111 8.8.8.8 2001:4860:4860::8844
       Domains=~.
+      MulticastDNS=true
+      LLMNR=true
     '';
   };
 }
