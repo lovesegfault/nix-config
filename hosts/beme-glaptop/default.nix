28f399be34d50a89a6a69ce083b4cc604f3801f2
Bernardo Meurer <bernardo@meurer.org>
2022-04-14 18:12:57 -0700

    feat(hosts/beme-glaptop): set auto-optimise-store

--- hosts/beme-glaptop/default.nix
+++ hosts/beme-glaptop/default.nix
@@ -143,6 +143,7 @@
     trusted-public-keys = nix-config.cachix.org-1:Vd6raEuldeIZpttVQfrUbLvXJHzzzkS0pezXCVVjDG4= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
     require-sigs = true
 
+    auto-optimise-store = true
     system-features = recursive-nix benchmark nixos-test big-parallel kvm
     sandbox-fallback = false
 
