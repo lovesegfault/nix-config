62e9cf0182c48fb2a5d22cc392384c6c13b20fee
Bernardo Meurer <bernardo@meurer.org>
2022-04-23 21:10:33 -0700

    refactor(nix/host-drvs): improve some var names

--- nix/host-drvs.nix
+++ nix/host-drvs.nix
@@ -5,9 +5,9 @@ system:
 let
   inherit (self.nixpkgs.${system}) lib linkFarm;
 
-  nixosDrvs = lib.mapAttrs (_: cfg: cfg.config.system.build.toplevel) self.nixosConfigurations;
-  hmDrvs = lib.mapAttrs (_: hm: hm.activationPackage) self.homeConfigurations;
-  hostDrvs = nixosDrvs // hmDrvs;
+  nixosDrvs = lib.mapAttrs (_: nixos: nixos.config.system.build.toplevel) self.nixosConfigurations;
+  homeDrvs = lib.mapAttrs (_: home: home.activationPackage) self.homeConfigurations;
+  hostDrvs = nixosDrvs // homeDrvs;
 in
 hostDrvs // {
   all-hosts = linkFarm "all-hosts" (lib.mapAttrsToList (name: path: { inherit name path; }) hostDrvs);
