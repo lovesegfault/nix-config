d56443690db508c3ce656425c4bb9f1c6fdfc868
Bernardo Meurer <bernardo@meurer.org>
2022-05-02 10:49:03 -0700

    refactor(hosts/nozick): incorporate prometheus cfg

--- hosts/nozick/default.nix
+++ hosts/nozick/default.nix
@@ -8,7 +8,6 @@
 
     ../../users/bemeurer
 
-    ./prometheus.nix
     ./state.nix
     ./unbound.nix
   ];
@@ -178,6 +177,38 @@
       };
     };
     plex.enable = true;
+    prometheus = {
+      enable = true;
+      extraFlags = [ "--storage.tsdb.retention.time=90d" ];
+      scrapeConfigs = [
+        {
+          job_name = "node";
+          scrape_interval = "10s";
+          static_configs = [{ targets = [ "127.0.0.1:9100" ]; }];
+        }
+        {
+          job_name = "unbound";
+          scrape_interval = "30s";
+          static_configs = [{ targets = [ "127.0.0.1:9167" ]; }];
+        }
+        {
+          job_name = "prometheus";
+          scrape_interval = "30s";
+          static_configs = [{ targets = [ "127.0.0.1:9090" ]; }];
+        }
+      ];
+      exporters = {
+        node = {
+          enable = true;
+          enabledCollectors = [ "systemd" "pressure" ];
+        };
+        unbound = {
+          enable = true;
+          controlInterface = "/run/unbound/unbound.ctl";
+          user = "unbound";
+        };
+      };
+    };
     smartd.enable = true;
     sshguard.enable = true;
     zfs = {
