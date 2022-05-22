6fab56fe12d802bf73b4bfdd8b07ed0511861e68
Bernardo Meurer <bernardo@meurer.org>
2022-05-22 16:10:46 -0700

    refactor(nix/overlays/scripts/fixart): add error messages

--- nix/overlays/scripts/fixart.nix
+++ nix/overlays/scripts/fixart.nix
@@ -9,14 +9,18 @@ final: _: {
         name = "fixart";
         runtimeInputs = [ beets coreutils findutils ];
         text = ''
-          trap ctrl_c INT
-
           function ctrl_c() {
               exit 1;
           }
+          trap ctrl_c INT
+
+          function die() {
+            echo "$@"
+            exit 1
+          }
 
           newart=$1
-          [ $# -gt 1 ] || exit 1
+          [ $# -gt 1 ] || die "Invalid usage"
 
           beet clearart "''${@:2}"
           beet embedart -f "$newart" "''${@:2}"
@@ -24,14 +28,16 @@ final: _: {
 
           outpath="$(beet ls -a -p "''${@:2}")"
 
-          jpgs="$(find "$outpath" -type f -name "*.jpg" | wc -l)"
-          [ "$jpgs" -eq 1 ] || exit 1
-          echo "OK: cover count"
-
           original="$(sha256sum "$newart" | cut -f 1 -d " ")"
           updated="$(sha256sum "$outpath/cover.jpg" | cut -f 1 -d " ")"
           [ "$original" == "$updated" ] || exit 1
           echo "OK: SHA"
+
+          jpgs="$(find "$outpath" -type f -name "*.jpg" | wc -l)"
+          [ "$jpgs" -eq 1 ] || die "Too many .jpg in album path"
+          lingering="$(find "$outpath" -type f -iname "cover.*" -not -iname "cover.jpg")"
+          [ -z "$lingering" ] || die "Lingering cover files: $lingering"
+          echo "OK: cover count"
         '';
       }
     )
