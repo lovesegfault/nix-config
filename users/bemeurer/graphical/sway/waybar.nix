bd9e14bc4b976f5263a9de6d3984a16dacba6c3a
Bernardo Meurer <bernardo@meurer.org>
2022-06-08 10:01:09 -0400

    fix(users/bemeurer/graphical/sway/waybar): don't nest modules

--- users/bemeurer/graphical/sway/waybar.nix
+++ users/bemeurer/graphical/sway/waybar.nix
@@ -16,81 +16,79 @@
         "battery"
         "tray"
       ];
-      modules = {
-        "sway/workspaces" = {
-          all-outputs = true;
-          format = "{name}";
-        };
-        "sway/mode" = { format = ''<span style="italic">{}</span>''; };
+      "sway/workspaces" = {
+        all-outputs = true;
+        format = "{name}";
+      };
+      "sway/mode" = { format = ''<span style="italic">{}</span>''; };
 
-        pulseaudio = {
-          format = "{volume}% {icon} {format_source}";
-          format-bluetooth = "{volume}% {icon} {format_source}";
-          format-bluetooth-muted = " {icon} {format_source}";
-          format-muted = " {format_source}";
-          format-source = "{volume}% ";
-          format-source-muted = "";
-          format-icons = {
-            headphones = "";
-            handsfree = "";
-            headset = "";
-            phone = "";
-            portable = "";
-            car = "";
-            default = [ "" "" "" ];
-          };
-          on-click = "${pkgs.ponymix}/bin/ponymix -t sink toggle";
-          on-scroll-up = "${pkgs.ponymix}/bin/ponymix increase 1";
-          on-scroll-down = "${pkgs.ponymix}/bin/ponymix decrease 1";
+      pulseaudio = {
+        format = "{volume}% {icon} {format_source}";
+        format-bluetooth = "{volume}% {icon} {format_source}";
+        format-bluetooth-muted = " {icon} {format_source}";
+        format-muted = " {format_source}";
+        format-source = "{volume}% ";
+        format-source-muted = "";
+        format-icons = {
+          headphones = "";
+          handsfree = "";
+          headset = "";
+          phone = "";
+          portable = "";
+          car = "";
+          default = [ "" "" "" ];
         };
-        idle_inhibitor = {
-          format = "{icon}";
-          format-icons = {
-            activated = "";
-            deactivated = "";
-          };
+        on-click = "${pkgs.ponymix}/bin/ponymix -t sink toggle";
+        on-scroll-up = "${pkgs.ponymix}/bin/ponymix increase 1";
+        on-scroll-down = "${pkgs.ponymix}/bin/ponymix decrease 1";
+      };
+      idle_inhibitor = {
+        format = "{icon}";
+        format-icons = {
+          activated = "";
+          deactivated = "";
         };
-        network = {
-          format-wifi = "{essid} ({signalStrength}%) ";
-          format-ethernet = "{ifname}: {ipaddr}/{cidr} ";
-          format-linked = "{ifname} (No IP) ";
-          format-disconnected = "Disconnected ⚠";
-          format-alt = "{ifname}: {ipaddr}/{cidr}";
+      };
+      network = {
+        format-wifi = "{essid} ({signalStrength}%) ";
+        format-ethernet = "{ifname}: {ipaddr}/{cidr} ";
+        format-linked = "{ifname} (No IP) ";
+        format-disconnected = "Disconnected ⚠";
+        format-alt = "{ifname}: {ipaddr}/{cidr}";
+      };
+      temperature = {
+        thermal-zone = 1;
+        critical-threshold = 80;
+        format = "{temperatureC}°C {icon}";
+        format-icons = [ "" "" "" ];
+      };
+      backlight = {
+        device = "intel_backlight";
+        format = "{percent}% {icon}";
+        format-icons = [ "" "" ];
+        on-scroll-up = "${pkgs.brillo}/bin/brillo -e -A 0.5";
+        on-scroll-down = "${pkgs.brillo}/bin/brillo -e -U 0.5";
+      };
+      battery = {
+        bat = "BAT0";
+        states = {
+          good = 90;
+          warning = 30;
+          critical = 15;
         };
-        temperature = {
-          thermal-zone = 1;
-          critical-threshold = 80;
-          format = "{temperatureC}°C {icon}";
-          format-icons = [ "" "" "" ];
-        };
-        backlight = {
-          device = "intel_backlight";
-          format = "{percent}% {icon}";
-          format-icons = [ "" "" ];
-          on-scroll-up = "${pkgs.brillo}/bin/brillo -e -A 0.5";
-          on-scroll-down = "${pkgs.brillo}/bin/brillo -e -U 0.5";
-        };
-        battery = {
-          bat = "BAT0";
-          states = {
-            good = 90;
-            warning = 30;
-            critical = 15;
-          };
-          format = "{capacity}% {icon}";
-          format-charging = "{capacity}% ";
-          format-plugged = "{capacity}% ";
-          format-alt = "{time} {icon}";
-          format-icons = [ " " " " " " " " " " ];
-        };
-        clock = {
-          tooltip-format = "{calendar}";
-          format = "{:%F | %H:%M | %Z}";
-        };
-        tray = {
-          icon-size = 20;
-          spacing = 5;
-        };
+        format = "{capacity}% {icon}";
+        format-charging = "{capacity}% ";
+        format-plugged = "{capacity}% ";
+        format-alt = "{time} {icon}";
+        format-icons = [ " " " " " " " " " " ];
+      };
+      clock = {
+        tooltip-format = "{calendar}";
+        format = "{:%F | %H:%M | %Z}";
+      };
+      tray = {
+        icon-size = 20;
+        spacing = 5;
       };
     }];
     style = ''
