3d6e8ed6acc9a92f30974a7e21fe0aafc37ca492
Bernardo Meurer <bernardo@meurer.org>
2022-06-09 12:12:45 -0400

    Revert "refactor(hosts/luther): remove single-user Nix hacks"
    This reverts commit 641881a9b053a692c4d28c9b2583384a97632494.

--- hosts/luther/default.nix
+++ hosts/luther/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   imports = [
     ../../users/bemeurer/core
     ../../users/bemeurer/dev
@@ -24,10 +24,29 @@
         fi
       '';
       profileExtra = ''
+        source ${config.home.homeDirectory}/.nix-profile/etc/profile.d/nix.sh
         if [ -f /etc/bashrc ]; then
         . /etc/bashrc
         fi
       '';
     };
+    zsh.initExtra = ''
+      source ${config.home.homeDirectory}/.nix-profile/etc/profile.d/nix.sh
+    '';
   };
+
+  xdg.configFile."nix/nix.conf".text = ''
+    accept-flake-config = true
+    auto-optimise-store = true
+    builders-use-substitutes = true
+    connect-timeout = 5
+    cores = 0
+    experimental-features = nix-command flakes recursive-nix
+    http-connections = 0
+    max-jobs = 64
+    sandbox = true
+    substituters = https://cache.nixos.org/ https://nix-config.cachix.org https://nix-community.cachix.org
+    system-features = recursive-nix benchmark nixos-test big-parallel kvm
+    trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-config.cachix.org-1:Vd6raEuldeIZpttVQfrUbLvXJHzzzkS0pezXCVVjDG4= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+  '';
 }
