0198af587cb984456a0e4616b9a4b0478929fc1f
Bernardo Meurer <bernardo@meurer.org>
2022-07-01 15:21:33 -0400

    refactor(hosts/nozick/vouch): incorporate deluge overrides

--- hosts/nozick/vouch.nix
+++ hosts/nozick/vouch.nix
@@ -6,6 +6,30 @@
   security.acme.certs."vouch.meurer.org" = { };
 
   services.nginx.virtualHosts = {
+    "deluge.meurer.org".locations = {
+      "/".extraConfig = ''
+        auth_request /validate;
+        proxy_set_header X-Vouch-User $auth_resp_x_vouch_user;
+        error_page 401 = @error401;
+      '';
+
+      "@error401".extraConfig = ''
+        return 302 https://vouch.meurer.org/login?url=$scheme://$http_host$request_uri&vouch-failcount=$auth_resp_failcount&X-Vouch-Token=$auth_resp_jwt&error=$auth_resp_err;
+      '';
+
+      "/validate" = {
+        proxyPass = "http://127.0.0.1:30746/validate";
+        extraConfig = ''
+          internal;
+          proxy_pass_request_body off;
+          proxy_set_header Content-Length "";
+          auth_request_set $auth_resp_x_vouch_user $upstream_http_x_vouch_user;
+          auth_request_set $auth_resp_jwt $upstream_http_x_vouch_jwt;
+          auth_request_set $auth_resp_err $upstream_http_x_vouch_err;
+          auth_request_set $auth_resp_failcount $upstream_http_x_vouch_failcount;
+        '';
+      };
+    };
     "vouch.meurer.org" = {
       useACMEHost = "vouch.meurer.org";
       forceSSL = true;
