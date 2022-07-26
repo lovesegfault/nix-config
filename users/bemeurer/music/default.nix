800e1b9002c5395b43e43fcdbdc8b7b3ea8d2a10
Bernardo Meurer <bernardo@meurer.org>
2022-07-26 06:32:44 +0000

    feat(users/bemeurer/music): add beets-alternatives

--- users/bemeurer/music/default.nix
+++ users/bemeurer/music/default.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    beets-unstable
+    (beets-unstable.override {
+      pluginOverrides = {
+        alternatives = { enable = true; propagatedBuildInputs = [ beetsPackages.alternatives ]; };
+        limit = { enable = true; builtin = true; };
+      };
+    })
     checkart
     fixart
     mediainfo
