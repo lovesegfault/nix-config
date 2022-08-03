250a71fbe44c45dc3b272983f1e34e50e06daf2c
Bernardo Meurer <bernardo@meurer.org>
2022-08-02 18:27:32 -0700

    feat(hardware/thinkpad-z13): configure tlp

--- hardware/thinkpad-z13.nix
+++ hardware/thinkpad-z13.nix
@@ -44,5 +44,23 @@
     fprintd.enable = true;
     fwupd.enable = true;
     xserver.dpi = 96;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "balanced";
+
+        DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = [ "bluetooth" "wifi" ];
+        DEVICES_TO_ENABLE_ON_AC = [ "bluetooth" "wifi" ];
+
+        DISK_IOSCHED = [ "none" ];
+
+        START_CHARGE_THRESH_BAT0 = 70;
+        STOP_CHARGE_THRESH_BAT0 = 80;
+      };
+    };
   };
 }
