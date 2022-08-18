a34bd46b3cc62fd26e0fc3a37558fcf1c200e0dc
Bernardo Meurer <bernardo@meurer.org>
2022-08-18 15:12:13 -0500

    refactor(core/resolved): set fallbackDns instead of DNS

--- core/resolved.nix
+++ core/resolved.nix
@@ -10,10 +10,15 @@
   services.resolved = {
     enable = true;
     dnssec = "allow-downgrade";
+    fallbackDns = [
+      "1.1.1.1"
+      "2606:4700:4700::1111"
+      "8.8.8.8"
+      "2001:4860:4860::8844"
+    ];
+    llmnr = "true";
     extraConfig = ''
-      DNS=1.1.1.1 2606:4700:4700::1111 8.8.8.8 2001:4860:4860::8844
       Domains=~.
-      LLMNR=true
       MulticastDNS=true
     '';
   };
