41bab8cdc3ff53e9df1bfdb9c10136ad8f93d5a4
Bernardo Meurer <bernardo@meurer.org>
2022-08-24 16:48:28 -0300

    refactor(users/bemeurer/core/ssh): reorder glob clauses

--- users/bemeurer/core/ssh.nix
+++ users/bemeurer/core/ssh.nix
@@ -1,9 +1,11 @@
 {
   home.file.".ssh/config".text = ''
-    CanonicalizeHostname yes
-    PermitLocalCommand yes
-
     Include ~/.ssh/config.host
+
+    Host *
+      CanonicalizeHostname yes
+      PermitLocalCommand yes
+      CanonicalDomains meurer.org.beta.tailscale.net
 
     Match canonical Host aarch64.nixos.community,147.28.143.250
       User lovesegfault
@@ -25,8 +27,5 @@
       StrictHostKeyChecking ask
       VerifyHostKeyDNS yes
       SetEnv TERM=xterm-256color
-
-    Host *
-      CanonicalDomains meurer.org.beta.tailscale.net
   '';
 }
