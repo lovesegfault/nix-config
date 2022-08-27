a6294574ad0d68880d6c4465054472dd84720036
Bernardo Meurer <bernardo@meurer.org>
2022-08-27 17:22:42 -0300

    refactor(users/bemeurer/graphical/common): remove superfluous config

--- users/bemeurer/graphical/common.nix
+++ users/bemeurer/graphical/common.nix
@@ -58,42 +58,22 @@ let
         execSpawn = cmd: "exec --no-startup-id ${pkgs.spawn}/bin/spawn ${cmd}";
       in
       lib.mkOptionDefault {
-        # fancy workspace names
-        "${modifier}+1" = "workspace 1";
-        "${modifier}+2" = "workspace 2";
-        "${modifier}+3" = "workspace 3";
-        "${modifier}+4" = "workspace 4";
-        "${modifier}+5" = "workspace 5";
-        "${modifier}+6" = "workspace 6";
-        "${modifier}+7" = "workspace 7";
-        "${modifier}+8" = "workspace 8";
-        "${modifier}+9" = "workspace 9";
         "${modifier}+0" = "workspace 10";
-        "${modifier}+Shift+1" = "move container to workspace 1";
-        "${modifier}+Shift+2" = "move container to workspace 2";
-        "${modifier}+Shift+3" = "move container to workspace 3";
-        "${modifier}+Shift+4" = "move container to workspace 4";
-        "${modifier}+Shift+5" = "move container to workspace 5";
-        "${modifier}+Shift+6" = "move container to workspace 6";
-        "${modifier}+Shift+7" = "move container to workspace 7";
-        "${modifier}+Shift+8" = "move container to workspace 8";
-        "${modifier}+Shift+9" = "move container to workspace 9";
         "${modifier}+Shift+0" = "move container to workspace 10";
-        # normal ones
-        "Mod1+Tab" = " workspace next";
-        "Mod4+Tab" = " workspace prev";
-        "${modifier}+comma" = " workspace prev";
-        "${modifier}+period" = " workspace next";
-        "XF86AudioLowerVolume" = execSpawn "${pkgs.ponymix}/bin/ponymix decrease 1";
-        "XF86AudioMicMute" = execSpawn "${pkgs.ponymix}/bin/ponymix -t source toggle";
-        "XF86AudioMute" = execSpawn "${pkgs.ponymix}/bin/ponymix -t sink toggle";
-        "XF86AudioNext" = execSpawn "${pkgs.playerctl}/bin/playerctl next";
-        "XF86AudioPause" = execSpawn "${pkgs.playerctl}/bin/playerctl pause";
-        "XF86AudioPlay" = execSpawn "${pkgs.playerctl}/bin/playerctl play";
-        "XF86AudioPrev" = execSpawn "${pkgs.playerctl}/bin/playerctl previous";
-        "XF86AudioRaiseVolume" = execSpawn "${pkgs.ponymix}/bin/ponymix increase 1";
-        "XF86MonBrightnessDown" = execSpawn "${pkgs.brillo}/bin/brillo -e -U 1";
-        "XF86MonBrightnessUp" = execSpawn "${pkgs.brillo}/bin/brillo -e -A 1";
+        "${modifier}+comma" = "workspace prev";
+        "${modifier}+period" = "workspace next";
+        "Mod1+Tab" = "workspace next";
+        "Mod4+Tab" = "workspace prev";
+        "XF86AudioLowerVolume" = execSpawn "${lib.getExe pkgs.ponymix} decrease 1";
+        "XF86AudioMicMute" = execSpawn "${lib.getExe pkgs.ponymix} -t source toggle";
+        "XF86AudioMute" = execSpawn "${lib.getExe pkgs.ponymix} -t sink toggle";
+        "XF86AudioNext" = execSpawn "${lib.getExe pkgs.playerctl} next";
+        "XF86AudioPause" = execSpawn "${lib.getExe pkgs.playerctl} pause";
+        "XF86AudioPlay" = execSpawn "${lib.getExe pkgs.playerctl} play";
+        "XF86AudioPrev" = execSpawn "${lib.getExe pkgs.playerctl} previous";
+        "XF86AudioRaiseVolume" = execSpawn "${lib.getExe pkgs.ponymix} increase 1";
+        "XF86MonBrightnessDown" = execSpawn "${lib.getExe pkgs.brillo} -e -U 1";
+        "XF86MonBrightnessUp" = execSpawn "${lib.getExe pkgs.brillo} -e -A 1";
       };
 
     modifier = "Mod1";
