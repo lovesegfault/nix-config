288ab86a20f8ee23f5a9e2b0e0c770ce1cbf868a
Bernardo Meurer <bernardo@meurer.org>
2022-09-01 12:02:31 -0300

    refactor(nix/deploy): disable auto-rollback

--- nix/deploy.nix
+++ nix/deploy.nix
@@ -18,7 +18,9 @@ let
     };
 in
 {
-  autoRollback = true;
+  # XXX: auto-rollback is too noisy since any service failing will cause it to
+  # go haywire.
+  autoRollback = false;
   magicRollback = true;
   user = "root";
   nodes = lib.mapAttrs genNode self.nixosConfigurations;
