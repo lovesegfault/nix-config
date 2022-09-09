ebed4ceb94493851d38a5f11c4ff6fa955f21085
Bernardo Meurer <bernardo@meurer.org>
2022-09-09 16:40:30 -0300

    refactor(hosts/jung/roon): open port 55000

--- hosts/jung/roon.nix
+++ hosts/jung/roon.nix
@@ -1,25 +1,10 @@
 { config, ... }: {
   networking.firewall = {
-    allowedTCPPortRanges = [
-      { from = 8008; to = 8009; }
-      { from = 9330; to = 9339; }
-      { from = 30000; to = 30010; }
-    ];
-    allowedTCPPorts = [ 8088 4321 ];
-    allowedUDPPorts = [ 9003 ];
-    extraCommands = ''
-      ## IGMP / Broadcast - required by Roon ##
-      iptables -A INPUT -s 224.0.0.0/4 -j ACCEPT
-      iptables -A INPUT -d 224.0.0.0/4 -j ACCEPT
-      iptables -A INPUT -s 240.0.0.0/5 -j ACCEPT
-      iptables -A INPUT -m pkttype --pkt-type multicast -j ACCEPT
-      iptables -A INPUT -m pkttype --pkt-type broadcast -j ACCEPT
-    '';
+    allowedTCPPorts = [ 55000 ];
+    allowedUDPPorts = [ 55000 ];
   };
 
-  services = {
-    roon-server.enable = true;
-  };
+  services.roon-server.enable = true;
 
   sound.enable = true;
 
