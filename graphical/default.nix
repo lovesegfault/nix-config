80fb1987153c62638a7413296e5fd97531278320
Bernardo Meurer <bernardo@meurer.org>
2022-10-27 12:50:48 +0100

    refactor(graphical): rollup boot-silent into default

--- graphical/default.nix
+++ graphical/default.nix
@@ -1,9 +1,18 @@
 { pkgs, ... }: {
   imports = [
-    ./boot-silent.nix
     ./fonts.nix
     ./greetd.nix
   ];
+
+  boot = {
+    consoleLogLevel = 3;
+    kernelParams = [
+      "quiet"
+      "systemd.show_status=auto"
+      "udev.log_level=3"
+      "vt.global_cursor_default=0"
+    ];
+  };
 
   programs.dconf.enable = true;
 
