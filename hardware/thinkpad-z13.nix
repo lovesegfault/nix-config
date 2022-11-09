18b8a59f78d00885f226ea7533ca4f6c7cd29479
Bernardo Meurer <bernardo@meurer.org>
2022-11-09 19:25:34 +0000

    fix(hardware/thinkpad-z13): fix gdm fprint module

--- hardware/thinkpad-z13.nix
+++ hardware/thinkpad-z13.nix
@@ -44,6 +44,19 @@
     "gccarch-znver3"
   ];
 
+  security.pam.services.gdm-fingerprint.text = ''
+    account   include       login
+
+    auth      requisite     pam_nologin.so
+    auth      required      pam_succeed_if.so uid >= 1000 quiet
+    auth      sufficient    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+    auth      optional      ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+
+    password  required      ${pkgs.fprintd}/lib/security/pam_fprintd.so
+
+    session   include       login
+  '';
+
   services = {
     fprintd.enable = true;
     fwupd.enable = true;
