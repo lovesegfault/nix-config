f2ca3bbd14e67a950c57d9f575f488df44ec4ef9
Bernardo Meurer <bernardo@meurer.org>
2022-12-20 10:18:59 -0300

    refactor(nix/overlays/sway-scene-graph): remove outdated overrides

--- nix/overlays/sway-scene-graph.nix
+++ nix/overlays/sway-scene-graph.nix
@@ -1,43 +1,14 @@
 final: prev:
 with final.lib;
 {
-  libdrm_latest = final.libdrm.overrideAttrs (_: rec {
-    version = "2.4.114";
-    src = final.fetchurl {
-      url = "https://dri.freedesktop.org/libdrm/libdrm-${version}.tar.xz";
-      hash = "sha256-MEnPhDpH0S5e7vvDvjSW14L6CfQjRr8Lfe/j0eWY0CY=";
-    };
-  });
-
-  hwdata_latest = final.hwdata.overrideAttrs (_: rec {
-    version = "0.364";
-    src = final.fetchFromGitHub {
-      owner = "vcrhonek";
-      repo = "hwdata";
-      rev = "v${version}";
-      hash = "sha256-9fGYoyj7vN3j72H+6jv/R0MaWPZ+4UNQhCSWnZRZZS4=";
-    };
-
-    outputHash = null;
-  });
-
-  wayland-protocols_latest = final.wayland-protocols.overrideAttrs (old: rec {
-    inherit (old) pname;
-    version = "1.31";
-    src = final.fetchurl {
-      url = "https://gitlab.freedesktop.org/wayland/${pname}/-/releases/${version}/downloads/${pname}-${version}.tar.xz";
-      hash = "sha256-oH+nIu2HZ27AINhncUvJovJMRk2nORLzlwbu71IZ4jg=";
-    };
-  });
-
-  mesa_latest = (final.mesa.overrideAttrs (old: rec {
+  mesa_latest = final.mesa.overrideAttrs (old: rec {
     version = "22.3.0";
     src = final.fetchurl {
       url = "https://gitlab.freedesktop.org/mesa/mesa/-/archive/mesa-${version}/mesa-mesa-${version}.tar.gz";
       hash = "sha256-iTJpmr9G0uy26N0gjsK45n6lde8cuW41xFGQVzbhrrA=";
     };
     mesonFlags = remove "-Dxvmc-libs-path=${placeholder "drivers"}/lib" old.mesonFlags;
-  })).override { libdrm = final.libdrm_latest; wayland-protocols = final.wayland-protocols_latest; };
+  });
 
   wlroots_0_17 = (final.wlroots_0_15.overrideAttrs (old: {
     version = "unstable-2022-12-09";
@@ -48,9 +19,9 @@ with final.lib;
       rev = "8f58c060fd6095e34ede2a2d1c14caea517636e7";
       hash = "sha256-XRBwmO6MbGZ3QEq5Ly+1EUAUMS2wxMfuvH3Ry/dE1E4=";
     };
-    buildInputs = old.buildInputs ++ [ final.hwdata_latest ];
+    buildInputs = old.buildInputs ++ [ final.hwdata ];
     dontStrip = true;
-  })).override { mesa = final.mesa_latest; wayland-protocols = final.wayland-protocols_latest; };
+  })).override { mesa = final.mesa_latest; };
 
   sway-unwrapped = (prev.sway-unwrapped.overrideAttrs (old: {
     version = "scene-graph-2022-12-09";
@@ -62,5 +33,5 @@ with final.lib;
     };
     buildInputs = old.buildInputs ++ (with final; [ pcre2 xorg.xcbutilwm ]);
     dontStrip = true;
-  })).override { wayland-protocols = final.wayland-protocols_latest; wlroots = final.wlroots_0_17; };
+  })).override { wlroots = final.wlroots_0_17; };
 }
