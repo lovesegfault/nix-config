92e11b8e15d419c412900cc010c1c9af90999289
Bernardo Meurer <bernardo@meurer.org>
2022-12-27 12:02:08 -0300

    refactor(hosts/spinoza/state): link tokens.conf

--- hosts/spinoza/state.nix
+++ hosts/spinoza/state.nix
@@ -16,6 +16,7 @@
     ];
     files = [
       "/etc/machine-id"
+      "/etc/nix/tokens.conf"
       "/etc/ssh/ssh_host_ed25519_key"
       "/etc/ssh/ssh_host_ed25519_key.pub"
       "/etc/ssh/ssh_host_rsa_key"
