9ed556ed69d3f0764d3cc5a8648a2bd61b2588fe
Bernardo Meurer <bernardo@meurer.org>
2023-01-06 21:31:04 -0500

    refactor(hardware/bluetooth): disable experimental features

--- hardware/bluetooth.nix
+++ hardware/bluetooth.nix
@@ -7,8 +7,6 @@
       General = {
         JustWorksRepairing = "always";
         MultiProfile = "multiple";
-        Experimental = "true";
-        KernelExperimental = "a6695ace-ee7f-4fb9-881a-5fac66c629af 6fbaf188-05e0-496a-9885-d6ddfdb4e03e";
       };
     };
   };
