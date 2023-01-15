7fa6bed2b6b6c948fb664f71b199224cb3b433e1
Bernardo Meurer <bernardo@meurer.org>
2023-01-15 17:52:38 -0500

    feat(hosts/jung): enable chrony

--- hosts/jung/default.nix
+++ hosts/jung/default.nix
@@ -92,6 +92,13 @@
   ];
 
   services = {
+    chrony = {
+      enable = true;
+      servers = [ "time.nist.gov" "time.cloudflare.com" "time.google.com" "tick.usnogps.navy.mil" ];
+      extraConfig = ''
+        allow 192.168.50.0/24
+      '';
+    };
     fwupd.enable = true;
     smartd.enable = true;
     syncthing = {
