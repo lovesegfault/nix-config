5e4de2c9e564cf92d0daa10a38be2067bdd8bbc7
Bernardo Meurer <bernardo@meurer.org>
2023-01-15 19:49:02 -0500

    feat(services): add pihole

---
+++ services/pihole.nix
@@ -0,0 +1,28 @@
+{ config, ... }: {
+  age.secrets.pihole.file = ./pihole.age;
+
+  environment.persistence."/nix/state".directories = [
+    "/var/lib/pihole"
+  ];
+
+  networking.firewall = {
+    allowedTCPPorts = [ 80 53 5335 ];
+    allowedUDPPorts = [ 53 5335 ];
+  };
+
+  services.unbound.settings.port = "5335";
+
+  virtualisation.oci-containers.containers.pihole = {
+    autoStart = true;
+    image = "pihole/pihole:2023.1";
+    volumes = [ "/var/lib/pihole:/etc/pihole/" ];
+    environment = {
+      CUSTOM_CACHE_SIZE = "0";
+      DNSSEC = "false";
+      REV_SERVER = "true";
+      WEBTHEME = "default-darker";
+    };
+    environmentFiles = [ config.age.secrets.pihole.path ];
+    extraOptions = [ "--network=host" ];
+  };
+}
