8eac3b185b98d1ab880819c97525922715ae59a2
Bernardo Meurer <bernardo@meurer.org>
2023-01-17 13:43:11 -0500

    refactor(services/pihole): place behind nginx

--- services/pihole.nix
+++ services/pihole.nix
@@ -10,7 +10,19 @@
     allowedUDPPorts = [ 53 5335 ];
   };
 
+  security.acme.certs."pihole.${config.networking.hostName}.meurer.org" = { };
+
   services.unbound.settings.port = "5335";
+
+  services.nginx.virtualHosts."pihole.${config.networking.hostName}.meurer.org" = {
+    useACMEHost = "pihole.${config.networking.hostName}.meurer.org";
+    forceSSL = true;
+    kTLS = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:8053";
+      proxyWebsockets = true;
+    };
+  };
 
   virtualisation.oci-containers.containers.pihole = {
     autoStart = true;
@@ -20,9 +32,14 @@
       CUSTOM_CACHE_SIZE = "0";
       DNSSEC = "false";
       REV_SERVER = "true";
+      VIRTUAL_HOST = "pihole.${config.networking.hostName}.meurer.org";
       WEBTHEME = "default-darker";
     };
     environmentFiles = [ config.age.secrets.pihole.path ];
-    extraOptions = [ "--network=host" ];
+    ports = [
+      "53:53/tcp"
+      "53:53/udp"
+      "8053:80/tcp"
+    ];
   };
 }
