582db01e265c387d89a0d9343f02cbd4f69c2972
Bernardo Meurer <bernardo@meurer.org>
2023-01-21 18:33:27 -0500

    refactor(core/default): permitRootLogin -> PermitRootLogin

--- core/default.nix
+++ core/default.nix
@@ -72,7 +72,7 @@ in
   services = {
     openssh = {
       enable = true;
-      permitRootLogin = lib.mkDefault "no";
+      settings.PermitRootLogin = lib.mkDefault "no";
     };
     tailscale.enable = true;
   };
