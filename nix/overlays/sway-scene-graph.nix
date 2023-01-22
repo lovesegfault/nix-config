340ef122e5ae807aca379c98662a7fa573f1f04e
Bernardo Meurer <bernardo@meurer.org>
2023-01-21 19:24:53 -0500

    chore(nix/overlays/sway-scene-graph): update

--- nix/overlays/sway-scene-graph.nix
+++ nix/overlays/sway-scene-graph.nix
@@ -1,33 +1,34 @@
 final: prev:
 with final.lib;
 {
-  mesa_latest = final.mesa.overrideAttrs (old: rec {
-    version = "22.3.2";
-    src = final.fetchurl {
-      url = "https://gitlab.freedesktop.org/mesa/mesa/-/archive/mesa-${version}/mesa-mesa-${version}.tar.gz";
-      hash = "sha256-uG14v6C/2HeAhyAtynncfHee/plsNnRShN2tIvofg0M=";
+  libxkbcommon_1_5_0 = final.libxkbcommon.overrideAttrs (old: rec {
+    version = "1.5.0";
+    src = final.fetchFromGitHub {
+      owner = "xkbcommon";
+      repo = "libxkbcommon";
+      rev = "xkbcommon-${version}";
+      hash = "sha256-+LJa+v4eV36xBNYFDelgLIc7EyHVtspIRAluf16i774=";
     };
-    mesonFlags = remove "-Dxvmc-libs-path=${placeholder "drivers"}/lib" old.mesonFlags;
   });
 
-  wlroots_0_17 = (final.wlroots_0_16.overrideAttrs (old: {
+  wlroots_0_17 = final.wlroots_0_16.overrideAttrs (old: {
     version = "unstable-2023-02-04";
     src = final.fetchFromGitLab {
       domain = "gitlab.freedesktop.org";
       owner = "wlroots";
       repo = "wlroots";
-      rev = "e0b2bf2a6b8699ac75973702a791efe5af8d421e";
-      hash = "sha256-CtdYMeWXsCblM+HenD7AP2oqKd+gi+7MoTKANccOPHI=";
+      rev = "5007e713b4c0d0f0cd8e97503294b81a250d3459";
+      hash = "sha256-OgdGCBLI0YWr4PSXgQhTXEVNcMutLm+Mr7i57q7iH+0=";
     };
-  })).override { mesa = final.mesa_latest; };
+  });
 
   sway-unwrapped = (prev.sway-unwrapped.overrideAttrs (old: {
     version = "scene-graph-2022-12-09";
     src = final.fetchFromGitHub {
       owner = "Nefsen402";
       repo = "sway";
-      rev = "993deff56993dd5565061a73b6ca2d65cfa6e135";
-      hash = "sha256-6bN3ux/POQwMeNkN8r/sgg+3p6agbgK+1FKzK9oCxiA=";
+      rev = "c77e6c0008747f1de4dee7912009bcba043cb257";
+      hash = "sha256-W99yp35sFiS/WwGHn8kmPYS+B/6ldYVMOrF1mq6L+Cs=";
     };
-  })).override { wlroots_0_16 = final.wlroots_0_17; };
+  })).override { libxkbcommon = final.libxkbcommon_1_5_0; wlroots_0_16 = final.wlroots_0_17; };
 }
