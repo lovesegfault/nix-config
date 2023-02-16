4d549af5d09178c35f4e41d10dd18dc0a05a2c85
Bernardo Meurer <bernardo@meurer.org>
2023-02-16 10:19:15 -0500

    chore(nix/overlays/sway-scene-graph): sync

--- nix/overlays/sway-scene-graph.nix
+++ nix/overlays/sway-scene-graph.nix
@@ -12,23 +12,24 @@ with final.lib;
   });
 
   wlroots_0_17 = final.wlroots_0_16.overrideAttrs (old: {
-    version = "unstable-2023-02-04";
+    version = "unstable-2023-02-15";
     src = final.fetchFromGitLab {
       domain = "gitlab.freedesktop.org";
       owner = "wlroots";
       repo = "wlroots";
-      rev = "5007e713b4c0d0f0cd8e97503294b81a250d3459";
-      hash = "sha256-OgdGCBLI0YWr4PSXgQhTXEVNcMutLm+Mr7i57q7iH+0=";
+      rev = "822eb07eaca392d9b7a99b0d8b96e402c8976fb4";
+      hash = "sha256-ntJlF0PXguPsWCOJ7WQYcOKVpESlFaWDapdDGRybVyM=";
     };
+    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ final.hwdata ];
   });
 
   sway-unwrapped = (prev.sway-unwrapped.overrideAttrs (old: {
-    version = "scene-graph-2022-12-09";
+    version = "scene-graph-2023-02-15";
     src = final.fetchFromGitHub {
       owner = "Nefsen402";
       repo = "sway";
-      rev = "c77e6c0008747f1de4dee7912009bcba043cb257";
-      hash = "sha256-W99yp35sFiS/WwGHn8kmPYS+B/6ldYVMOrF1mq6L+Cs=";
+      rev = "d767e1a92cba244fdf971641835e73665a9436a3";
+      hash = "sha256-LzeS7BcsceFQj0Sfbqi/MzjH9hrAyq1b5rSzo8xzbM8=";
     };
   })).override { libxkbcommon = final.libxkbcommon_1_5_0; wlroots_0_16 = final.wlroots_0_17; };
 }
