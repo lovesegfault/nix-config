95c9ce546e1b30807ae145057a1a3e33445b93fe
Bernardo Meurer <bernardo@meurer.org>
2023-03-17 17:56:58 -0300

    feat(flake): config substituters

--- flake.nix
+++ flake.nix
@@ -1,6 +1,19 @@
 {
   description = "lovesegfault's NixOS config";
 
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-config.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-config.cachix.org-1:Vd6raEuldeIZpttVQfrUbLvXJHzzzkS0pezXCVVjDG4="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     agenix = {
       url = "github:ryantm/agenix";
