304b7e4d3efa902eaa2d05dde0ba63c0de8b6b52
Bernardo Meurer <bernardo@meurer.org>
2023-03-28 15:35:50 -0300

    refactor(users/bemeurer/core/fish): use stylix

--- users/bemeurer/core/fish.nix
+++ users/bemeurer/core/fish.nix
@@ -7,7 +7,6 @@
         set -g fish_greeting
       '')
       (lib.mkAfter ''
-        enable_ayu_theme_dark
         ${pkgs.any-nix-shell}/bin/any-nix-shell fish | source
 
         fish_vi_key_bindings insert
@@ -16,15 +15,6 @@
       '')
     ];
     plugins = [
-      {
-        name = "ayu-theme.fish";
-        src = pkgs.fetchFromGitHub {
-          owner = "edouard-lopez";
-          repo = "ayu-theme.fish";
-          rev = "d351d24263d87bef3a90424e0e9c74746673e383";
-          hash = "sha256-rx9izD2pc3hLObOehuiMwFB4Ta5G1lWVv9Jdb+JHIz0=";
-        };
-      }
       {
         name = "autopair.fish";
         src = pkgs.fetchFromGitHub {
