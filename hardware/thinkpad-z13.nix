1ff0bf2852601eb26cfeccfe3af2ad1a7a1cbf47
Bernardo Meurer <bernardo@meurer.org>
2023-03-28 14:12:51 -0300

    refactor(hardware/thinkpad-z13): remove microsoft guid preference

--- hardware/thinkpad-z13.nix
+++ hardware/thinkpad-z13.nix
@@ -16,10 +16,7 @@
     kernelModules = [ "cpufreq_conservative" "cpufreq_ondemand" "kvm-amd" "thinkpad_acpi" ];
     kernel.sysctl = { "vm.swappiness" = 1; };
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelParams = [
-      "nowatchdog"
-      "acpi.prefer_microsoft_guid=1" # XXX: Maybe helps with sleep?
-    ];
+    kernelParams = [ "nowatchdog" ];
     tmpOnTmpfs = true;
   };
 
