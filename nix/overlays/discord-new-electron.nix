5bb2383b80d0a063b1691d37a53da1b5c64d5e86
Bernardo Meurer <bernardo@meurer.org>
2023-03-29 14:54:09 -0300

    refactor(nix/overlays/discord-new-electron): don't overlay on macOS

--- nix/overlays/discord-new-electron.nix
+++ nix/overlays/discord-new-electron.nix
@@ -1,18 +1,19 @@
 final: prev: {
-  discord = (prev.discord.overrideAttrs (old:
+  discord = if final.stdenv.hostPlatform.isMacOS then prev.discord else final.discord_new_electron;
+  discord_new_electron = (prev.discord.overrideAttrs (old:
     let
       binaryName = "Discord";
       inherit (final) runtimeShell lib electron;
     in
     {
       nativeBuildInputs = old.nativeBuildInputs ++ [ final.nodePackages.asar ];
-      postInstall = (old.postInstall or "") + (lib.optionalString final.stdenv.hostPlatform.isLinux ''
+      postInstall = (old.postInstall or "") + ''
         rm $out/opt/${binaryName}/${binaryName}
         cat << EOF > $out/opt/${binaryName}/${binaryName}
         #!${runtimeShell}
         ${lib.getExe electron} "$out/opt/${binaryName}/resources/app.asar" "$@"
         EOF
         chmod +x $out/opt/${binaryName}/${binaryName}
-      '');
+      '';
     })).override { withOpenASAR = true; };
 }
