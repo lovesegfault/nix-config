70a90140778c87b11ca777abf6d0f5d456994528
Bernardo Meurer <bernardo@meurer.org>
2023-04-03 18:15:24 -0300

    refactor(graphical): move back to gdm

--- graphical/nixos.nix
+++ graphical/nixos.nix
@@ -1,6 +1,4 @@
 { pkgs, ... }: {
-  imports = [ ./greetd.nix ];
-
   boot = {
     consoleLogLevel = 3;
     kernelParams = [
@@ -13,9 +11,29 @@
 
   programs.dconf.enable = true;
 
+  security.pam.services.gdm-fingerprint.text = ''
+    account  include    login
+
+    auth     requisite  pam_nologin.so
+    auth     required   pam_succeed_if.so uid >= 1000 quiet
+    auth     sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so
+    auth     optional   ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+
+    password required   ${pkgs.fprintd}/lib/security/pam_fprintd.so
+    password optional   ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so use_authok
+
+    session  include    login
+  '';
+
   services = {
     dbus.packages = with pkgs; [ dconf ];
     gnome.at-spi2-core.enable = true;
+    xserver.enable = true;
+    xserver.displayManager.gdm = {
+      enable = true;
+      autoSuspend = true;
+      wayland = true;
+    };
   };
 
   stylix.targets.plymouth.enable = false;
