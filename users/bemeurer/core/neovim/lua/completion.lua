be90117febb076bfc0a80b62c5d78daa2f22adaf
Bernardo Meurer <bernardo@meurer.org>
2023-04-13 16:59:53 -0300

    refactor(users/bemeurer/core/neovim): rely on autocmd for crates cmp src

--- users/bemeurer/core/neovim/lua/completion.lua
+++ users/bemeurer/core/neovim/lua/completion.lua
@@ -72,7 +72,6 @@ cmp.setup({
   sources = {
     { name = "nvim_lsp" },
     { name = "nvim_lsp_signature_help" },
-    { name = "crates" },
     { name = "luasnip" },
     { name = "cmp_tabnine" },
     { name = "treesitter" },
