0b918f6ef4f81d7cccd2b441099dda32673e0840
Bernardo Meurer <bernardo@meurer.org>
2023-04-19 10:27:43 -0300

    refactor(core/nix): use nix 2.15

--- core/nix.nix
+++ core/nix.nix
@@ -1,5 +1,6 @@
-{ hostType, lib, ... }: {
+{ hostType, lib, pkgs, ... }: {
   nix = {
+    package = pkgs.nixVersions.nix_2_15;
     settings = {
       accept-flake-config = true;
       # XXX: Causes annoying "cannot link ... to ...: File exists" errors on Darwin
