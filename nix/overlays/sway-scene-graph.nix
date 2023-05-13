24a906f81a811731063646571f556923d9ea9d3c
Bernardo Meurer <bernardo@meurer.org>
2023-05-13 18:19:45 -0400

    chore(nix/overlays/sway-scene-graph): update

--- nix/overlays/sway-scene-graph.nix
+++ nix/overlays/sway-scene-graph.nix
@@ -1,25 +1,13 @@
 final: prev:
 {
-  wayland_1_22 = final.wayland.overrideAttrs (old: rec {
-    version = "1.22.0";
-
-    src = final.fetchFromGitLab {
-      domain = "gitlab.freedesktop.org";
-      owner = "wayland";
-      repo = "wayland";
-      rev = version;
-      hash = "sha256-Y1ePN+FVOigDsnPBnDKkhYnQrm+Obwf/YcCKr/clErw=";
-    };
-  });
-
   wlroots_0_17 = (final.wlroots_0_16.overrideAttrs (old: {
     version = "unstable-2023-04-16";
     src = final.fetchFromGitLab {
       domain = "gitlab.freedesktop.org";
       owner = "wlroots";
       repo = "wlroots";
-      rev = "eac2eaa6a97872d0eaab3b7725528d1ad65c80e2";
-      hash = "sha256-xwjColWIWQGhywPTUcii8Syvk99bnAf+2f6+ti8369Y=";
+      rev = "10dd41669492f7582c26bd3989536a66278df75d";
+      hash = "sha256-lYmvTWMNcD/1cU7x2CHjLtCtivsO2xz4E87YcPZg24s=";
     };
     nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ final.hwdata ];
     buildInputs = (old.buildInputs or [ ]) ++ [
@@ -27,15 +15,15 @@ final: prev:
       final.libliftoff
     ];
     mesonFlags = (old.mesonFlags or [ ]) ++ [ "-Dwerror=false" ];
-  })).override { wayland = final.wayland_1_22; };
+  })).override { };
 
   sway-unwrapped = (prev.sway-unwrapped.overrideAttrs (old: {
     version = "scene-graph-2023-04-16";
     src = final.fetchFromGitHub {
       owner = "Nefsen402";
       repo = "sway";
-      rev = "3b3a500ed71be9bf6b2b252b24920d1d02c9ee36";
-      hash = "sha256-vV+z5BqHyCWlsA4q4d1g/UPuURGmUMFg36LXbJTLORU=";
+      rev = "01587db619375753cd925c0c2259b79c8a370f87";
+      hash = "sha256-IC1zgvpDjdu5ND0gdpWA+uTMGTcU+UBqU+cnSjLU//M=";
     };
 
     nativeBuildInputs = with final; (old.nativeBuildInputs or [ ]) ++ [ bash-completion fish ];
