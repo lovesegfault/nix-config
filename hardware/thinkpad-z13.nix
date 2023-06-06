85c88976ecf2d6f8bd3c5cc33c4582b863a97fd8
Bernardo Meurer <bernardo@meurer.org>
2023-06-06 13:03:32 -0400

    feat(hardware/thinkpad-z13): use alpha TLP

--- hardware/thinkpad-z13.nix
+++ hardware/thinkpad-z13.nix
@@ -51,8 +51,11 @@
     tlp = {
       enable = true;
       settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
-        CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
         PLATFORM_PROFILE_ON_AC = "performance";
         PLATFORM_PROFILE_ON_BAT = "low-power";
