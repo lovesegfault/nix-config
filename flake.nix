7fdbe2eb0e9060f0385b8bb3f5137aed788a4842
Bernardo Meurer <bernardo@meurer.org>
2023-07-26 10:57:07 -0400

    fix(flake): don't shadow default substituters

--- flake.nix
+++ flake.nix
@@ -2,13 +2,11 @@
   description = "lovesegfault's NixOS config";
 
   nixConfig = {
-    trusted-substituters = [
-      "https://cache.nixos.org"
+    extra-trusted-substituters = [
       "https://nix-config.cachix.org"
       "https://nix-community.cachix.org"
     ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    extra-trusted-public-keys = [
       "nix-config.cachix.org-1:Vd6raEuldeIZpttVQfrUbLvXJHzzzkS0pezXCVVjDG4="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
