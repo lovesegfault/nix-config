230a213dc4ccab72e0d7f3ea96883f90608688fe
Bernardo Meurer <bernardo@meurer.org>
2023-08-17 10:24:38 -0400

    feat(hosts/{derrida,luther}): add rust-analyzer

--- hosts/luther/default.nix
+++ hosts/luther/default.nix
@@ -3,7 +3,7 @@
 
   home = {
     uid = 22314791;
-    packages = with pkgs; [ rustup ];
+    packages = with pkgs; [ rustup rust-analyzer ];
     shellAliases = {
       bre = "brazil-runtime-exec";
       brc = "brazil-recursive-cmd";
