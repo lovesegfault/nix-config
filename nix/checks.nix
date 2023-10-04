8fa7d3fef6783b00be25cc689b0388456fe5b52c
Bernardo Meurer <bernardo@meurer.org>
2023-10-04 14:00:27 -0400

    refactor(nix/checks): incorporate parsing check

--- nix/checks.nix
+++ nix/checks.nix
@@ -8,9 +8,23 @@ system:
 with self.pkgs.${system};
 
 {
+  parse = stdenvNoCC.mkDerivation {
+    name = "check-parse-nix";
+    src = lib.cleanSource self;
+    nativeBuildInputs = [ nix ];
+    dontPatch = true;
+    dontConfigure = true;
+    dontFixup = true;
+    buildPhase = ''
+      nix-instantiate --json --parse "$src"/**/*.nix
+    '';
+    installPhase = ''
+      touch "$out"
+    '';
+  };
   pre-commit-check = pre-commit-hooks.lib.${system}.run
     {
-      src = lib.cleanSource ../.;
+      src = lib.cleanSource self;
       hooks = {
         actionlint.enable = true;
         luacheck.enable = true;
