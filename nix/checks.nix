c2236fd5622e7581d7c00cab05061d2a16d47623
Bernardo Meurer <bernardo@meurer.org>
2023-10-04 15:03:26 -0400

    fix(nix/checks): use --readonly-mode when parsing

--- nix/checks.nix
+++ nix/checks.nix
@@ -8,20 +8,15 @@ system:
 with self.pkgs.${system};
 
 {
-  parse = stdenvNoCC.mkDerivation {
-    name = "check-parse-nix";
-    src = lib.cleanSource self;
-    nativeBuildInputs = [ nix ];
-    dontPatch = true;
-    dontConfigure = true;
-    dontFixup = true;
-    buildPhase = ''
-      nix-instantiate --json --parse "$src"/**/*.nix
-    '';
-    installPhase = ''
+  parse-nix = runCommandNoCC "parse-nix"
+    {
+      src = lib.cleanSource self;
+      nativeBuildInputs = [ nix ];
+    }
+    ''
+      nix-instantiate --readonly-mode --json --parse "$src"/**/*.nix
       touch "$out"
     '';
-  };
   pre-commit-check = pre-commit-hooks.lib.${system}.run
     {
       src = lib.cleanSource self;
