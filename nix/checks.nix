879e6d25fbd8d4b5090e13aed2232977ff19ad20
Bernardo Meurer <bernardo@meurer.org>
2023-10-06 10:03:47 -0400

    refactor(nix/checks): move parse check into pre-commit

--- nix/checks.nix
+++ nix/checks.nix
@@ -8,15 +8,6 @@ system:
 with self.pkgs.${system};
 
 {
-  parse-nix = runCommandNoCC "parse-nix"
-    {
-      src = lib.cleanSource self;
-      nativeBuildInputs = [ nix ];
-    }
-    ''
-      nix-instantiate --readonly-mode --store dummy:// --json --parse "$src"/**/*.nix
-      touch "$out"
-    '';
   pre-commit-check = pre-commit-hooks.lib.${system}.run
     {
       src = lib.cleanSource self;
@@ -32,6 +23,14 @@ with self.pkgs.${system};
         shfmt.enable = true;
         statix.enable = true;
         stylua.enable = true;
+
+        nix-parse = {
+          enable = true;
+          name = "nix-parse";
+          description = "Parses all Nix files";
+          entry = "${nix}/bin/nix-instantiate --readonly-mode --store dummy:// --json --parse";
+          files = "\\.nix$";
+        };
       };
     };
 }
