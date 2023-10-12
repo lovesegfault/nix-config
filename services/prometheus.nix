0b1b7a018759e4e5452c6976afe1956b30365ae4
Bernardo Meurer <bernardo@meurer.org>
2023-10-12 08:37:02 -0400

    refactor(services/prometheus): increase scrape intervals

--- services/prometheus.nix
+++ services/prometheus.nix
@@ -11,12 +11,12 @@
     scrapeConfigs = [
       {
         job_name = "node";
-        scrape_interval = "10s";
+        scrape_interval = "1m";
         static_configs = [{ targets = [ "127.0.0.1:9100" ]; }];
       }
       {
         job_name = "prometheus";
-        scrape_interval = "30s";
+        scrape_interval = "1m";
         static_configs = [{ targets = [ "127.0.0.1:9090" ]; }];
       }
     ];
