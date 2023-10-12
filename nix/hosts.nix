b1fd0184740a115734a048bf12306e6eaac43fcd
Bernardo Meurer <bernardo@meurer.org>
2023-10-12 09:27:05 -0400

    refactor(nix/hosts): add mkHost fn

--- nix/hosts.nix
+++ nix/hosts.nix
@@ -1,71 +1,85 @@
+let
+  mkHost =
+    { type, hostPlatform, address ? null, pubkey ? null, remoteBuild ? true, homeDirectory ? null }:
+    if type == "nixos" then
+      assert address != null && pubkey != null;
+      {
+        inherit type hostPlatform address pubkey remoteBuild;
+      }
+    else if type == "darwin" then
+      assert pubkey != null;
+      {
+        inherit type hostPlatform pubkey;
+      }
+    else if type == "home-manager" then
+      assert homeDirectory != null;
+      {
+        inherit type hostPlatform homeDirectory;
+      }
+    else throw "unknown host type '${type}'";
+in
 {
-  aurelius = {
+  aurelius = mkHost {
     type = "nixos";
     address = "100.69.178.40";
     hostPlatform = "aarch64-linux";
     pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRlfGCSK2w34ckIGoRHaZ01CbF/7Zk4VNmyokkvg7cF";
     remoteBuild = false;
   };
-  bohr = {
+  bohr = mkHost {
     type = "nixos";
     address = "100.123.20.11";
     hostPlatform = "x86_64-linux";
     pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBTh+kYOeeYoBuxvA00nGojfBHUQlXW3iF7aRIw9VbY1";
-    remoteBuild = true;
   };
-  derrida = {
+  derrida = mkHost {
     type = "home-manager";
     hostPlatform = "x86_64-linux";
     homeDirectory = "/home/bemeurer";
   };
-  fourier = {
+  fourier = mkHost {
     type = "nixos";
     address = "100.77.107.1";
     hostPlatform = "x86_64-linux";
     pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMJEc036Z0umFUeSgksRgBWhcEeqiVhuXNQZTipZVRMn";
-    remoteBuild = true;
   };
-  goethe = {
+  goethe = mkHost {
     type = "home-manager";
     hostPlatform = "x86_64-linux";
     homeDirectory = "/home/bemeurer";
   };
-  jung = {
+  jung = mkHost {
     type = "nixos";
     address = "100.80.1.112";
     hostPlatform = "x86_64-linux";
     pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHws1wwXYHDmU+Bjcbw8IZv2V+fbxaTDQc44XoUQ604t";
-    remoteBuild = true;
   };
-  luther = {
+  luther = mkHost {
     type = "home-manager";
     hostPlatform = "aarch64-linux";
     homeDirectory = "/home/bemeurer";
   };
-  nozick = {
+  nozick = mkHost {
     type = "nixos";
     address = "100.124.29.84";
     hostPlatform = "x86_64-linux";
     pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBEzb5JCgcXJZHDkY09vBAvIF34JabI+ZBpGqJDy6KbI";
-    remoteBuild = true;
   };
-  poincare = {
+  poincare = mkHost {
     type = "darwin";
     hostPlatform = "aarch64-darwin";
     pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMYvFEyV+nebaTfrwAULWDmCk0L6O+1OyZc43JnizcIB";
   };
-  riemann = {
+  riemann = mkHost {
     type = "nixos";
     address = "100.67.173.60";
     hostPlatform = "aarch64-linux";
     pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOof4536ylMfznpkKbH/kqiuCOs2hCLXMBnF9md462sW";
-    remoteBuild = true;
   };
-  spinoza = {
+  spinoza = mkHost {
     type = "nixos";
     address = "100.68.240.30";
     hostPlatform = "x86_64-linux";
     pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDUZPmPTATZ4nBWstPqlUiguvxr26XWAE9BGPVNNRBR5";
-    remoteBuild = true;
   };
 }
