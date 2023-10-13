470c9ee1af6dbb5ee113655f0e6f37588252c87d
Bernardo Meurer <bernardo@meurer.org>
2023-10-13 10:02:52 -0400

    refactor(users/bemeurer/core/zsh): get plugins from nixpkgs

--- users/bemeurer/core/zsh.nix
+++ users/bemeurer/core/zsh.nix
@@ -40,7 +40,10 @@
       bindkey -rpM viins '^[^['
       KEYTIMEOUT=1
 
+      source ${pkgs.zsh-autopair.src}/zsh-autopair.plugin.zsh
+      source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
       ${pkgs.nix-your-shell}/bin/nix-your-shell zsh | source /dev/stdin
+
       # 1Password CLI
       if [ -e "$HOME/.config/op/plugins.sh" ]; then
         source "$HOME/.config/op/plugins.sh"
@@ -49,29 +52,5 @@
     sessionVariables = {
       RPROMPT = "";
     };
-    plugins = [
-      {
-        # https://github.com/softmoth/zsh-vim-mode
-        name = "zsh-vim-mode";
-        file = "zsh-vim-mode.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "softmoth";
-          repo = "zsh-vim-mode";
-          rev = "abef0c0c03506009b56bb94260f846163c4f287a";
-          sha256 = "0cnjazclz1kyi13m078ca2v6l8pg4y8jjrry6mkvszd383dx1wib";
-        };
-      }
-      {
-        # https://github.com/hlissner/zsh-autopair
-        name = "zsh-autopair";
-        file = "zsh-autopair.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "hlissner";
-          repo = "zsh-autopair";
-          rev = "34a8bca0c18fcf3ab1561caef9790abffc1d3d49";
-          sha256 = "1h0vm2dgrmb8i2pvsgis3lshc5b0ad846836m62y8h3rdb3zmpy1";
-        };
-      }
-    ];
   };
 }
