e98c55c4baaf9e48231a0668d3374a04f99cfd2d
Bernardo Meurer <bernardo@meurer.org>
2023-10-26 11:51:39 -0300

    chore(nix/overlays/sway-scene-graph): update

--- nix/overlays/sway-scene-graph.nix
+++ nix/overlays/sway-scene-graph.nix
@@ -1,13 +1,13 @@
 final: prev:
 {
   wlroots_0_17 = (final.wlroots_0_16.overrideAttrs (old: {
-    version = "unstable-2023-10-03";
+    version = "unstable-2023-10-25";
     src = final.fetchFromGitLab {
       domain = "gitlab.freedesktop.org";
       owner = "wlroots";
       repo = "wlroots";
-      rev = "1b0694b79481643cb456d03e1be50a1b4f6ca591";
-      hash = "sha256-3yudCZTkisn8FSwMGid3VWcbDsYZtA6XHrxdA6hRdcM=";
+      rev = "ffa8e3ec81c093b809c5902e082826120a09429e";
+      hash = "sha256-hbqKTtP0bSCw6N/kN/c3QlQzwR42q6tG4K3XMfkf5C4=";
     };
     nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ final.hwdata ];
     buildInputs = (old.buildInputs or [ ]) ++ [
@@ -17,12 +17,12 @@ final: prev:
   })).override { };
 
   sway-unwrapped = (prev.sway-unwrapped.overrideAttrs (old: {
-    version = "scene-graph-2023-08-21";
+    version = "scene-graph-2023-10-25";
     src = final.fetchFromGitHub {
       owner = "Nefsen402";
       repo = "sway";
-      rev = "e6a87c619b9af8f263720ffd511dbd221a81eb94";
-      hash = "sha256-EltpBOr6e9ifr0AiDzzzMGCgWa5rDNmFBUyy4NMjEb8=";
+      rev = "06efc587beb95db72f091a911c3b80746af9cee6";
+      hash = "sha256-jR+E45zsyfjvIaSmj33+3Fr9daSFCznZ71ASz6fM7io=";
     };
 
     nativeBuildInputs = with final; (old.nativeBuildInputs or [ ]) ++ [ bash-completion fish ];
