2f1980a32a3817d2310e8f82cb4a451770517787
Bernardo Meurer <bernardo@meurer.org>
2023-11-14 18:32:14 -0300

    refactor(users/bemeurer/graphical/sway): disable libliftoff

--- users/bemeurer/graphical/sway/sway.nix
+++ users/bemeurer/graphical/sway/sway.nix
@@ -55,7 +55,6 @@
       export _JAVA_AWT_WM_NONREPARENTING=1
       export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true"
 
-      export WLR_DRM_FORCE_LIBLIFTOFF=1
       export WLR_RENDERER=vulkan
     '';
 
