ffc81491424d14db37a5b32d7586e65b4689e360
Bernardo Meurer <bernardo@meurer.org>
2023-12-21 18:22:13 -0300

    refactor(hardware/thinkpad-z13): use balanced perf on battery

--- hardware/thinkpad-z13.nix
+++ hardware/thinkpad-z13.nix
@@ -55,10 +55,10 @@
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
 
         PLATFORM_PROFILE_ON_AC = "performance";
-        PLATFORM_PROFILE_ON_BAT = "low-power";
+        PLATFORM_PROFILE_ON_BAT = "balanced";
 
         DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = [ "bluetooth" "wifi" ];
         DEVICES_TO_ENABLE_ON_AC = [ "bluetooth" "wifi" ];
