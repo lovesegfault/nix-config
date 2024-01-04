2c9a9551d4c5b996570d2f90aef1959bcfdb1df9
Bernardo Meurer <bernardo@meurer.org>
2024-01-04 17:30:37 -0300

    feat(hosts/nozick/nextcloud): 27 -> 28

--- hosts/nozick/nextcloud.nix
+++ hosts/nozick/nextcloud.nix
@@ -19,7 +19,7 @@
       autoUpdateApps.enable = true;
       hostName = "nextcloud.meurer.org";
       https = true;
-      package = pkgs.nextcloud27;
+      package = pkgs.nextcloud28;
       config = {
         adminpassFile = config.age.secrets.nextcloud.path;
         dbhost = "/run/postgresql";
