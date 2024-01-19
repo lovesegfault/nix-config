6af5365ae55298e7b8ee20d544e7bebb21103bbc
Bernardo Meurer <bernardo@meurer.org>
2024-01-19 11:41:52 -0500

    fix(nix/overlays/nix-unstable): workaround electron dependency on nix-prefetch-git

--- nix/overlays/nix-unstable.nix
+++ nix/overlays/nix-unstable.nix
@@ -4,10 +4,15 @@ let
     { inherit name; value = prev.${name}.override { nix = final.nixVersions.unstable; }; };
   useUnstableNix = names: builtins.listToAttrs (map useUnstableNixFor names);
 in
-useUnstableNix [
+(useUnstableNix [
   "agenix"
   "nix-direnv"
-  "nix-prefetch-git"
   "nix-update"
   "nixpkgs-review"
-]
+]) // {
+  # Workaround for electron depending on nix-prefetch-git at build-time via
+  # prefetch-yarn-deps
+  nix-prefetch-git = prev.nix-prefetch-git.override { nix = final.nixVersions.unstable; };
+  nix-prefetch-git-stable = prev.nix-prefetch-git;
+  prefetch-yarn-deps = prev.prefetch-yarn-deps.override { nix-prefetch-git = final.nix-prefetch-git-stable; };
+}
