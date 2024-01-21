35f62060b4572348b979bdc4b15cb5b108873eeb
Bernardo Meurer <bernardo@meurer.org>
2024-01-21 13:36:57 -0500

    feat(users/bemeurer/core/neovim): enable bufls

--- users/bemeurer/core/neovim/lua/lsp.lua
+++ users/bemeurer/core/neovim/lua/lsp.lua
@@ -67,7 +67,7 @@ local on_attach = function(_, bufnr)
 end
 
 -- Enable the following language servers
-local servers = { "clangd", "pyright", "ruff_lsp", "texlab" }
+local servers = { "bufls", "clangd", "pyright", "ruff_lsp", "texlab" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup({ on_attach = on_attach })
 end
