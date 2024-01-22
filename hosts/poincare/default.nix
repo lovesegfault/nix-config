201ed7bf349fc2c6fa477399b9dac2e6ee061f04
Bernardo Meurer <bernardo@meurer.org>
2024-01-22 10:14:29 -0500

    feat(hosts/poincare): use ng protocol for linux-builder

--- hosts/poincare/default.nix
+++ hosts/poincare/default.nix
@@ -26,6 +26,7 @@
       hostName = "linux-builder";
       sshUser = "builder";
       sshKey = "/etc/nix/builder_ed25519";
+      protocol = "ssh-ng";
       system = "${pkgs.stdenv.hostPlatform.uname.processor}-linux";
       publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo=";
       inherit (config.nix.linux-builder) maxJobs supportedFeatures;
