63bd97d9f5008c1b3acfa2a2555a76fdb930d140
Bernardo Meurer <bernardo@meurer.org>
2024-02-01 11:35:43 -0500

    chore(nix/overlays/vim-plugins): update lsp-progress.nvim

--- nix/overlays/vim-plugins.nix
+++ nix/overlays/vim-plugins.nix
@@ -5,8 +5,8 @@ final: prev: {
       src = final.fetchFromGitHub {
         owner = "linrongbin16";
         repo = "lsp-progress.nvim";
-        rev = "d4c5440e6bccfcd0aeb3070a53290272cf5a332a";
-        hash = "sha256-DHMnOngdm9ql/w7eyD4MXfYTO3iSkDLK8Go3OEKARnw=";
+        rev = "36c84b33bed21f33e62e778b0567eb59ec21dc38";
+        hash = "sha256-njUl/TBjjuvcz8tPktQbuJuofe25X36hR+vzLWMqFl0=";
       };
     };
   };
