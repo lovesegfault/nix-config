032a1fc24eb040c4f22cf4b826585bd9e501871c
Bernardo Meurer <bernardo@meurer.org>
2024-03-20 11:51:01 -0400

    refactor(hosts/poincare): set protocol

--- hosts/poincare/default.nix
+++ hosts/poincare/default.nix
@@ -22,15 +22,6 @@
 
   nix = {
     gc.automatic = true;
-    buildMachines = lib.mkForce [{
-      hostName = "linux-builder";
-      sshUser = "builder";
-      sshKey = "/etc/nix/builder_ed25519";
-      protocol = "ssh-ng";
-      system = "${pkgs.stdenv.hostPlatform.uname.processor}-linux";
-      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo=";
-      inherit (config.nix.linux-builder) maxJobs supportedFeatures;
-    }];
     linux-builder = {
       enable = true;
       ephemeral = true;
@@ -40,6 +31,7 @@
         virtualisation.host.pkgs = lib.mkForce (pkgs.extend (final: _: { nix = final.nixVersions.unstable; }));
       };
       maxJobs = 4;
+      protocol = "ssh-ng";
     };
     settings = {
       max-substitution-jobs = 20;
