b16b24ac22ac5bd01e7b978b7b1974d8c0b7a071
Bernardo Meurer <bernardo@meurer.org>
2024-03-27 10:31:48 -0400

    fix(services/oauth2): enforce ACME host for auth vhost

--- services/oauth2.nix
+++ services/oauth2.nix
@@ -3,19 +3,28 @@ with config.networking;
 {
   age.secrets.oauth2.file = ./oauth2.age;
 
-  services.oauth2_proxy = {
-    enable = true;
-    nginx.domain = "auth.${hostName}.meurer.org";
-    cookie.domain = ".${hostName}.meurer.org";
-    email.domains = [ "meurer.org" ];
-    keyFile = config.age.secrets.oauth2.path;
-    reverseProxy = true;
-    passBasicAuth = true;
-    setXauthrequest = true;
-    extraConfig = {
-      skip-provider-button = true;
-      whitelist-domain = "*.${hostName}.meurer.org";
-      cookie-samesite = "lax";
+  security.acme.certs."auth.${hostName}.meurer.org" = { };
+
+  services = {
+    oauth2_proxy = {
+      enable = true;
+      nginx.domain = "auth.${hostName}.meurer.org";
+      cookie.domain = ".${hostName}.meurer.org";
+      email.domains = [ "meurer.org" ];
+      keyFile = config.age.secrets.oauth2.path;
+      reverseProxy = true;
+      passBasicAuth = true;
+      setXauthrequest = true;
+      extraConfig = {
+        skip-provider-button = true;
+        whitelist-domain = "*.${hostName}.meurer.org";
+        cookie-samesite = "lax";
+      };
+    };
+    nginx.virtualHosts."auth.${hostName}.meurer.org" = {
+      useACMEHost = "auth.${hostName}.meurer.org";
+      forceSSL = true;
+      kTLS = true;
     };
   };
 }
