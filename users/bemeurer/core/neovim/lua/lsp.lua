91e757f7c4ab9c919c798db3fff72aebfbc3725a
Bernardo Meurer <bernardo@meurer.org>
2024-04-29 11:06:37 -0400

    refactor(users/bemeurer/core/neovim): remove ruby_ls

--- users/bemeurer/core/neovim/lua/lsp.lua
+++ users/bemeurer/core/neovim/lua/lsp.lua
@@ -67,7 +67,7 @@ local on_attach = function(_, bufnr)
 end
 
 -- Enable the following language servers
-local servers = { "bufls", "clangd", "pyright", "ruby_ls", "ruff_lsp", "texlab" }
+local servers = { "bufls", "clangd", "pyright", "ruff_lsp", "texlab" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup({ on_attach = on_attach })
 end
