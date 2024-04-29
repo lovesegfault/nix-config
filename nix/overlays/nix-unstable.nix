bc350296cc3e27584019c11bf13155cdbae833f4
Bernardo Meurer <bernardo@meurer.org>
2024-04-29 11:08:52 -0400

    fix(nix/overlays/nix-unstable): remove unstable pin to 2.21

--- nix/overlays/nix-unstable.nix
+++ nix/overlays/nix-unstable.nix
@@ -15,17 +15,4 @@ in
   nix-prefetch-git = prev.nix-prefetch-git.override { nix = final.nixVersions.unstable; };
   nix-prefetch-git-stable = prev.nix-prefetch-git;
   prefetch-yarn-deps = prev.prefetch-yarn-deps.override { nix-prefetch-git = final.nix-prefetch-git-stable; };
-
-  nixVersions = prev.nixVersions // {
-    unstable = final.nixVersions.nix_2_21;
-    nix_2_21 = prev.nixVersions.nix_2_21.overrideAttrs (_: rec {
-      version = "2.21.1";
-      src = final.fetchFromGitHub {
-        owner = "NixOS";
-        repo = "nix";
-        rev = version;
-        hash = "sha256-iRtvOcJbohyhav+deEajI/Ln/LU/6WqSfLyXDQaNEro=";
-      };
-    });
-  };
 }
