0a68f0e711ef1543d5c5a286e73802b46e38adbf
Bernardo Meurer <bernardo@meurer.org>
2024-04-28 20:22:51 -0400

    chore(nix/overlays/sway-unstable): update

--- nix/overlays/sway-unstable.nix
+++ nix/overlays/sway-unstable.nix
@@ -1,23 +1,27 @@
 final: prev:
 {
   wlroots-unstable = (final.wlroots_0_17.overrideAttrs (old: {
-    version = "unstable-2024-03-18";
+    version = "unstable-2024-04-26";
     src = final.fetchFromGitLab {
       domain = "gitlab.freedesktop.org";
       owner = "wlroots";
       repo = "wlroots";
-      rev = "873e8e455892fbd6e85a8accd7e689e8e1a9c776";
-      hash = "sha256-5zX0ILonBFwAmx7NZYX9TgixDLt3wBVfgx6M24zcxMY=";
+      rev = "82b4bc3f5f524edd90c7c9989084cbfdb02d90e8";
+      hash = "sha256-G8fV+lO5sYw5/7Rq2mwuv05cL5bI6n5aZT5Rhku0kVA=";
     };
+
+    buildInputs = (old.buildInputs or [ ]) ++ [
+      final.lcms2
+    ];
   })).override { };
 
   sway-unwrapped = (prev.sway-unwrapped.overrideAttrs (old: {
-    version = "unstable-2024-03-18";
+    version = "unstable-2024-04-23";
     src = final.fetchFromGitHub {
       owner = "swaywm";
       repo = "sway";
-      rev = "5a7477cb8f568ce4aeb852215ad40899f18f3d91";
-      hash = "sha256-56CRUjaebyERr1JGTQFWnrET04gFg9p20GZcixgWoVY=";
+      rev = "646019cad9e8a075911e960fc7645471d9c26bf6";
+      hash = "sha256-DeHPdl5JK9s9bOIEJF/rmD6Pb1tOAGoQDri+8nt7Nng=";
     };
 
     nativeBuildInputs = with final; (old.nativeBuildInputs or [ ]) ++ [ bash-completion fish ];
