1e99ea2918f1d9dfa18584113855e1e1d234a717
Bernardo Meurer <bernardo@meurer.org>
2024-08-22 11:41:10 -0400

    fix(users/bemeurer/core/neovim): set EDITOR and VISUAL

--- users/bemeurer/core/neovim.nix
+++ users/bemeurer/core/neovim.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }: {
   # This comes from https://github.com/lovesegfault/vim-config
   home.packages = with pkgs; [ neovim-lovesegfault ];
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+  };
   programs.git.extraConfig.core.editor = "nvim";
 }
