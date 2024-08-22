fe81360f3503aa263f25ff8bf7693009d98b7b32
Bernardo Meurer <bernardo@meurer.org>
2024-08-22 17:00:03 -0400

    fix(core/aspell): use aspellWithDicts

--- core/aspell.nix
+++ core/aspell.nix
@@ -1,14 +1,10 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
-    aspellDicts.en
-    aspellDicts.en-computers
-    aspellDicts.pt_BR
+    (aspellWithDicts (ds: with ds; [
+      en
+      en-computers
+      en-science
+      pt_BR
+    ]))
   ];
-
-  # Configure aspell system wide
-  environment.etc."aspell.conf".text = ''
-    master en_US
-    add-extra-dicts en-computers.rws
-    add-extra-dicts pt_BR.rws
-  '';
 }
