da2f628efaae661990bf611d336c8f0228262bde
Bernardo Meurer <bernardo@meurer.org>
2024-09-02 21:31:18 +0000

    fix(users/bemeurer/graphical/sway/foot): main.notify -> desktop-notifications

--- users/bemeurer/graphical/sway/foot.nix
+++ users/bemeurer/graphical/sway/foot.nix
@@ -2,7 +2,7 @@
   programs.foot = {
     enable = true;
     settings = {
-      main.notify = "${pkgs.libnotify}/bin/notify-send -a foot -i foot \${title} \${body}";
+      desktop-notifications.command = "${pkgs.libnotify}/bin/notify-send -a foot -i foot \${title} \${body}";
       mouse.hide-when-typing = "yes";
       scrollback.lines = 32768;
       url.launch = "${pkgs.xdg-utils}/bin/xdg-open \${url}";
