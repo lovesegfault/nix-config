f4f423ac5c34d689aaad0926860d9ae9ea09a619
Bernardo Meurer <bernardo@meurer.org>
2024-09-09 17:44:01 -0400

    chore(nix/overlays/fix-kitty): remove upstreamed

--- nix/overlays/fix-kitty.nix
+++
@@ -1,9 +0,0 @@
-final: prev: {
-  kitty = prev.kitty.overrideAttrs (old: {
-    preBuild = (old.preBuild or "") + ''
-      # Add the font by hand because fontconfig does not finds it in darwin
-      mkdir ./fonts/
-      cp "${(final.nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})}/share/fonts/truetype/NerdFonts/SymbolsNerdFontMono-Regular.ttf" ./fonts/
-    '';
-  });
-}
