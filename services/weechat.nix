a53bfae36724c33e64a4a715965a4abbc03786d0
Bernardo Meurer <bernardo@meurer.org>
2024-09-30 23:42:12 -0300

    Revert "refactor(services/weechat): use tmux"
    This reverts commit ac974afa37e8f3548327519c83bd94eb72cdbeea.

--- services/weechat.nix
+++ services/weechat.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: with config.networking; {
   environment.persistence."/nix/state".directories = [
     {
-      directory = "/var/lib/weechat";
+      directory = config.services.weechat.root;
       user = config.systemd.services.weechat.serviceConfig.User;
       group = config.systemd.services.weechat.serviceConfig.Group;
     }
@@ -11,7 +11,7 @@
     (pkgs.writeShellApplication {
       name = "weechat-attach";
       text = ''
-        sudo --user=weechat tmux -S /var/lib/weechat/tmux attach
+        sudo --user=weechat screen -x weechat/weechat-screen
       '';
     })
   ];
@@ -31,23 +31,18 @@
         root = pkgs.glowing-bear;
       };
     };
+    weechat.enable = true;
     # for libera.chat port scanning...
     fail2ban.ignoreIP = [ "irc.libera.chat" ];
   };
 
-  systemd.services.weechat = {
-    environment.WEECHAT_HOME = "/var/lib/weechat";
-    serviceConfig = {
-      User = "weechat";
-      Group = "weechat";
-      RemainAfterExit = "yes";
-      Type = "forking";
-      ExecStart = "${pkgs.tmux}/bin/tmux -S /var/lib/weechat/tmux new -d -s weechat ${pkgs.weechat}/bin/weechat";
-      ExecStop = "${pkgs.tmux}/bin/tmux -S /var/lib/weechat/tmux kill-session -t weechat";
-    };
-
-    wantedBy = [ "multi-user.target" ];
-    wants = [ "network.target" ];
+  programs.screen = {
+    enable = true;
+    screenrc = ''
+      multiuser on
+      acladd normal_user
+      truecolor on
+    '';
   };
 
   users = {
