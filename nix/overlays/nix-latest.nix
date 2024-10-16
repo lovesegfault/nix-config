33c89e979260935805b6cf5a6fe16198e7a16030
Bernardo Meurer <bernardo@meurer.org>
2024-10-16 08:44:50 -0300

    Revert "fix: use nix 2.24.6"
    This reverts commit e2aa56da961908d6623661fa0202b2338ba7cd5c.

--- nix/overlays/nix-latest.nix
+++ nix/overlays/nix-latest.nix
@@ -15,22 +15,4 @@ in
   nix-prefetch-git = prev.nix-prefetch-git.override { nix = final.nixVersions.latest; };
   nix-prefetch-git-stable = prev.nix-prefetch-git;
   prefetch-yarn-deps = prev.prefetch-yarn-deps.override { nix-prefetch-git = final.nix-prefetch-git-stable; };
-  nix-eval-jobs = prev.nix-eval-jobs.override { nix = final.nixVersions.nix_2_24; };
-  nix-fast-build = prev.nix-fast-build.override { inherit (final) nix-eval-jobs; };
-
-  nixVersions = prev.nixVersions // {
-    latest = final.nixVersions.nix_2_24;
-    nix_2_24 = prev.nixVersions.nix_2_24.overrideAttrs (old: {
-      version = "2.24.6";
-      src = final.fetchFromGitHub {
-        owner = "NixOS";
-        repo = "nix";
-        rev = "2.24.6";
-        hash = "sha256-kgq3B+olx62bzGD5C6ighdAoDweLq+AebxVHcDnKH4w=";
-      };
-      meta = prev.nixVersions.nix_2_23.meta // {
-        name = "nix-2.24.6";
-      };
-    });
-  };
 }
