96146554a23afa536684650bf102118365e61c00
Bernardo Meurer <bernardo@meurer.org>
2025-01-08 13:06:35 -0300

    fix(users/bemeurer/graphical): disable ghostty syntax on darwin

--- users/bemeurer/graphical/default.nix
+++ users/bemeurer/graphical/default.nix
@@ -40,6 +40,7 @@
       enableBashIntegration = true;
       enableFishIntegration = true;
       enableZshIntegration = true;
+      installBatSyntax = !pkgs.stdenv.hostPlatform.isDarwin;
       # FIXME: Remove this hack when the nixpkgs pkg works again
       package = if pkgs.stdenv.hostPlatform.isDarwin then pkgs.hello else pkgs.ghostty;
     };
