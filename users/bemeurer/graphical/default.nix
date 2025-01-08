751026ef193c9bf9c3d0594f80fb3b7b284af74f
Bernardo Meurer <bernardo@meurer.org>
2025-01-08 13:44:14 -0300

    refactor(users/bemeurer/graphical): quit ghostty on close

--- users/bemeurer/graphical/default.nix
+++ users/bemeurer/graphical/default.nix
@@ -43,6 +43,9 @@
       installBatSyntax = !pkgs.stdenv.hostPlatform.isDarwin;
       # FIXME: Remove this hack when the nixpkgs pkg works again
       package = if pkgs.stdenv.hostPlatform.isDarwin then pkgs.hello else pkgs.ghostty;
+      settings = {
+        quit-after-last-window-closed = true;
+      };
     };
   };
 
