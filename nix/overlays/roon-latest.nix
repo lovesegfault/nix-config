0304cf21fbbfcaf7400fbd22a2a1adaac54d2796
Bernardo Meurer <bernardo@meurer.org>
2025-02-24 13:49:29 -0500

    chore(nix/overlays): remove roon-latest (upstreamed)

--- nix/overlays/roon-latest.nix
+++
@@ -1,9 +0,0 @@
-final: prev: {
-  roon-server = prev.roon-server.overrideAttrs (old: {
-    version = "2.47.1510";
-    src = final.fetchurl {
-      url = "https://download.roonlabs.com/updates/production/RoonServer_linuxx64_204701510.tar.bz2";
-      hash = "sha256-xuXQLniV2PKIeupfCH00j0e9mRxLocEraENqIUkdWNo=";
-    };
-  });
-}
