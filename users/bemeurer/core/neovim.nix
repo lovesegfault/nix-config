b669ffeaf6e9b16e231a995fd05eb786e821458b
Bernardo Meurer <bernardo@meurer.org>
2025-02-27 10:40:19 -0500

    feat(users/bemeurer/core/neovim): apply stylix to nixvim

--- users/bemeurer/core/neovim.nix
+++ users/bemeurer/core/neovim.nix
@@ -1,7 +1,8 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   # This comes from https://github.com/lovesegfault/vim-config
-  home.packages = with pkgs; [ neovim-lovesegfault ];
+  # c.f. https://github.com/danth/stylix/blob/e7c09d206680e6fe6771e1ac9a83515313feaf95/docs/src/configuration.md#standalone-nixvim
+  home.packages = [ (pkgs.neovim-lovesegfault.extend config.lib.stylix.nixvim.config) ];
   home.sessionVariables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
