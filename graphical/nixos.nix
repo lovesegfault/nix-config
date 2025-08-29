7d75d65d08787e0fba6fb7032939ef2ac3515c17
Bernardo Meurer Costa <beme@anthropic.com>
2025-08-29 23:50:40 +0000

    fix(graphical/nixos): xserver.displayManager -> displayManager

--- graphical/nixos.nix
+++ graphical/nixos.nix
@@ -29,7 +29,7 @@
     dbus.packages = with pkgs; [ dconf ];
     gnome.at-spi2-core.enable = true;
     xserver.enable = true;
-    xserver.displayManager.gdm = {
+    displayManager.gdm = {
       enable = true;
       autoSuspend = true;
       wayland = true;
