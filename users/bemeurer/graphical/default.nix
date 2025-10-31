ea69065cb436e72589f6d1425af79fe6ebe346c7
Bernardo Meurer Costa <beme@anthropic.com>
2025-10-31 00:18:25 +0000

    fix(graphical/fonts): noto-fonts-emoji -> noto-fonts-color-emoji

--- users/bemeurer/graphical/default.nix
+++ users/bemeurer/graphical/default.nix
@@ -64,7 +64,7 @@
       name = "Hack Nerd Font";
     };
     emoji = {
-      package = pkgs.noto-fonts-emoji;
+      package = pkgs.noto-fonts-color-emoji;
       name = "Noto Color Emoji";
     };
   };
