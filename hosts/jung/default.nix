c0c763c4655f79bd0db20b3d45bd83039f80e477
Bernardo Meurer <bernardo@meurer.org>
2025-11-03 20:10:59 -0500

    fix(hosts/jung): don't fail when /mnt/music fails to mount

--- hosts/jung/default.nix
+++ hosts/jung/default.nix
@@ -2,6 +2,7 @@
   config,
   nixos-hardware,
   pkgs,
+  utils,
   ...
 }:
 {
@@ -93,6 +94,7 @@
       options = [
         "defaults"
         "noatime"
+        "nofail"
       ];
     };
     "/mnt/music-opus" = {
@@ -101,6 +103,7 @@
       options = [
         "defaults"
         "noatime"
+        "nofail"
       ];
     };
   };
@@ -207,6 +210,25 @@
       dhcpV6Config.RouteMetric = 40;
     };
   };
+
+  systemd.services =
+    let
+      mountPoints = [
+        "/mnt/music"
+        "/mnt/music-opus"
+      ];
+      mountPointUnits = builtins.map (x: "${utils.escapeSystemdPath x}.mount") mountPoints;
+    in
+    {
+      syncthing.unitConfig = {
+        Requires = mountPointUnits;
+        After = mountPointUnits;
+      };
+      roon-server.unitConfig = {
+        Requires = mountPointUnits;
+        After = mountPointUnits;
+      };
+    };
 
   swapDevices = [ { device = "/dev/disk/by-uuid/a66412e6-ff55-4053-b436-d066319ed922"; } ];
 
