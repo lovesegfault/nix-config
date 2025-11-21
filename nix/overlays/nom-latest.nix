dda0a8e84e5a173bc49aae81c3deb878d2882570
Bernardo Meurer Costa <beme@anthropic.com>
2025-11-21 19:30:21 +0000

    chore(nix/overlays/nom-latest): update

--- nix/overlays/nom-latest.nix
+++ nix/overlays/nom-latest.nix
@@ -3,8 +3,11 @@ final: prev: {
     src = final.fetchFromGitHub {
       owner = "maralorn";
       repo = "nix-output-monitor";
-      rev = "698e6f3afdc9d68dd65d84df7b030499dbfaf84b";
-      hash = "sha256-QwEVaUxvXEdx5icIZZYQQjvJO5j0+GeWtJvCJ/LZwpA=";
+      rev = "0cb46615fb8187e4598feac4ccf8f27a06aae0b7";
+      hash = "sha256-gFjy6lIr+mcoubIT+NufH2dqXiQmqKOlwue1OjW6n2g=";
+      postFetch = ''
+        rm -f $out/log.json
+      '';
     };
   });
 }
