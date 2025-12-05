3f687d088d181c8df62e421521db3da0cae72967
Bernardo Meurer <beme@anthropic.com>
2025-12-05 00:13:11 -0500

    refactor(home/hilbert): tune zsh

--- configurations/home/hilbert/default.nix
+++ configurations/home/hilbert/default.nix
@@ -24,7 +24,6 @@ in
     uid = 999;
     file.".ssh/config".enable = false;
     packages = with pkgs; [ nixVersions.latest ];
-    sessionPath = [ "${config.home.homeDirectory}/.npm-global/bin" ];
   };
 
   programs = {
@@ -42,13 +41,21 @@ in
     };
     git.settings = {
       user.email = lib.mkForce "beme@anthropic.com";
+      user.signingKey = lib.mkForce "/root/.ssh/git.pub";
       gpg = lib.mkForce { format = "ssh"; };
       commit.gpgsign = true;
     };
-    zsh.initContent = lib.mkOrder 0 ''
-      if [[ "$ZSH_VERSION" != "${config.programs.zsh.package.version}" ]]; then
-        exec "${config.programs.zsh.package}/bin/zsh"
-      fi
-    '';
+    zsh.initContent = lib.mkMerge [
+      (lib.mkOrder 0 ''
+        if [[ "$ZSH_VERSION" != "${config.programs.zsh.package.version}" ]]; then
+          exec "${config.programs.zsh.package}/bin/zsh"
+        fi
+      '')
+      # Immediately before home-manager applies aliases at 1100
+      (lib.mkOrder 1099 ''
+        export ANT_PRISTINE_SHELL=1
+        source /root/code/config/remote/zshrc
+      '')
+    ];
   };
 }
