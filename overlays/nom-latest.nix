b76a831ff10031a64d3754188bdd3af3be8bf01c
Bernardo Meurer <beme@anthropic.com>
2025-12-16 18:00:47 +0000

    feat(overlays/nom-latest): use my patches

--- overlays/nom-latest.nix
+++ overlays/nom-latest.nix
@@ -1,10 +1,11 @@
 final: prev: {
+  # FIXME: using my patch for now
   nix-output-monitor = prev.nix-output-monitor.overrideAttrs (_: {
     src = final.fetchFromGitHub {
-      owner = "maralorn";
+      owner = "lovesegfault";
       repo = "nix-output-monitor";
-      rev = "0cb46615fb8187e4598feac4ccf8f27a06aae0b7";
-      hash = "sha256-gFjy6lIr+mcoubIT+NufH2dqXiQmqKOlwue1OjW6n2g=";
+      rev = "c9ce708f72b95d92f52b77e04d51dd214cc58592";
+      hash = "sha256-dTL3f1zx2PNqmemvWhmuwdXdI1F3FcrkHC86F1Y78qQ=";
       postFetch = ''
         rm -f $out/log.json
       '';
