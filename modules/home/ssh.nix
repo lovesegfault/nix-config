0f79cd05f41d7ed066b1aa49179ac168b441e61d
Bernardo Meurer <beme@anthropic.com>
2026-03-27 01:22:02 -0400

    refactor(home/ssh): use matchBlocks and rename deprecated option

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -1,27 +1,36 @@
 {
-  home.file.".ssh/config".text = ''
-    Include ~/.ssh/config.host
-
-    Host *
-      CanonicalizeHostname yes
-      PermitLocalCommand yes
-      CanonicalDomains meurer.org.beta.tailscale.net
-
-    Match canonical Host *.meurer.org,*.meurer.org.beta.tailscale.net
-      ForwardAgent yes
-
-    Match canonical Host *
-      ChallengeResponseAuthentication no
-      ControlMaster auto
-      ControlPath ~/.ssh/ssh-%r@%h:%p
-      ControlPersist 30m
-      ForwardAgent no
-      ForwardX11 no
-      ForwardX11Trusted no
-      HashKnownHosts yes
-      ServerAliveCountMax 5
-      ServerAliveInterval 60
-      StrictHostKeyChecking ask
-      VerifyHostKeyDNS yes
-  '';
+  programs.ssh = {
+    enable = true;
+    includes = [ "~/.ssh/config.host" ];
+    matchBlocks = {
+      "*" = {
+        extraOptions = {
+          CanonicalizeHostname = "yes";
+          PermitLocalCommand = "yes";
+          CanonicalDomains = "meurer.org.beta.tailscale.net";
+        };
+      };
+      "canonical-meurer" = {
+        match = "canonical Host *.meurer.org,*.meurer.org.beta.tailscale.net";
+        forwardAgent = true;
+      };
+      "canonical-all" = {
+        match = "canonical Host *";
+        forwardAgent = false;
+        forwardX11 = false;
+        forwardX11Trusted = false;
+        hashKnownHosts = true;
+        serverAliveCountMax = 5;
+        serverAliveInterval = 60;
+        controlMaster = "auto";
+        controlPath = "~/.ssh/ssh-%r@%h:%p";
+        controlPersist = "30m";
+        extraOptions = {
+          KbdInteractiveAuthentication = "no";
+          StrictHostKeyChecking = "ask";
+          VerifyHostKeyDNS = "yes";
+        };
+      };
+    };
+  };
 }
