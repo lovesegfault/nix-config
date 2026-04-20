9855075adbc328139e47672895f7a12a6e1f50b3
Bernardo Meurer <beme@anthropic.com>
2026-04-20 05:02:52 -0400

    refactor(nixos/comte): keynes 64 -> 96

--- configurations/nixos/comte/default.nix
+++ configurations/nixos/comte/default.nix
@@ -62,7 +62,7 @@ in
         protocol = "ssh-ng";
         sshUser = "root";
         sshKey = "/etc/ssh/ssh_host_ed25519_key";
-        maxJobs = 64;
+        maxJobs = 96;
         speedFactor = 1;
         supportedFeatures = [
           "benchmark"
